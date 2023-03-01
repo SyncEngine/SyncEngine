082d7eceb44ac061027ee587b8126332f2e28e9e
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-03-01 16:30:52 +0100

    Add default controller

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class IndexController extends AbstractController
+class IndexController extends DefaultController
 {
     #[Route('/', name: 'app_index')]
     public function index(): Response
