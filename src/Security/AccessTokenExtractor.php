09b56aa821d4ede8a79bef3ced913ae1f87dd7cd
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-03-09 15:55:06 +0100

    Initial install AccessTokens

---
+++ src/Security/AccessTokenExtractor.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\AccessToken\AccessTokenExtractorInterface;
+
+class AccessTokenExtractor implements AccessTokenExtractorInterface
+{
+	public function extractAccessToken( Request $request ): ?string {
+		// TODO: Implement extractAccessToken() method.
+		return '';
+	}
+}
