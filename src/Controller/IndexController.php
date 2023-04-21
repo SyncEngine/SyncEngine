cabfcd56eeeea49925ee78fa6c6fea8d28604e78
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-04-21 17:35:28 +0200

    Enhance overview page

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Connection;
 use App\Entity\Automation;
+use App\Entity\Flow;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,9 +14,13 @@ class IndexController extends DefaultController
 	#[Route('/', name: 'app_index')]
 	public function index(EntityManagerInterface $entityManager): Response
 	{
+		$connections = $entityManager->getRepository(Connection::class)->findAll();
 		$automations = $entityManager->getRepository(Automation::class)->findAll();
+		$flows = $entityManager->getRepository(Flow::class)->findAll();
 		return $this->render('index/index.html.twig', [
+			'connections' => $connections,
 			'automations' => $automations,
+			'flows' => $flows,
 		]);
 	}
 }
