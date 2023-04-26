3dea70c23a6e7fec05bbad6f7411dd1bf301d200
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-04-26 15:04:50 +0200

    Move step executions to step controller

--- src/Controller/ModuleController.php
+++ src/Controller/ModuleController.php
@@ -9,11 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class ModuleController extends AbstractController
 {
 
-	public function executeModuleStep( string $moduleName, $config, $datafields )
-	{
-		$moduleClass = "modules\\".$moduleName."\\".$moduleName;
-		$mod = new $moduleClass;
+	public function __construct() {
 
-		return $mod->executeStep( $config, $datafields );
 	}
 }
