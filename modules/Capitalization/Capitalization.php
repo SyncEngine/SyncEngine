c4f69a9ee69e1fa1d17fde3d394dd44934bb5c70
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-05-03 12:17:38 +0200

    Improve compatibility with modulecontroller

--- modules/Capitalization/Capitalization.php
+++ modules/Capitalization/Capitalization.php
@@ -3,14 +3,9 @@
 namespace modules\Capitalization;
 
 use App\Controller\ModuleController;
-use App\Controller\TaskController;
 
 class Capitalization extends ModuleController
 {
-	public function getTask( string $name ): TaskController {
-		// TODO: Implement getTask() method.
-	}
-
 	public function executeTask( string $task, array $config, array $data ): array
 	{
 		$fixTag = $config['tag'];
