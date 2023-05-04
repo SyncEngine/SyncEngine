b9f585d75e6046dfd3769cd6712c72bedaa7f798
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-05-04 18:00:16 +0200

    Basic config loader

--- assets/controllers/config_controller.js
+++ assets/controllers/config_controller.js
@@ -1,12 +1,40 @@
 import * as React from 'react';
 import * as ReactDOMClient from 'react-dom/client';
 import { Controller } from '@hotwired/stimulus';
+import { StepView } from './views/StepView';
+
+const views = {
+	step: StepView,
+}
 
 export default class extends Controller {
     connect() {
-	    const root = ReactDOMClient.createRoot( this.element );
+	    let root = this.element;
 
-	    console.log( this.context );
-	    root.render( <hr /> );
+	    switch ( this.element.type ) {
+		    case 'textarea':
+		    case 'input':
+			    root = document.createElement('div');
+				root.id = this.element.id + '_root';
+				// @todo Hide textarea?
+				this.element.after( root );
+				break;
+		    default:
+				// Wrong element.
+				return;
+				break;
+	    }
+
+		const {
+			type,
+			config,
+		} = this.element.dataset;
+
+		//<textarea {...this.element}></textarea>
+	    ReactDOMClient.createRoot( root ).render(
+			React.createElement( views[ type ], {
+				config: config
+			} )
+	    );
     }
 }
---
+++ assets/controllers/views/StepView.jsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+export function StepView( props ) {
+
+	console.log( props );
+
+	return (
+		<p>View: Step</p>
+	);
+}
