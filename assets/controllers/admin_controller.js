e1cce2464432d85c95df51e51379fe5304f782bc
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-05-04 14:32:25 +0200

    Switch to bootstrap for easier collaboration between React and default forms

--- assets/controllers/admin_controller.js
+++ assets/controllers/admin_controller.js
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import * as ReactDOMClient from 'react-dom/client';
 import { Controller } from '@hotwired/stimulus';
-import { Loader } from '../components/Loader';
 
 export default class extends Controller {
     connect() {
-	    const root = ReactDOMClient.createRoot( this.element );
-	    root.render( <Loader {...this.context} /> );
+	    this.element.textContent = 'Admin Controller';
+	    //const root = ReactDOMClient.createRoot( this.element );
+	    //root.render( <ChakraLoader {...this.context} /> );
     }
 }
