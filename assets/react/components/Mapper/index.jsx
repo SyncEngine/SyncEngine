075b538d5eddffdff68619d5c377c85a97d610e5
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-05-12 20:44:56 +0200

    Basic work for mapper component

---
+++ assets/react/components/Mapper/index.jsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
+export function Mapper( props ) {
+	const [ mapper, setMapper ] = useState([]);
+
+	let {
+		sourceKeys,
+		targetKeys,
+	} = props;
+
+	const updateMap = ( values ) => {
+		// Parser..
+		setMapper( values );
+	}
+
+	return (
+		<Form>
+			<Row>
+				<Col>
+					Source
+				</Col>
+				<Col>
+					Target
+				</Col>
+			</Row>
+			{
+				mapper.map( ( data, index ) => {
+					const {
+						source,
+						target,
+					} = data;
+
+					return (
+						<Row key={ index }>
+							<Col>
+								<Form.Select aria-label="" value={ source }>
+									{
+										sourceKeys.map( ( sourceKey, index ) => {
+											return <option value={ sourceKey } key={ index }>{ sourceKey }</option>
+										} )
+									}
+								</Form.Select>
+							</Col>
+
+							<Col>
+								{ targetKeys &&
+								  <Form.Select aria-label="" value={ target }>
+									  {
+										  targetKeys.map( ( targetKey, index ) => {
+											  return <option value={ targetKey } key={ index }>{ targetKey }</option>
+										  } )
+									  }
+								  </Form.Select>
+								}
+								{ ! targetKeys &&
+									<Form.Control type="text" value={ target } />
+								}
+							</Col>
+						</Row>
+					)
+				} )
+			}
+		</Form>
+	);
+}
