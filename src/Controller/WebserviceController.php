a0ad220a0447ce1d344967ffac766241b864762f
Niek Koonen <info@inform-all.nl>
2023-05-15 13:19:02 +0200

    Changed to webservice controller

---
+++ src/Controller/WebserviceController.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WebserviceController extends AbstractController
+{
+	public function apiKey($config, $results)
+	{
+		$curl = curl_init();
+
+		curl_setopt_array($curl, array(
+			CURLOPT_URL => $config["url"],
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_ENCODING => '',
+			CURLOPT_MAXREDIRS => 10,
+			CURLOPT_TIMEOUT => 0,
+			CURLOPT_FOLLOWLOCATION => true,
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST => 'POST',
+			CURLOPT_POSTFIELDS => $results,
+			CURLOPT_HTTPHEADER => array(
+				$config["key"].': '.$config["value"]
+			),
+		));
+
+		return $this->executeCurl($curl);
+	}
+
+	public function noAuthMethod($config, $results)
+	{
+		$curl = curl_init();
+		curl_setopt_array($curl, [
+			CURLOPT_URL => $config["url"],
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_ENCODING => "",
+			CURLOPT_MAXREDIRS => 10,
+			CURLOPT_TIMEOUT => 30,
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST => "POST",
+			CURLOPT_POSTFIELDS => $results,
+			CURLOPT_HTTPHEADER => ["accept: application/json", "content-type: application/json"]
+		]);
+		return $this->executeCurl($curl);
+	}
+
+	public function basicAuthMethod($config, $results)
+	{
+		$curl = curl_init();
+		$base64 = base64_encode($config["username"] . ":" . $config["password"]);
+		//@todo select content-type accordingly to results format
+		$header = ["Authorization: Basic " . $base64, "accept: application/json", "content-type: application/json"];
+		curl_setopt_array($curl, [
+			CURLOPT_URL => $config["url"],
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_ENCODING => "",
+			CURLOPT_MAXREDIRS => 10,
+			CURLOPT_TIMEOUT => 30,
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST => "POST",
+			CURLOPT_POSTFIELDS => $results,
+			CURLOPT_HTTPHEADER => $header
+		]);
+
+		return $this->executeCurl($curl);
+	}
+
+	public function uploadToFTP($config, $results, $sftp)
+	{
+		$filename = strval($config['filename']);
+		$localCSVfile = fopen($filename, 'w');
+		fputcsv($localCSVfile, $results);
+		fclose($localCSVfile);
+
+		$localCSVfile = fopen($filename, 'r');
+		$curl = curl_init();
+		if ($sftp) {
+			curl_setopt($curl, CURLOPT_PROTOCOLS, CURLPROTO_SFTP);
+			$baseProtocol = 'sftp://';
+		} else {
+			$baseProtocol = 'ftp://';
+		}
+		curl_setopt($curl, CURLOPT_URL, $baseProtocol . $config["username"] . ':' . $config["password"] . '@' . $config["url"] . '/' . $config["path"] . '/' . $filename);
+		curl_setopt($curl, CURLOPT_UPLOAD, 1);
+		curl_setopt($curl, CURLOPT_INFILE, $localCSVfile);
+		curl_setopt($curl, CURLOPT_INFILESIZE, filesize($filename));
+		$response = $this->executeCurl($curl);
+		fclose($localCSVfile);
+		unlink($filename);
+
+		return $response;
+	}
+
+	public function bearerToken($config, $results)
+	{
+		$curl = curl_init();
+		curl_setopt_array($curl, [
+			CURLOPT_URL => $config["url"],
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_ENCODING => '',
+			CURLOPT_MAXREDIRS => 10,
+			CURLOPT_TIMEOUT => 0,
+			CURLOPT_FOLLOWLOCATION => true,
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST => 'POST',
+			CURLOPT_POSTFIELDS => $results,
+			CURLOPT_HTTPHEADER => array(
+				'Authorization: Bearer ' . $config["Bearer Token"]
+			)
+		]);
+		return $this->executeCurl($curl);
+	}
+
+	public function executeCurl($curl)
+	{
+		$response = curl_exec($curl);
+		$err = curl_error($curl);
+		curl_close($curl);
+		if ($err) {
+			$response = "cURL Error #:" . $err;
+		}
+		return $response;
+	}
+}
