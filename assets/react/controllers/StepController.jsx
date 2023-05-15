908073a785f702f92f9afc5c46595cb22be1b9de
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-05-15 10:08:50 +0200

    Setup base for task selection in a step.

--- assets/react/controllers/StepController.jsx
+++ assets/react/controllers/StepController.jsx
@@ -1,10 +1,50 @@
 import * as React from 'react';
+import { getTaskTypes } from '../utils/globals';
+import { TaskController } from "./TaskController";
+import { TaskSelector } from "../components/TaskSelector";
 
 export function StepController( props ) {
 
-	console.log( props );
+	const {
+		config,
+	} = props;
+
+	const {
+		tasks,
+	} = config;
+
+	const addTask = ( type ) => {
+		let allTasks = getTaskTypes();
+		tasks.push( allTasks( type ) );
+	}
+
+	const AddTask = (
+		<TaskSelector options={ getTaskTypes } onChange={ addTask }></TaskSelector>
+	);
+
+	if ( ! tasks ) {
+		return AddTask;
+	}
 
 	return (
-		<p>View: Step</p>
+		<>
+			<Accordion>
+			{
+				tasks.map( ( task, index ) => {
+					return (
+						<Accordion.Item key={ index }>
+							<Accordion.Header>{ task.label ?? task.name ?? 'Task: ' + index }</Accordion.Header>
+							<Accordion.Body>
+								<TaskController {...task}/>
+							</Accordion.Body>
+						</Accordion.Item>
+					)
+				} )
+			}
+			</Accordion>
+			<hr>
+			<h2>Add Task</h2>
+			{ AddTask }
+		</>
 	);
 }
---
+++ assets/react/controllers/TaskController.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export function TaskController( props ) {
+
+	const {
+		fields,
+	} = props;
+
+	return (
+		<p>View: Step</p>
+	);
+}
