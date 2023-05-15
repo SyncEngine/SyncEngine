4bd97940f86437f20133d7878cb354f1bb25dce3
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-05-15 11:45:05 +0200

    Parse task and load field controller

--- assets/react/controllers/TaskController.jsx
+++ assets/react/controllers/TaskController.jsx
@@ -3,10 +3,40 @@ import React from 'react';
 export function TaskController( props ) {
 
 	const {
-		fields,
+		name,
+		label,
+		description,
 	} = props;
 
+	if ( ! props.hasOwnProperty( 'fields' ) ) {
+		return (
+			<p>Task has no fields</p>
+		)
+	}
+
+	let fields = [];
+	if ( 'function' !== typeof props.fields.map ) {
+		for ( const key in props.fields ) {
+			if ( ! props.fields.hasOwnProperty( key ) ) {
+				continue;
+			}
+			if ( ! props.fields[ key ].name ) {
+				props.fields[ key ].name = key;
+			}
+			fields.push( props.fields[ key ] );
+		}
+	} else {
+		fields = props.fields;
+	}
+
 	return (
-		<p>View: Step</p>
+		<>
+			{ description }
+			{
+				fields.map( ( field, index ) => {
+					return ( <FieldController key={ index } {...field}></FieldController> )
+				} )
+			}
+		</>
 	);
 }
