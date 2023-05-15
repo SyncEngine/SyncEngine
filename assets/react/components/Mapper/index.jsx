badd8fe612077aeb24a3681c9a12db2845e045de
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-05-15 11:10:00 +0200

    Fix react imports

--- assets/react/components/Mapper/index.jsx
+++ assets/react/components/Mapper/index.jsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
+import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
 export function Mapper( props ) {
-	const [ mapper, setMapper ] = useState([]);
+	const [ mapper, setMapper ] = useState( [] );
 
 	let {
 		sourceKeys,
