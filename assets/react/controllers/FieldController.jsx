faf81e81e6bd6d157904d3888f5a4c14bd08834a
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-05-15 12:15:06 +0200

    Load first field types

--- assets/react/controllers/FieldController.jsx
+++ assets/react/controllers/FieldController.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
+import { Mapper } from "../components/Mapper";
 
 export function FieldController( props ) {
 
@@ -8,13 +9,27 @@ export function FieldController( props ) {
 		name,
 		label,
 		description,
+		value,
 	} = props;
+
+	let field;
+
+	switch ( type ) {
+		case 'mapper':
+			field = ( <Mapper {...props} /> );
+			break;
+		default:
+			// @todo custom field types?
+			field = ( <Form.Control {...props} /> );
+			break;
+	}
 
 	return (
 		<>
 			{ name }
 			{ type }
 			{ description }
+			{ field }
 		</>
 	);
 }
