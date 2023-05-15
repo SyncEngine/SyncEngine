1e266aaf46c0de955b2881de1328ae7f957339ed
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-05-15 15:11:29 +0200

    Improve UI

--- assets/react/controllers/TaskController.jsx
+++ assets/react/controllers/TaskController.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Stack from 'react-bootstrap/Stack';
 import { objectToMappable } from "../utils/format";
 import FieldController from "./FieldController";
 
@@ -24,13 +25,13 @@ export default function TaskController( props ) {
 	}
 
 	return (
-		<>
+		<Stack gap={ 2 }>
 			{ description }
 			{
 				objectToMappable( props.fields, 'name' ).map( ( field, index ) => {
 					return ( <FieldController { ...field } key={ index } onChange={ ( value ) => { update( value, field.name ) } }></FieldController> )
 				} )
 			}
-		</>
+		</Stack>
 	);
 }
