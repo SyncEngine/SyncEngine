d83cba3ee263ce5c5737973ddd86b03cd357c502
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-05-16 22:42:43 +0200

    Basic conditions controller

---
+++ assets/react/controllers/ConditionsController.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import { Stack, } from 'react-bootstrap';
+import Params from "../components/Params";
+import { getOperators } from "../utils/conditionals";
+
+export default function ConditionsController( props ) {
+
+	const {
+		conditionTypes: conditionTypes = {},
+		value: value = [],
+		onChange,
+	} = props;
+
+	const [ conditions, setConditions ] = useState( value );
+
+	const updateConditions = ( newConditions ) => {
+		setConditions( newConditions );
+		onChange( conditions );
+	}
+
+	return (
+		<Stack gap={2}>
+			<Params
+				value={ value }
+				onChange={ updateConditions }
+				columns={ {
+					key: {
+						label: 'Field',
+					},
+					operator: {
+						label: 'Operator',
+						predefined: getOperators(),
+					},
+					compare: {
+						table: 'Compare',
+						mutliple: true,
+					}
+				} }
+			/>
+		</Stack>
+	);
+}
