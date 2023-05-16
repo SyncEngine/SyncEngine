3fb069b3da6ba7840ce645cf18af537a2148fb71
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-05-16 09:22:17 +0200

    new TasksController and convert steps controller to tabs view

--- assets/react/components/TaskSelector/index.jsx
+++ assets/react/components/TaskSelector/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import { objectToMappable } from "../../utils/format";
 
 export default function TaskSelector( props ) {
@@ -10,24 +11,26 @@ export default function TaskSelector( props ) {
 	} = props;
 
 	return (
-		<Form.Select onChange={ ( event ) => { onChange( event.target.value ) } } value="">
-			<option>-- Add Task --</option>
-			{
-				objectToMappable( options ).map( ( option, index ) => {
-					let {
-						key,
-						label,
-						name,
-						type,
-						value
-					} = option;
+		<FloatingLabel label="Add Task" className="my-2">
+			<Form.Select onChange={ ( event ) => { onChange( event.target.value ) } } value="">
+				<option>-- Select Task --</option>
+				{
+					objectToMappable( options ).map( ( option, index ) => {
+						let {
+							key,
+							label,
+							name,
+							type,
+							value
+						} = option;
 
-					value = value ?? type ?? name;
-					label = label ?? name ?? key ?? value;
+						value = value ?? type ?? name;
+						label = label ?? name ?? key ?? value;
 
-					return ( <option key={ index } value={ value }>{ label }</option> );
-				} )
-			}
-		</Form.Select>
+						return ( <option key={ index } value={ value }>{ label }</option> );
+					} )
+				}
+			</Form.Select>
+		</FloatingLabel>
 	);
 }
