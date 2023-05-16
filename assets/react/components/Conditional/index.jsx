15cef40bbf56c1bcfa7329ec43728a6c419d9341
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-05-16 17:01:44 +0200

    Basic conditional component

---
+++ assets/react/components/Conditional/index.jsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import { hasValue, isEmpty } from "../../utils/conditionals";
+
+export default function Conditional( props ) {
+	const {
+		children,
+		conditionals,
+		data,
+	} = props;
+
+	const validate = () => {
+		let valid = true;
+		if ( Object.keys( conditionals ).length ) {
+			for ( let key in conditionals ) {
+				const conditional = conditionals[ key ];
+
+				const {
+					compare,
+					key,
+					value
+				} = conditional;
+
+				switch ( compare ) {
+					case 'isset':
+						valid = data.hasOwnProperty( key );
+						break;
+					case 'empty':
+						valid = data.hasOwnProperty( key ) || isEmpty( data[ key ] );
+						break;
+					case '<':
+						valid = value < data[ key ];
+						break;
+					case '>':
+						valid = value > data[ key ];
+						break;
+					case '<=':
+						valid = value <= data[ key ];
+						break;
+					case '>=':
+						valid = value >= data[ key ];
+						break;
+					case '!=':
+						valid = value != data[ key ];
+						break;
+					case '==':
+						valid = value == data[ key ];
+						break;
+					case '!==':
+						valid = value !== data[ key ];
+						break;
+					case '===':
+						valid = value === data[ key ];
+						break;
+					default:
+						if ( ! data.hasOwnProperty( key ) ) {
+							valid = isEmpty( value );
+						}
+						if ( value ) {
+							if ( true === value ) {
+								valid = hasValue( data[ key ] );
+							}
+							valid = value === data[ key ];
+						} else {
+							valid = isEmpty( data[ key ] );
+						}
+
+						if ( ! valid ) {
+							break;
+						}
+						break;
+				}
+			}
+		}
+
+		return valid;
+	}
+
+	if ( validate() ) {
+		return children;
+	}
+	return [];
+}
