5ac66774ff0acbd3e404bdce002ee77643cb4d49
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-06-02 17:03:28 +0200

    Fix getting module tasks

--- src/Controller/ModulesController.php
+++ src/Controller/ModulesController.php
@@ -2,15 +2,39 @@
 
 namespace App\Controller;
 
+use Symfony\Component\Finder\Finder;
+
 class ModulesController extends AdminController
 {
 	public static function getModule( string $module )
 	{
 		$moduleClass = "modules\\" . $module . "\\" . $module;
 		if ( class_exists( $moduleClass ) ) {
-			return new $moduleClass;
+			return new $moduleClass();
 		}
 		// @todo Error or log.
 		return null;
+	}
+
+	public function getModules(): array {
+		$modules = [];
+		$finder  = new Finder();
+
+		$finder->files()->in('../modules')->name('*.php');
+
+		foreach ( $finder as $file ) {
+			$class_name = "modules\\" . $file->getFilenameWithoutExtension() . "\\" . $file->getFilenameWithoutExtension();
+
+			if ( class_exists( $class_name ) ) {
+				try {
+					$module = new $class_name();
+				} catch ( \Throwable $e ) {
+					// @todo Error or log.
+					continue;
+				}
+			}
+		}
+
+		return $modules;
 	}
 }
