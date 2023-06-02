0a82f8ca08c4e9c4ba507418f9359c08510a7a1b
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-06-02 17:32:08 +0200

    Update packages and rebuild

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\UX\React\ReactBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
 ];
