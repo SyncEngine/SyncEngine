ef0ab9ffe43a825d67d5f3b792cf373a1cbc5239
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-06-05 15:51:24 +0200

    Helper methods for module tasks

--- src/Model/TaskModel.php
+++ src/Model/TaskModel.php
@@ -3,6 +3,7 @@
 namespace App\Model;
 
 use App\Model\Trait\Task;
+use App\Service\ModuleService;
 
 abstract class TaskModel
 {
@@ -20,5 +21,20 @@ abstract class TaskModel
 	final static function isTask( $class ): bool
 	{
 		return $class instanceof TaskModel || in_array( "App\Model\Trait\Task", class_uses( $class ), true );
+	}
+
+	final static function isModuleTask( $class ): bool
+	{
+		return str_starts_with( ( new \ReflectionClass( $class ) )->getNamespaceName(), ModuleService::getRootNamespace() );
+	}
+
+	final static function getModule( $class ): bool
+	{
+		$parts = explode( "\\", get_class( $class ) );
+		array_pop( $parts ); // Remove Class name.
+		array_pop( $parts ); // Remove Task namespace.
+		$className = array_pop( $parts );
+		$moduleClass = implode( "\\", $parts ) . "\\" . $className . "\\" . $className;
+		return new $moduleClass();
 	}
 }
