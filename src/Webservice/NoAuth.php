87973999ecb23b4e61c3f3e4e0b25a1d87a14287
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-06-13 13:44:33 +0200

    Create noAuth webservice

---
+++ src/Webservice/NoAuth.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Webservice;
+
+use App\Model\WebserviceModel;
+use mysql_xdevapi\Exception;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class NoAuth extends WebserviceModel
+{
+	public function __construct()
+	{
+		$this->type = 'noauth';
+		$this->name = 'No Auth';
+		$this->description = 'Connect without authorization';
+
+		parent::__construct();
+	}
+
+	public function getAuthFields(): array
+	{
+		return [
+			'host' => [
+				'label' => 'Host',
+				'type' => 'text',
+			],
+		];
+	}
+
+	public function getFields(): array
+	{
+		// @todo define task fields.
+		// @todo allow override or even custom auth fields.
+		return [
+			'endpoint' => [
+				'label' => 'Endpoint',
+				'type' => 'text',
+			],
+			'format' => [
+				'label' => 'Format',
+				'type' => 'select',
+				'choices' => [
+					// @todo other formats.
+					'json' => 'JSON',
+				]
+			],
+		];
+	}
+
+	public function getClientOptions( array $config = array() ): array
+	{
+		$options = [];
+
+		// @todo Set headers
+		// @todo Set content type
+		// $options['headers'] => [
+		//  	'Content-Type' => 'text/plain',
+		// ]
+
+		return $options;
+	}
+
+	public function getRequestUrl( array $config ): string
+	{
+		return $config['host' ] . $config['endpoint'];
+	}
+
+	public function retrieve( array $config )
+	{
+		$client = $this->getClient( $config );
+		// TODO: Implement retrieve() method.
+
+		try {
+			$response = $client->request( 'GET', $this->getRequestUrl( $config ) );
+
+			return $response->getContent();
+		} catch ( TransportExceptionInterface $e ) {
+			// @todo error.
+		}
+	}
+
+	public function send( array $config, $data )
+	{
+		$client = $this->getClient( $config );
+		// TODO: Implement send() method.
+
+		try {
+			$response = $client->request( 'POST', $this->getRequestUrl( $config ), [ 'body' => $data ] );
+
+			return $response->getContent();
+		} catch ( TransportExceptionInterface $e ) {
+			// @todo error.
+		}
+	}
+}
