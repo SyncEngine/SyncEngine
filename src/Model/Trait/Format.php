4f4d6b4311ecbdc593be0452ab93f6d499c3f3b4
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-06-13 14:57:05 +0200

    Format trait

---
+++ src/Model/Trait/Format.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Model\Trait;
+
+use Symfony\Component\Serializer\Encoder\CsvEncoder;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Encoder\YamlEncoder;
+
+trait Format
+{
+	public function getFormatEncoder( $format )
+	{
+		switch ( $format ) {
+			case 'json':
+				return new JsonEncoder();
+			case 'csv':
+				return new CsvEncoder();
+			case 'xml':
+				return new XmlEncoder();
+			case 'yml':
+				return new YamlEncoder();
+		}
+		return null;
+	}
+
+	public function toFormat( string $format, array $data, array $context = [] ): string
+	{
+		return $this->getFormatEncoder( $format )->encode( $data, $format, $context );
+	}
+
+	public function fromFormat( string $format, string $data, array $context = [] ): array
+	{
+		return $this->getFormatEncoder( $format )->decode( $data, $format, $context );
+	}
+}
