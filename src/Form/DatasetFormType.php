ebfda66a5d113dd385dc95212c3117f993e65df5
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-06-14 20:27:27 +0200

    Create DatasetFormType.php

---
+++ src/Form/DatasetFormType.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Dataset;
+use App\Form\Type\JsonType;
+use App\Service\ModuleService;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DatasetFormType extends AbstractType
+{
+	public function buildForm(FormBuilderInterface $builder, array $options): void
+	{
+		$modules = ModuleService::getModules();
+		foreach ( $modules as $key => $module ) {
+			$modules[ $module->getName() ] = $module->getName();
+			unset( $modules[ $key ] );
+		}
+
+		$builder
+			->add('name', TextType::class, [
+				'row_attr' => [
+					'class' => 'form-floating mb-2',
+				],
+			] )
+			->add('description', TextType::class, [
+				'row_attr' => [
+					'class' => 'form-floating mb-2',
+				],
+			] )
+			->add('type', ChoiceType::class, [
+				'row_attr' => [
+					'class' => 'form-floating mb-2',
+				],
+				'choices' => [
+					'entities' => 'Entities',
+					'mapper' => 'Mapper',
+					'other' => 'Other',
+				],
+			] )
+			->add('type', ChoiceType::class, [
+				'row_attr' => [
+					'class' => 'form-floating mb-2',
+				],
+				'choices' => $modules,
+			] )
+			->add( 'config', JsonType::class, [
+				'row_attr' => [
+					'class' => 'form-floating mb-2',
+				],
+				'attr' => [
+					'data-controller' => 'config',
+					'data-type'       => 'dataset',
+					'data-args'       => json_encode([]),
+				]
+			] );
+	}
+
+	public function configureOptions(OptionsResolver $resolver): void
+	{
+		$resolver->setDefaults([
+			'data_class' => Dataset::class,
+		]);
+	}
+}
