2c91a56b169d4367fb8ec53f2d5aa3544ab0667a
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-06-14 20:03:03 +0200

    Create dataset entity and repo

---
+++ src/Repository/DatasetRepository.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Dataset;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Dataset>
+ *
+ * @method Dataset|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Dataset|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Dataset[]    findAll()
+ * @method Dataset[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class DatasetRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Dataset::class);
+    }
+
+    public function save(Dataset $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(Dataset $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+//    /**
+//     * @return Dataset[] Returns an array of Dataset objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('d')
+//            ->andWhere('d.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('d.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Dataset
+//    {
+//        return $this->createQueryBuilder('d')
+//            ->andWhere('d.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
