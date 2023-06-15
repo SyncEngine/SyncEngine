2c0b82dbf43bbac3cc914f21a8d01197bbdd5c30
Niek Koonen <info@inform-all.nl>
2023-06-15 16:36:41 +0200

    Added FTP webservice functions

--- src/Webservice/Ftp.php
+++ src/Webservice/Ftp.php
@@ -3,6 +3,7 @@
 namespace App\Webservice;
 
 use App\Model\WebserviceModel;
+use Symfony\Component\Finder\Finder;
 
 class Ftp extends WebserviceModel
 {
@@ -39,7 +40,7 @@ class Ftp extends WebserviceModel
 
 	public function getFields(): array
 	{
-		return [
+		$fields = [
 			'filename' => [
 				'label' => 'Filename',
 				'type' => 'text',
@@ -48,7 +49,14 @@ class Ftp extends WebserviceModel
 				'label' => 'Path',
 				'type' => 'text',
 			],
+			'override' => [
+				'label' => 'Overwrite if file exists',
+				'type' => 'boolean',
+				'conditional' => [] //@ToDo task is sender
+			],
 		];
+
+		return array_merge( $this->getFormatFields(), $fields );
 	}
 
 	public function getRequestUrl( array $config ): string {
@@ -57,11 +65,82 @@ class Ftp extends WebserviceModel
 
 	public function retrieve( array $config )
 	{
-		// TODO: Implement retrieve() method.
+		try {
+			$finder = new Finder();
+			$finder->in("ftp://".$config['username' ].":".$config['password' ]."@".$config['host' ].$config['path']);
+			$finder->name($config['filename']);
+
+			if (!$finder->hasResults()) {
+				var_dump("File not found");
+				// @todo error.
+			}
+
+			$filePath = "";
+			foreach ($finder as $found) {
+				$filePath = $found->getPathname();
+			}
+			$handle = fopen($filePath, "r");
+			$content = fread($handle, filesize($filePath));
+			fclose($handle);
+
+			return $this->fromFormat( $config['format'], $content );
+
+
+		} catch ( TransportExceptionInterface $e ) {
+			// @todo error.
+		}
 	}
 
 	public function send( array $config, $data )
 	{
-		// TODO: Implement send() method.
+		$ftp_conn = ftp_connect($config['host' ]) or die("Could not connect to ".$config['host' ]);
+		$login = ftp_login($ftp_conn, $config['username' ], $config['password' ]);
+
+		$filecontent = $this->toFormat( $config['format'], $data );
+
+
+		if(empty($config['override']))
+		{
+			$finder = new Finder();
+			$finder->in("ftp://".$config['username' ].":".$config['password' ]."@".$config['host' ].$config['path']);
+
+			$existing_files = [];
+			if ($finder->hasResults()) {
+				foreach ($finder as $found) {
+					array_push($existing_files, $found->getFilename());
+				}
+			}
+
+			$ext = pathinfo($config['filename'], PATHINFO_EXTENSION);
+			$file_basename = basename($config['filename'],".".$ext);
+
+			$x = 1;
+			while($x <= 999) {
+				if(!in_array($file_basename.$x.".".$ext, $existing_files))
+				{
+					$filename = $file_basename.$x.".".$ext;
+					break;
+				}
+				$x++;
+			}
+		}else{
+			$filename = $config['filename'];
+		}
+
+
+
+		$local_file=fopen('php://temp', 'r+');
+		fwrite($local_file, $filecontent);
+		rewind($local_file);
+
+		ftp_chdir($ftp_conn, $config['path']);
+		if($login) $upload_result=ftp_fput($ftp_conn, $filename, $local_file, FTP_BINARY);
+
+		if(!$login or !$upload_result)
+		{
+			echo('FTP error: The file could not be written to the FTP server.');
+		}
+
+		ftp_close($ftp_conn);
 	}
 }
