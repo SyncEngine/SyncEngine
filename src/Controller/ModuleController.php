70106d530fe048b306087debbfdb649e48c8be5a
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-06-19 00:48:09 +0200

    Add file upload route

--- src/Controller/ModuleController.php
+++ src/Controller/ModuleController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Service\ModuleService;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -23,5 +24,28 @@ class ModuleController extends AdminController
 		return $this->render('module/index.html.twig', [
 			'modules' => $modules,
 		]);
+	}
+
+	#[Route('/modules/upload', name: 'module_upload')]
+	public function uploadModule( Request $request ): Response
+	{
+		$form = $this->createFormBuilder([])
+			->add( 'module', FileType::class )
+			->getForm();
+
+		$form->handleRequest( $request );
+
+		if ($form->isSubmitted() && $form->isValid()) {
+			$file = $form['module']->getData();
+			$this->install( $file );
+        }
+
+		return $this->render( 'module/upload.html.twig', [
+			'form' => $form,
+		] );
+	}
+
+	private function install( $file ) {
+
 	}
 }
