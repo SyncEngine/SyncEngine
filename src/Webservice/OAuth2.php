b162a3d07e17fdf9e71f4eddb83e510f006686ee
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-06-19 18:34:54 +0200

    Update OAuth2 fields

--- src/Webservice/OAuth2.php
+++ src/Webservice/OAuth2.php
@@ -38,34 +38,39 @@ class OAuth2 extends WebserviceModel
 					'client_cred' => 'Client Credentials',
 				]
 			],
+			'callback_url' => [
+				'label' => 'Callback/Redirect URL',
+				'type' => 'text',
+				'help' => 'This is the callback URL that you will be redirected to, after your application is authorized. It is used to extract the authorization code or access token. The Callback URL should match the one your use during the application registration process',
+				'conditionals' => [
+					'grant_type' => [ 'auth_code', 'auth_code_pkce', 'implicit' ],
+				]
+			],
 			'auth_url' => [
 				'label' => 'Authorization URL',
 				'type' => 'text',
+				'help' => 'The endpoint for the authorization server. This is used to get the authorization code.',
 				'conditionals' => [
 					'grant_type' => [ 'auth_code', 'auth_code_pkce', 'implicit' ],
 				]
 			],
 			'access_token_url' => [
-				'label' => 'Token URL',
+				'label' => 'Access Token URL',
 				'type' => 'text',
+				'help' => 'The endpoint for the authentication server. This is used to exchange the authorization code for an access token.',
 				'conditionals' => [
 					'grant_type' => [ 'auth_code', 'auth_code_pkce', 'pass_cred', 'client_cred' ],
 				]
 			],
-			'callback_url' => [
-				'label' => 'Callback/Redirect URL',
-				'type' => 'text',
-				'conditionals' => [
-					'grant_type' => [ 'auth_code', 'auth_code_pkce', 'implicit' ],
-				]
-			],
 			'key' => [
 				'label' => 'Client Key (ID)',
+				'help' => 'The client identifier issued to the client during the Application registration process.',
 				'type' => 'text',
 			],
 			'secret' => [
 				'label' => 'Client Secret',
 				'type' => 'text',
+				'help' => 'The client secret issued to the client during the Application registration process.',
 				'conditionals' => [
 					'grant_type' => [ 'auth_code', 'auth_code_pkce', 'pass_cred', 'client_cred' ],
 				]
@@ -88,10 +93,12 @@ class OAuth2 extends WebserviceModel
 				'label' => 'Scope',
 				'type' => 'text',
 				'placeholder' => 'e.g. read:org',
+				'help' => 'The scope of the access request. It may have multiple space-delimited values.',
 			],
 			'state' => [
 				'label' => 'State',
 				'type' => 'text',
+				'help' => 'An opaque value that is used for preventing cross-site request forgery.',
 				'conditionals' => [
 					'grant_type' => [ 'auth_code', 'auth_code_pkce', 'implicit' ],
 				]
@@ -99,6 +106,7 @@ class OAuth2 extends WebserviceModel
 			'client_authentication' => [
 				'label' => 'Client Authentication',
 				'type' => 'select',
+				//'help' => 'The client authentication method will be ignored if you have added a client_id or client_secret in the advanced options.',
 				'choices' => [
 					'basic_auth' => 'Send as Basic Auth Header',
 					'body_cred' => 'Send client credentials in body',
@@ -106,8 +114,9 @@ class OAuth2 extends WebserviceModel
 			],
 			// Advanced.
 			'refresh_token_url' => [
-				'label' => 'Token URL',
+				'label' => 'Refresh Token URL',
 				'type' => 'text',
+				'help' => 'Used to exchange the refresh token for and access token. If empty, the Access Token URL will be used instead.',
 			],
 		];
 	}
