cd49f550335f79b732096dd7c23a419f68a0f6d4
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-06-19 11:37:52 +0200

    Cleanup

--- modules/Capitalization/Capitalization.php
+++ modules/Capitalization/Capitalization.php
@@ -2,15 +2,12 @@
 
 namespace modules\Capitalization;
 
-use App\Component\AutomationContext;
 use App\Model\ModuleModel;
-use App\Model\Trait\Task;
 
 class Capitalization extends ModuleModel
 {
 	public function __construct()
 	{
-		$this->type = 'capitalization';
 		$this->name = 'Capitalization';
 		$this->description = 'Capitalize certain words';
 
