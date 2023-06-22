afea0d7c6a1c6e1f02ad3251826eb3ca06a8e5cc
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-06-22 11:31:40 +0200

    Refactor in separate components + support for object values
    Todo: nest params in case of object values

---
+++ assets/react/components/fields/Params/Col.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+
+import { objectToMappable } from "../../../utils/format";
+
+export default function ParamsCol( props ) {
+	const {
+		value,
+		predefined,
+		nest = false,
+		onChange,
+	} = props;
+
+	// @todo Implement param nesting.
+
+	return (
+		<Col>
+			{ ( predefined && 'object' !== typeof value ) &&
+			  <Form.Select
+				  aria-label=""
+				  value={ value }
+				  onChange={ ( event ) => { onChange( event.target.value ) } }
+			  >
+				  {
+					  objectToMappable( predefined, 'value', 'label' ).map( ( paramTypeKey, keyIndex ) => {
+						  return <option key={ keyIndex } value={ paramTypeKey.value }>{ paramTypeKey.label ?? paramTypeKey.value }</option>
+					  } )
+				  }
+			  </Form.Select>
+			}
+			{ ! predefined &&
+			  <Form.Control
+				  column="text"
+				  value={ ( 'object' === typeof value ) ? JSON.stringify( value ) : value }
+				  onChange={ ( event ) => { onChange( event.target.value ) } }
+			  />
+			}
+		</Col>
+	);
+}
