39b64aa672e1c24b111486d03cebf3da3d7c7e66
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-06-22 12:07:02 +0200

    Basic store task to store and fetch from datasets

---
+++ src/Service/DatasetService.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Service;
+
+use App\Controller\DefaultController;
+use App\Entity\Dataset;
+
+class DatasetService
+{
+	public static function getDataset( int $id ): Dataset
+	{
+		return DefaultController::getEntityManager()->getRepository( Dataset::class )->findOneBy( [ 'id' => $id ] );
+	}
+
+	public static function getDatasets(): array
+	{
+		return DefaultController::getEntityManager()->getRepository( Dataset::class )->findAll();
+	}
+}
