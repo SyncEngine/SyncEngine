f45e51f138eeeeed864a460c3110a3ac164a987a
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-06-22 14:59:46 +0200

    Create model config trait
    To be used as a layer for getting and setting entity configs and fields

---
+++ src/Model/Trait/Config.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Model\Trait;
+
+trait Config
+{
+	protected array $config = [];
+
+	public function getConfig( $key = null, $default = null ): mixed {
+		if ( $key ) {
+			return $this->config[ $key ] ?? $default;
+		}
+
+		return $this->config;
+	}
+
+	public function setConfig( $value, $key = null ): void {
+		if ( $key ) {
+			$this->config[ $key ] = $value;
+		} else {
+			$this->config = $value;
+		}
+
+		if ( isset( $this->entity ) && is_callable( array( $this->entity, 'setConfig' ) ) ) {
+			$this->entity->setConfig( $value );
+		}
+	}
+
+	public function getConfigFields(): array
+	{
+		return [];
+	}
+}
