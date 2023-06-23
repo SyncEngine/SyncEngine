9b4297f39390ffac40bfe4c3d4de078eb498bed6
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-06-23 11:54:49 +0200

    Create context in ApiController

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Component\AutomationContext;
 use App\Entity\Automation;
 use App\Service\AutomationService;
 use Doctrine\ORM\EntityManagerInterface;
@@ -21,17 +22,26 @@ class ApiController extends AbstractController
 	}
 
 	#[Route('/api/{endpoint}', name: 'api_endpoint')]
-	public function endpoint( Automation $automation, Request $request, AutomationService $automationService ): Response
+	public function endpoint( Automation $automation, AutomationService $automationService, Request $request ): Response
 	{
-		$results = $automationService->execute( $automation, $request );
+		// @todo get request data based on config.
+		$data = [];
+		$model = AutomationService::getAutomation( $automation );
+		$context = new AutomationContext( $model );
+
+		$results = $automationService->execute( $model, $context, $data );
 		return $this->json( $results );
 	}
 
 	// @todo Allow in dev only.
 	#[Route('/api/{endpoint}/profiler', name: 'api_endpoint_profiler')]
-	public function endpoint_profiler( Automation $automation, Request $request, AutomationService $automationService ): Response
+	public function endpoint_profiler( Automation $automation, AutomationService $automationService, Request $request ): Response
 	{
-		$results = $automationService->execute( $automation, $request );
+		$data = [];
+		$model = AutomationService::getAutomation( $automation );
+		$context = new AutomationContext( $model );
+
+		$results = $automationService->execute( $model, $context, $data );
 		return $this->render( 'api/endpoint.html.twig', [ 'response' => $results ] );
 	}
 }
