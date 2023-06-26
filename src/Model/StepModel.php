a80884e2166be74c2fb59a4be3462cef67017f26
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-06-26 14:27:37 +0200

    Example of config usage in model (step)

--- src/Model/StepModel.php
+++ src/Model/StepModel.php
@@ -5,6 +5,8 @@ namespace App\Model;
 use App\Entity\Step;
 use App\Model\Trait\Config;
 use App\Model\Trait\Entity;
+use App\Model\TaskModel;
+use App\Service\TaskService;
 
 /**
  * @method int getId()
@@ -23,5 +25,35 @@ class StepModel
 	{
 		$this->entity = $step;
 		$this->config = $step->getConfig();
+	}
+
+	public function getTasks(): array
+	{
+		$tasks = [];
+		foreach ( $this->getConfig( 'tasks' ) ?? [] as $task ) {
+			$tasks = TaskService::getTask( $task['_class'] ?? '' );
+		}
+		return $tasks;
+	}
+
+	public function getTask( $ref ): TaskModel|null
+	{
+		foreach ( $this->getConfig( 'tasks' ) ?? [] as $task ) {
+			if ( $task['_ref'] === $ref ) {
+				return TaskService::getTask( $task['_class'] ?? '' );
+			}
+		}
+		return null;
+	}
+
+	public static function getFields(): array
+	{
+		// @todo Implement fields.
+		return [
+			'tasks' => [
+				'label' => 'Tasks',
+				'type' => 'tasks',
+			]
+		];
 	}
 }
