f5cf2e6f8c5ff6b79cf3a6fc4df81877124d5b8a
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-06-26 15:56:19 +0200

    ucfirst helper function

--- assets/react/utils/globals.jsx
+++ assets/react/utils/globals.jsx
@@ -3,6 +3,11 @@ function createRefId( prefix = '' ) {
 	return Date.now().toString(36) + Math.random().toString(36).substr(2);
 }
 
+function ucfirst( str ) {
+	return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
 export {
 	createRefId,
+	ucfirst,
 }
