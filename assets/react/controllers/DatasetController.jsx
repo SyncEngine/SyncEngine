21779f44f7c1e6c03239dc216f35f62a4944ac22
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-06-26 12:55:57 +0200

    Make data field react on config field changes

--- assets/react/controllers/DatasetController.jsx
+++ assets/react/controllers/DatasetController.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Dataset from "../components/fields/Dataset";
 import FieldGroup from "../components/form/FieldGroup";
+import { publish, subscribe } from "../utils/events";
 
 export default function DatasetController( props ) {
 
 	const {
 		args = {},
 		value = {},
+		element,
 		onChange,
 	} = props;
 
@@ -15,11 +17,32 @@ export default function DatasetController( props ) {
 		fields,
 	} = args;
 
+	const [ config, setConfig ] = useState( {} );
+
+	if ( 'data' === prop ) {
+		subscribe( 'updateConfig', ( e ) => {
+			if ( element.closest( 'form' ).id === e.detail.id ) {
+				setConfig( e.detail.value );
+			}
+		} );
+	}
+
+	const update = ( newValue ) => {
+		onChange( newValue );
+
+		if ( 'config' === prop ) {
+			publish( 'updateConfig', {
+				id: element.closest( 'form' ).id,
+				value: newValue
+			} );
+		}
+	}
+
 	switch ( prop ) {
 		case 'data':
-			return ( <Dataset value={ value } onChange={ onChange } columns={ value.columns ?? [] }></Dataset> );
+			return ( <Dataset value={ value } onChange={ update } columns={ config.columns ?? [] }></Dataset> );
 		case 'config':
 			// Todo
-			return ( <FieldGroup fields={ fields } value={ value } onChange={ onChange } /> )
+			return ( <FieldGroup fields={ fields } value={ value } onChange={ update } /> )
 	}
 }
