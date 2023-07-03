91c64c110bd735805c62ee388374d5bdf332dcb9
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-07-04 01:03:01 +0200

    Simplify item handler

--- assets/react/components/fields/Repeatable/Accordion.jsx
+++ assets/react/components/fields/Repeatable/Accordion.jsx
@@ -6,36 +6,65 @@ import Sortable from "../../services/Sortable";
 export default function RepeatableAccordion( props ) {
 
 	const {
-		items,
 		sortable = true,
 		reorderCallback,
 	} = props;
 
+	const items = props.items.map( ( row, index ) => {
+		const {
+			header,
+			body,
+			actions,
+			value,
+			ref = index,
+		} = row;
+
+		if ( ! sortable ) {
+			return (
+				<Accordion.Item eventKey={ ref } key={ ref }>
+					<Accordion.Header>
+						{ header }
+						{ actions }
+					</Accordion.Header>
+					<Accordion.Body className="bg-body-tertiary p-1">
+						<div className="bg-body p-3">
+							{ body }
+						</div>
+					</Accordion.Body>
+				</Accordion.Item>
+			)
+		}
+
+		return {
+			id: ref,
+			value: value,
+			component: Accordion.Item,
+			attributes: {
+				eventKey: ref,
+			},
+			header: {
+				component: Accordion.Header,
+				children: (
+					<>
+						{ header }
+						{ actions }
+					</>
+				)
+			},
+			body: (
+				<Accordion.Body className="bg-body-tertiary p-1">
+					<div className="bg-body p-3">
+						{ body }
+					</div>
+				</Accordion.Body>
+			),
+		};
+	} );
+
 	if ( ! sortable ) {
 		return (
 			<Accordion>
-				{ items.map( ( row ) => {
-					const {
-						header,
-						actions,
-						body,
-						ref
-					} = row;
-
-					return (
-						<Accordion.Item eventKey={ ref } key={ ref }>
-							<Accordion.Header>
-								{ header }
-								{ actions }
-							</Accordion.Header>
-							<Accordion.Body className="bg-body-tertiary p-1">
-								<div className="bg-body p-3">
-									{ body }
-								</div>
-							</Accordion.Body>
-						</Accordion.Item>
-					)
-				} ) }
+				{ items }
 			</Accordion>
 		)
 	}
@@ -44,40 +73,7 @@ export default function RepeatableAccordion( props ) {
 		<Accordion>
 			<Sortable
 				setItems={ reorderCallback }
-				items={ items.map( ( row ) => {
-					const {
-						header,
-						actions,
-						body,
-						ref,
-						value,
-					} = row;
-
-					return {
-						id: ref,
-						value: value,
-						component: Accordion.Item,
-						attributes: {
-							eventKey: ref,
-						},
-						header: {
-							component: Accordion.Header,
-							children: (
-								<>
-									{ header }
-									{ actions }
-								</>
-							)
-						},
-						body: (
-							<Accordion.Body className="bg-body-tertiary p-1">
-								<div className="bg-body p-3">
-									{ body }
-								</div>
-							</Accordion.Body>
-						),
-					}
-				} ) }
+				items={ items }
 			/>
 		</Accordion>
 	);
--- assets/react/components/fields/Repeatable/List.jsx
+++ assets/react/components/fields/Repeatable/List.jsx
@@ -7,36 +7,55 @@ import SortableIcon from "../../services/Sortable/SortableIcon";
 export default function RepeatableList( props ) {
 
 	const {
-		items,
 		sortable = true,
 		reorderCallback,
 	} = props;
 
+	const items = props.items.map( ( row, index ) => {
+		const {
+			header,
+			body,
+			actions,
+			onClick,
+			value,
+			ref = index,
+		} = row;
+
+		const attributes = onClick && {
+			action: true,
+			onClick: onClick,
+		};
+
+		if ( ! sortable ) {
+			return (
+				<ListGroup.Item key={ ref } action={ 'function' === typeof onClick } onClick={ onClick }>
+					{ header }
+					{ body }
+					{ actions }
+				</ListGroup.Item>
+			)
+		}
+
+		return {
+			id: ref,
+			value: value,
+			component: ListGroup.Item,
+			attributes: attributes ?? {},
+			header: (
+				<Stack direction="horizontal" gap={3}>
+					<SortableIcon></SortableIcon>
+					{ header }
+					{ body }
+					{ actions }
+				</Stack>
+			),
+		};
+	} );
+
 	if ( ! sortable ) {
 		return (
 			<ListGroup>
-				{ items.map( ( row, index ) => {
-					const {
-						header,
-						body,
-						actions,
-						onClick,
-						ref = index,
-					} = row;
-
-					const attributes = onClick && {
-						action: true,
-						onClick: onClick,
-					};
-
-					return (
-						<ListGroup.Item key={ ref } action={ 'function' === typeof onClick } onClick={ onClick }>
-							{ header }
-							{ body }
-							{ actions }
-						</ListGroup.Item>
-					)
-				} ) }
+				{ items }
 			</ListGroup>
 		)
 	}
@@ -45,36 +64,7 @@ export default function RepeatableList( props ) {
 		<ListGroup>
 			<Sortable
 				setItems={ reorderCallback }
-				items={ items.map( ( row ) => {
-					const {
-						header,
-						body,
-						actions,
-						onClick,
-						ref,
-						value,
-					} = row;
-
-					const attributes = onClick && {
-						action: true,
-						onClick: onClick,
-					};
-
-					return {
-						id: ref,
-						value: value,
-						component: ListGroup.Item,
-						attributes: attributes ?? {},
-						header: (
-							<Stack direction="horizontal" gap={3}>
-								<SortableIcon></SortableIcon>
-								{ header }
-								{ body }
-								{ actions }
-							</Stack>
-						),
-					};
-				} ) }
+				items={ items }
 			/>
 		</ListGroup>
 	);
