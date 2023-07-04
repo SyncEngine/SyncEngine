8a260d02d79d1466cb6efd869594d251a28faba6
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-07-04 16:01:22 +0200

    Data trait (similar to config)

---
+++ src/Model/Trait/Data.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Model\Trait;
+
+trait Data
+{
+	protected array $data = [];
+
+	public function getData( $key = null, $default = null ): mixed {
+		if ( $key ) {
+			return $this->data[ $key ] ?? $default;
+		}
+
+		return $this->data;
+	}
+
+	public function setData( $value, $key = null ): void {
+		if ( $key ) {
+			$this->data[ $key ] = $value;
+		} else {
+			$this->data = $value;
+		}
+
+		if ( isset( $this->entity ) && is_callable( array( $this->entity, 'setData' ) ) ) {
+			$this->entity->setData( $value );
+		}
+	}
+
+	public function getDataFields(): array
+	{
+		return [];
+	}
+}
