29dc0f90138b921cab4aa8b32e37e5ff250cee5d
Niek Koonen <info@inform-all.nl>
2023-07-07 15:15:33 +0200

    Module upload function

--- src/Controller/ModuleController.php
+++ src/Controller/ModuleController.php
@@ -8,6 +8,8 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Filesystem\Filesystem;
 
 class ModuleController extends AdminController
 {
@@ -16,8 +18,8 @@ class ModuleController extends AdminController
 	{
 		$modules = ModuleService::getModules();
 
-		foreach ( $modules as $key => $module ) {
-			$modules[ $key ] = [
+		foreach ($modules as $key => $module) {
+			$modules[$key] = [
 				'name' => $module->getName(),
 				'description' => $module->getDescription(),
 			];
@@ -29,36 +31,70 @@ class ModuleController extends AdminController
 	}
 
 	#[Route('/modules/upload', name: 'module_upload')]
-	public function uploadModule( Request $request ): Response
+	public function uploadModule(Request $request): Response
 	{
 		$form = $this->createFormBuilder([])
-			->add( 'module', FileType::class )
-			->add( 'submit', SubmitType::class, ['label' => 'Upload'] )
+			->add('module', FileType::class, [
+				'constraints' => [
+					new File([
+						'mimeTypes' => [
+							'application/zip',
+							'application/octet-stream',
+						],
+						'mimeTypesMessage' => 'Please upload a valid ZIP file',
+					])
+				],
+			])
+			->add('submit', SubmitType::class, ['label' => 'Upload'])
 			->getForm();
 
-		$form->handleRequest( $request );
+		$form->handleRequest($request);
 
 		if ($form->isSubmitted() && $form->isValid()) {
 			$file = $form['module']->getData();
-			$this->install( $file );
-        }
+			$this->install($file);
+			return $this->redirectToRoute('module_upload');
+		}
 
-		return $this->render( 'module/upload.html.twig', [
+		return $this->render('module/upload.html.twig', [
 			'form' => $form,
-		] );
+		]);
 	}
 
 	#[Route('/module/{name}', name: 'module')]
-	public function module( string $name, Request $request ): Response
+	public function module(string $name, Request $request): Response
 	{
-		$module = ModuleService::getModule( $name );
+		$module = ModuleService::getModule($name);
 
-		$response = $module->renderRequest( $request );
+		$response = $module->renderRequest($request);
 
-		return ( $response instanceof Response ) ? $response : $this->redirectToRoute( 'modules' );
+		return ($response instanceof Response) ? $response : $this->redirectToRoute('modules');
 	}
 
-	private function install( $file ) {
+	private function install($file)
+	{
+		try {
+			$file->move(
+				$this->getParameter('modules_directory'),
+				$file->getClientOriginalName()
+			);
+		} catch (FileException $e) {
+			$this->addFlash('warning', 'Cant place file!');
+			return $this->redirectToRoute('module_upload');
+		}
+
+		$zip = new \ZipArchive;
+		if ($zip->open($this->getParameter('modules_directory')."/".$file->getClientOriginalName()) === TRUE) {
+			$zip->extractTo($this->getParameter('modules_directory'));
+			$zip->close();
+			$this->addFlash('success', 'Module succesfully installed');
+		} else {
+			$this->addFlash('warning', 'Cant unzip file!');
+		}
+
+		$filesystem = new Filesystem();
+		$zipfile=$this->getParameter("modules_directory").'/'.$file->getClientOriginalName();
+		$filesystem->remove($zipfile);
 
 	}
 }
