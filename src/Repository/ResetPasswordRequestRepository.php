ddbe6cf78aa3c078ab4b277c6419d24d887bc73d
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-07-10 21:34:36 +0200

    Auto apply codestyle

--- src/Repository/ResetPasswordRequestRepository.php
+++ src/Repository/ResetPasswordRequestRepository.php
@@ -13,40 +13,40 @@ use SymfonyCasts\Bundle\ResetPassword\Persistence\ResetPasswordRequestRepository
 /**
  * @extends ServiceEntityRepository<ResetPasswordRequest>
  *
- * @method ResetPasswordRequest|null find($id, $lockMode = null, $lockVersion = null)
- * @method ResetPasswordRequest|null findOneBy(array $criteria, array $orderBy = null)
+ * @method ResetPasswordRequest|null find( $id, $lockMode = null, $lockVersion = null )
+ * @method ResetPasswordRequest|null findOneBy( array $criteria, array $orderBy = null )
  * @method ResetPasswordRequest[]    findAll()
- * @method ResetPasswordRequest[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ * @method ResetPasswordRequest[]    findBy( array $criteria, array $orderBy = null, $limit = null, $offset = null )
  */
 class ResetPasswordRequestRepository extends ServiceEntityRepository implements ResetPasswordRequestRepositoryInterface
 {
 	use ResetPasswordRequestRepositoryTrait;
 
-	public function __construct(ManagerRegistry $registry)
+	public function __construct( ManagerRegistry $registry )
 	{
-		parent::__construct($registry, ResetPasswordRequest::class);
+		parent::__construct( $registry, ResetPasswordRequest::class );
 	}
 
-	public function save(ResetPasswordRequest $entity, bool $flush = false): void
+	public function save( ResetPasswordRequest $entity, bool $flush = false ): void
 	{
-		$this->getEntityManager()->persist($entity);
+		$this->getEntityManager()->persist( $entity );
 
-		if ($flush) {
+		if ( $flush ) {
 			$this->getEntityManager()->flush();
 		}
 	}
 
-	public function remove(ResetPasswordRequest $entity, bool $flush = false): void
+	public function remove( ResetPasswordRequest $entity, bool $flush = false ): void
 	{
-		$this->getEntityManager()->remove($entity);
+		$this->getEntityManager()->remove( $entity );
 
-		if ($flush) {
+		if ( $flush ) {
 			$this->getEntityManager()->flush();
 		}
 	}
 
-	public function createResetPasswordRequest(object $user, DateTimeInterface $expiresAt, string $selector, string $hashedToken): ResetPasswordRequestInterface
+	public function createResetPasswordRequest( object $user, DateTimeInterface $expiresAt, string $selector, string $hashedToken ): ResetPasswordRequestInterface
 	{
-		return new ResetPasswordRequest($user, $expiresAt, $selector, $hashedToken);
+		return new ResetPasswordRequest( $user, $expiresAt, $selector, $hashedToken );
 	}
 }
--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -13,23 +13,23 @@ use function get_class;
 /**
  * @extends ServiceEntityRepository<User>
  *
- * @method User|null find($id, $lockMode = null, $lockVersion = null)
- * @method User|null findOneBy(array $criteria, array $orderBy = null)
+ * @method User|null find( $id, $lockMode = null, $lockVersion = null )
+ * @method User|null findOneBy( array $criteria, array $orderBy = null )
  * @method User[]    findAll()
- * @method User[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ * @method User[]    findBy( array $criteria, array $orderBy = null, $limit = null, $offset = null )
  */
 class UserRepository extends ServiceEntityRepository implements PasswordUpgraderInterface
 {
-	public function __construct(ManagerRegistry $registry)
+	public function __construct( ManagerRegistry $registry )
 	{
-		parent::__construct($registry, User::class);
+		parent::__construct( $registry, User::class );
 	}
 
-	public function remove(User $entity, bool $flush = false): void
+	public function remove( User $entity, bool $flush = false ): void
 	{
-		$this->getEntityManager()->remove($entity);
+		$this->getEntityManager()->remove( $entity );
 
-		if ($flush) {
+		if ( $flush ) {
 			$this->getEntityManager()->flush();
 		}
 	}
@@ -37,48 +37,48 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
 	/**
 	 * Used to upgrade (rehash) the user's password automatically over time.
 	 */
-	public function upgradePassword(PasswordAuthenticatedUserInterface $user, string $newHashedPassword): void
+	public function upgradePassword( PasswordAuthenticatedUserInterface $user, string $newHashedPassword ): void
 	{
-		if (!$user instanceof User) {
-			throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', get_class($user)));
+		if ( ! $user instanceof User ) {
+			throw new UnsupportedUserException( sprintf( 'Instances of "%s" are not supported.', get_class( $user ) ) );
 		}
 
-		$user->setPassword($newHashedPassword);
+		$user->setPassword( $newHashedPassword );
 
-		$this->save($user, true);
+		$this->save( $user, true );
 	}
 
-	public function save(User $entity, bool $flush = false): void
+	public function save( User $entity, bool $flush = false ): void
 	{
-		$this->getEntityManager()->persist($entity);
+		$this->getEntityManager()->persist( $entity );
 
-		if ($flush) {
+		if ( $flush ) {
 			$this->getEntityManager()->flush();
 		}
 	}
 
-//    /**
-//     * @return User[] Returns an array of User objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('u')
-//            ->andWhere('u.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('u.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+	//    /**
+	//     * @return User[] Returns an array of User objects
+	//     */
+	//    public function findByExampleField($value): array
+	//    {
+	//        return $this->createQueryBuilder('u')
+	//            ->andWhere('u.exampleField = :val')
+	//            ->setParameter('val', $value)
+	//            ->orderBy('u.id', 'ASC')
+	//            ->setMaxResults(10)
+	//            ->getQuery()
+	//            ->getResult()
+	//        ;
+	//    }
 
-//    public function findOneBySomeField($value): ?User
-//    {
-//        return $this->createQueryBuilder('u')
-//            ->andWhere('u.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+	//    public function findOneBySomeField($value): ?User
+	//    {
+	//        return $this->createQueryBuilder('u')
+	//            ->andWhere('u.exampleField = :val')
+	//            ->setParameter('val', $value)
+	//            ->getQuery()
+	//            ->getOneOrNullResult()
+	//        ;
+	//    }
 }
