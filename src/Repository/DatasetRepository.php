970c8c3569afeb76ec5d02daaafbe20a35e791ab
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-07-10 21:12:05 +0200

    DatasetRepository > findByType

--- src/Repository/DatasetRepository.php
+++ src/Repository/DatasetRepository.php
@@ -39,20 +39,19 @@ class DatasetRepository extends ServiceEntityRepository
         }
     }
 
-//    /**
-//     * @return Dataset[] Returns an array of Dataset objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('d')
-//            ->andWhere('d.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('d.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+	/**
+	 * @return Dataset[] Returns an array of Dataset objects
+	 */
+	public function findByType( $value ): array
+	{
+		return $this->createQueryBuilder( 'd' )
+			->andWhere( 'd.type = :val' )
+			->setParameter( 'val', $value )
+			//->orderBy( 'd.id', 'ASC' )
+			//->setMaxResults( 10 )
+			->getQuery()
+			->getResult();
+	}
 
 //    public function findOneBySomeField($value): ?Dataset
 //    {
