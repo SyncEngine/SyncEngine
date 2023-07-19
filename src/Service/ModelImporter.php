af9051b6efc2f5beb5dd4557cdeb8fee6661ef8b
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-07-19 16:25:52 +0200

    Entity importer (not done yet)

---
+++ src/Service/ModelImporter.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Service;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\PropertyAccess\PropertyAccessor;
+
+class ModelImporter
+{
+	private array $done = [];
+	private array $errors = [];
+	private array $data = [];
+	private EntityManagerInterface $em;
+
+	public function __construct(
+		EntityManagerInterface $entityManager
+	) {
+		$this->em = $entityManager;
+	}
+
+	public function import( array $data ): array
+	{
+		$this->done = [];
+		$this->data = $data;
+		foreach ( $data as $ref => $fields ) {
+			if ( isset( $this->done[ $ref ] ) ) {
+				continue;
+			}
+			$this->importRef( $ref, $fields );
+			$this->done[ $ref ] = true;
+		}
+
+		$this->em->flush();
+		return $this->errors;
+	}
+
+	public function importRef( $ref, $fields )
+	{
+		if ( isset( $this->done[ $ref ] ) ) {
+			return $this->done[ $ref ];
+		}
+
+		$entity = $fields['_entity'];
+		if ( ! $entity ) {
+			$this->errors[] = 'Entity not found for: ' . $ref;
+		}
+		unset( $fields['_entity'] );
+
+		$entity = "\\App\\Entity\\" . $entity;
+		$entity = new $entity();
+
+
+		$modelClass = $this->getModelClass( $entity );
+		$model = new $modelClass( $entity );
+
+		$this->done[ $ref ] = $model;
+
+		$queued = [];
+
+		foreach ( $fields as $property => $value ) {
+
+			if ( is_string( $value ) && ! empty( $this->data[ $value ] ) ) {
+				$queued[ $property ] = $value;
+				continue;
+			}
+
+			if ( is_array( $value ) ) {
+				$queued[ $property ] = $value;
+				continue;
+			}
+
+			$setter = 'set' . ucfirst( $property );
+			if ( method_exists( $entity, $setter ) ) {
+				call_user_func( [ $entity, $setter ], $value );
+			}
+		}
+
+		// Make sure that we store the scalar properties first so we can get an ID for this entity.
+		$model->persist( $this->em );
+
+		foreach ( $queued as $property => $value ) {
+
+			if ( is_string( $value ) && ! empty( $this->data[ $value ] ) ) {
+				$model = $this->importRef( $value, $this->data[ $value ] );
+				$value = $model->getId();
+			}
+
+			if ( is_array( $value ) ) {
+				$value = $this->parseConfigFields( $value );
+			}
+
+			$setter = 'set' . ucfirst( $property );
+			if ( method_exists( $entity, $setter ) ) {
+				call_user_func( [ $entity, $setter ], $value );
+			}
+		}
+
+		$model->persist( $this->em );
+
+		return $model;
+	}
+
+	public function parseConfigFields( array $config ): array
+	{
+		foreach ( $config as $key => $value ) {
+			if ( is_string( $key ) ) {
+				if ( isset( $this->data[ $key ] ) ) {
+					$model = $this->importRef( $value, $this->data[ $value ] );
+					$config[ $key ] = $model->getId();
+				}
+			} elseif ( is_array( $value ) ) {
+				$config[ $key ] = $this->parseConfigFields( $value );
+			}
+		}
+		return $config;
+	}
+
+	public function getModelClass( $class ): string
+	{
+		if ( is_object( $class ) ) {
+			$class = ( new \ReflectionClass( $class ) )->getShortName();
+		}
+		return '\\App\\Model\\' . $class . 'Model';
+	}
+}
