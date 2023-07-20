725d219cd89fbc1ee67d4ebf9e4f347f45728bd9
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-07-20 15:13:07 +0200

    Start on list component and move actions

---
+++ assets/react/components/views/List/ListActions.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import {  Button, Stack } from "react-bootstrap";
+import { BiExport, BiPencil, BiTrash } from 'react-icons/bi';
+
+import EntityModal from '../../modals/EntityModal';
+import ExportModal from '../../modals/ExportModal';
+import DeleteModal from '../../modals/DeleteModal';
+import { ucfirst } from '../../../utils/globals';
+
+export default function ListActions( props ) {
+
+	const {
+		actions = {},
+		entity,
+		type,
+		handleEdit,
+		handleDelete,
+		handleExport,
+	} = props;
+
+	return (
+		<Stack direction="horizontal" gap={2}>
+			{
+				actions.map( action => {
+
+					if ( 'string' === typeof action ) {
+						action = {
+							action: action,
+						};
+					}
+
+					if ( ! action.action ) {
+						return;
+					}
+
+					if ( ! action.type ) {
+						action.type = type;
+					}
+
+					action.id = entity.id;
+					action.name = entity.name;
+
+					switch ( action.action ) {
+						case 'edit':
+							action.callback = handleEdit;
+							action.label = <BiPencil/>
+							break;
+
+						case 'export':
+							action.callback = handleExport;
+							return (
+								<ExportModal key={ action.action } entity={ entity } { ...action }><Button variant={ type }><BiExport/></Button></ExportModal>
+							);
+
+						case 'delete':
+							action.callback = handleDelete;
+							return (
+								<DeleteModal key={ action.action } entity={ entity } { ...action }><Button variant={ type }><BiTrash/></Button></DeleteModal>
+							)
+					}
+
+					return (
+						<EntityModal key={ action.action } entity={ entity } { ...action }><Button variant={ type }>{ action.label ?? ucfirst( action.action ) ?? '' }</Button></EntityModal>
+					);
+				} )
+			}
+		</Stack>
+	);
+}
--- assets/react/controllers/ActionsController.jsx
+++ assets/react/controllers/ActionsController.jsx
@@ -1,11 +1,5 @@
 import React from 'react';
-import {  Button, Stack } from "react-bootstrap";
-
-import { ucfirst } from '../utils/globals';
-import EntityModal from '../components/modals/EntityModal';
-import { BiExport, BiPencil, BiTrash } from 'react-icons/bi';
-import ExportModal from '../components/modals/ExportModal';
-import DeleteModal from '../components/modals/DeleteModal';
+import ListActions from '../components/views/List/ListActions';
 
 export default function ActionsController( props ) {
 
@@ -33,51 +27,6 @@ export default function ActionsController( props ) {
 	}
 
 	return (
-		<Stack direction="horizontal" gap={2}>
-			{
-				actions.map( action => {
-
-					if ( 'string' === typeof action ) {
-						action = {
-							action: action,
-						};
-					}
-
-					if ( ! action.action ) {
-						return;
-					}
-
-					if ( ! action.type ) {
-						action.type = type;
-					}
-
-					action.id = entity.id;
-					action.name = entity.name;
-
-					switch ( action.action ) {
-						case 'edit':
-							action.callback = editEntity;
-							action.label = <BiPencil/>
-							break;
-
-						case 'export':
-							action.callback = exportEntity;
-							return (
-								<ExportModal key={ action.action } entity={ entity } { ...action }><Button variant={ type }><BiExport/></Button></ExportModal>
-							);
-
-						case 'delete':
-							action.callback = deleteEntity;
-							return (
-								<DeleteModal key={ action.action } entity={ entity } { ...action }><Button variant={ type }><BiTrash/></Button></DeleteModal>
-							)
-					}
-
-					return (
-						<EntityModal key={ action.action } entity={ entity } { ...action }><Button variant={ type }>{ action.label ?? ucfirst( action.action ) ?? '' }</Button></EntityModal>
-					);
-				} )
-			}
-		</Stack>
+		<ListActions actions={ actions } entity={ entity } type={ type } handleEdit={ editEntity } handleDelete={ deleteEntity } handleExport={ exportEntity } />
 	);
 }
