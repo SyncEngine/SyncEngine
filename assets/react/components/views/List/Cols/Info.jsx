27cc2b4690778e82694041ea44b65c8cc74ce0b0
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-07-20 20:14:55 +0200

    Fix item type

--- assets/react/components/views/List/Cols/Info.jsx
+++ assets/react/components/views/List/Cols/Info.jsx
@@ -3,13 +3,14 @@ import React from 'react';
 export default function Info( props ) {
 	const {
 		item,
+		type,
 	} = props;
 
 	return (
 		<>
 			{ item.name }
 			{ item.type &&
-				<span className={ "badge rounded-pill" + ( item.variant ? "text-bg-" + item.variant : '' ) }>{ item.type }</span>
+				<span className={ "badge rounded-pill ms-2 " + ( type ? "text-bg-" + type : '' ) }>{ item.type }</span>
 			}
 			{ item.description &&
 				<><br /><small>{ item.description }</small></>
