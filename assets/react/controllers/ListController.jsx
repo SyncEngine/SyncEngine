7b36c5aec844a914ef1d930627bf6471424294f0
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-07-20 16:50:27 +0200

    Add item loader

--- assets/react/controllers/ListController.jsx
+++ assets/react/controllers/ListController.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import List from '../components/views/List';
+import { fetchPost } from '../utils/fetch';
 
 export default function ListController( props ) {
 
@@ -9,11 +10,29 @@ export default function ListController( props ) {
 	} = props;
 
 	const {
-		actions = {},
 		columns = {},
-		items = {},
 		type,
+		endpoint = window.app.endpoints.entities[ type ] ?? window.app.baseUrl,
 	} = args;
+
+	const [ items, setItems ] = useState( args.items ?? {} );
+
+	const fetchItems = async ( query = {} ) => {
+		const response = await fetchPost( endpoint, { action: 'list', query: query } );
+
+		if ( response.success ) {
+			return response[ type ] ?? response.data ?? [];
+		}
+		// @todo Error.
+		return [];
+	}
+
+	useEffect( async() => {
+		if ( ! items ) {
+			const result = await fetchItems();
+			setItems( result );
+		}
+	}, [] );
 
 	const editEntity = ( entity ) => {
 		console.log( 'edit' );
