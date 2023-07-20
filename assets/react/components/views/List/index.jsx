c48b04609f1ab0e697b77ac5b4f43473f38b67ef
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-07-20 15:45:52 +0200

    Basic list components

---
+++ assets/react/components/views/List/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Table } from 'react-bootstrap';
+import Row from 'react-bootstrap/Row';
+import { objectToMappable } from '../../../utils/data';
+import ListRow from './Row';
+import ListHead from './Head';
+
+export default function List( props ) {
+
+	const {
+		actions = {},
+		columns = {},
+		items = {},
+		type,
+	} = props;
+
+	return (
+		<Table>
+			<Row>
+				<ListHead columns={ columns } />
+			</Row>
+			{
+				objectToMappable( items, 'key' ).map( ( item, index ) => {
+					return (
+						<ListRow key={ item.key ?? index } columns={ columns } item={ item } />
+					)
+				} )
+			}
+		</Table>
+	);
+}
