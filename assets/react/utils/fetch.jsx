0c57f39983ed7749c155f59d6ace65aea8dd2d4f
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-07-20 20:42:42 +0200

    Use formatter

--- assets/react/utils/fetch.jsx
+++ assets/react/utils/fetch.jsx
@@ -1,19 +1,15 @@
+import { toFormat } from './format';
 
 const fetchPost = async ( url, data, init = {} ) => {
-	const params = new URLSearchParams();
-	for ( const key in data ){
-		params.append( key, data[ key ] );
-	}
-
 	init.method = 'POST';
-	init.body = params;
+	init.body = toFormat( data, 'url' );
 
 	return ( await fetch( url, init ) ).json();
 }
 
 const fetchPostJson = async ( url, data, init = {} ) => {
 	init.method = 'POST';
-	init.body = JSON.stringify( data );
+	init.body = toFormat( data, 'json' );
 	init.headers[ 'Content-Type' ] = 'application/json';
 
 	return ( await fetch( url, init ) ).json();
