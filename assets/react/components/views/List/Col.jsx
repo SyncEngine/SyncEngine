2e45f747481fae848ff2e67a041cfccd3df6aefd
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-07-20 20:11:21 +0200

    Item placeholders

--- assets/react/components/views/List/Col.jsx
+++ assets/react/components/views/List/Col.jsx
@@ -2,15 +2,27 @@ import React from 'react';
 import Actions from './Cols/Actions';
 import Info from './Cols/Info';
 import Ref from './Cols/Ref';
+import { Placeholder } from 'react-bootstrap';
 
 export default function ListCol( props ) {
 	const {
 		column,
+		item,
 		content,
 	} = props;
 
 	let colContent;
 	let classes = '';
+
+	if ( 'placeholder' === item ) {
+		return (
+			<td>
+				<Placeholder animation="glow">
+					<Placeholder xs={12} size="lg" />
+				</Placeholder>
+			</td>
+		)
+	}
 
 	switch ( column.key ?? column.name ?? column ) {
 		case 'ref':
