058104b129303a2f4f00267b49af64d43060c6b6
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-07-20 20:24:15 +0200

    Empty notice

--- assets/react/components/views/List/index.jsx
+++ assets/react/components/views/List/index.jsx
@@ -4,6 +4,7 @@ import { Table, Row } from 'react-bootstrap';
 import { objectToMappable } from '../../../utils/data';
 import ListRow from './Row';
 import ListHead from './Head';
+import { isEmpty } from '../../../utils/conditionals';
 
 export default function List( props ) {
 
@@ -20,12 +21,19 @@ export default function List( props ) {
 		<Table hover className="align-middle">
 			<ListHead columns={ columnMap } />
 			<tbody>
-			{
+			{ ! isEmpty( items ) &&
 				objectToMappable( items, 'key' ).map( ( item, index ) => {
 					return (
 						<ListRow key={ item.key ?? index } type={ type } columns={ columnMap } item={ item } callbacks={ callbacks } />
 					)
 				} )
+			}
+			{ isEmpty( items ) &&
+				<tr>
+					<td colSpan={ columnMap.length } className="text-center">
+						No items found.
+					</td>
+				</tr>
 			}
 			</tbody>
 		</Table>
