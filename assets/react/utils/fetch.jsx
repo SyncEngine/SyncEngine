479c17eefb7890f3048aefd5bd03e0ea24226149
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-07-20 20:55:59 +0200

    Hotfix for query

--- assets/react/utils/fetch.jsx
+++ assets/react/utils/fetch.jsx
@@ -1,15 +1,19 @@
-import { toFormat } from './format';
 
 const fetchPost = async ( url, data, init = {} ) => {
+	const params = new URLSearchParams();
+	for ( const key in data ){
+		params.append( key, data[ key ] );
+	}
+
 	init.method = 'POST';
-	init.body = toFormat( data, 'url' );
+	init.body = params;
 
 	return ( await fetch( url, init ) ).json();
 }
 
 const fetchPostJson = async ( url, data, init = {} ) => {
 	init.method = 'POST';
-	init.body = toFormat( data, 'json' );
+	init.body = JSON.stringify( data );
 	init.headers[ 'Content-Type' ] = 'application/json';
 
 	return ( await fetch( url, init ) ).json();
