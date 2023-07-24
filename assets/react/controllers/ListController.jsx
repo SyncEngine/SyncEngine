6652ffa595971ae4ffc7f6c8a7cfee581c84e089
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-07-24 19:36:23 +0200

    List header

--- assets/react/controllers/ListController.jsx
+++ assets/react/controllers/ListController.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import List from '../components/views/List';
 import useEntities from '../hooks/useEntities';
+import { Button, Card } from 'react-bootstrap';
+import EntityModal from '../components/modals/EntityModal';
 
 export default function ListController( props ) {
 
@@ -10,7 +12,8 @@ export default function ListController( props ) {
 	} = props;
 
 	const {
-		type,
+		type, // @todo support other types like Modules?
+		header = {},
 		columns = {},
 		query = { limit: 10, offset: 0 },
 	} = args;
@@ -25,11 +28,33 @@ export default function ListController( props ) {
 	};
 
 	return (
-		<List
-			callbacks={ callbacks }
-			columns={ columns }
-			items={ items ?? Array( ( query.limit ?? 1 ) ).fill( 'placeholder' ) }
-			type={ type }
-		/>
+		<Card>
+			{ header &&
+				<Card.Header className={ "d-flex justify-content-between align-items-center" + ( ( type ) ? " bg-" + type + "-subtle" : "" ) }>
+					<div className="d-flex align-items-center gap-2">
+						{ header.icon &&
+							<i className={ "d-flex fs-5 " + header.icon }></i>
+						}
+						<span>{ header.title }</span>
+					</div>
+					{ header.actions &&
+						header.actions.map( ( action ) => {
+							switch ( action ) {
+								case 'create':
+									return <EntityModal key={ action } action="create" type={ type } callback={ createCallback }><Button variant={ type }>Create new</Button></EntityModal>
+							}
+						})
+					}
+				</Card.Header>
+			}
+			<Card.Body>
+				<List
+					callbacks={ callbacks }
+					columns={ columns }
+					items={ items ?? Array( ( query.limit ?? 1 ) ).fill( 'placeholder' ) }
+					type={ type }
+				/>
+			</Card.Body>
+		</Card>
 	);
 }
