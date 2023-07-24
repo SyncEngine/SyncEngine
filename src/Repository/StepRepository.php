c50d189862234e8bd5ffe3cd3b3ae3548ce042b8
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-07-24 17:11:17 +0200

    Searchable repositories

--- src/Repository/StepRepository.php
+++ src/Repository/StepRepository.php
@@ -3,6 +3,8 @@
 namespace App\Repository;
 
 use App\Entity\Step;
+use App\Repository\Interface\Searchable;
+use App\Repository\Trait\Search;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -14,8 +16,10 @@ use Doctrine\Persistence\ManagerRegistry;
  * @method Step[]    findAll()
  * @method Step[]    findBy( array $criteria, array $orderBy = null, $limit = null, $offset = null )
  */
-class StepRepository extends ServiceEntityRepository
+class StepRepository extends ServiceEntityRepository implements Searchable
 {
+	use Search;
+
 	public function __construct( ManagerRegistry $registry )
 	{
 		parent::__construct( $registry, Step::class );
---
+++ src/Repository/Trait/Search.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Repository\Trait;
+
+use Doctrine\ORM\QueryBuilder;
+
+trait Search
+{
+	private function createSearchQueryBuilder( array $search, array $criteria, array $orderBy = null, $limit = null, $offset = null ): QueryBuilder
+	{
+		$queryBuilder = $this->createQueryBuilder( 'd' );
+
+		foreach ( $search as $key => $value ) {
+			$queryBuilder->andWhere( 'd.' . $key . ' LIKE :search_' . $key );
+			$queryBuilder->setParameter( 'search_' . $key, '%' . $value . '%' );
+		}
+
+		foreach ( $criteria as $key => $value ) {
+			if ( is_array( $value ) ) {
+				$queryBuilder->andWhere( 'd.' . $key . ' IN (:val_' . $key . ')' );
+			} else {
+				$queryBuilder->andWhere( 'd.' . $key . ' = :val_' . $key );
+			}
+			$queryBuilder->setParameter( 'val_' . $key, $value );
+		}
+
+		foreach ( $orderBy as $sort => $order ) {
+			$queryBuilder->orderBy( $sort, $order );
+		}
+
+		if ( $limit ) {
+			$queryBuilder->setMaxResults( $limit );
+		}
+
+		if ( $offset ) {
+			$queryBuilder->setFirstResult( $offset );
+		}
+
+		return $queryBuilder;
+	}
+
+	public function searchBy( array $search, array $criteria, array $orderBy = null, $limit = null, $offset = null ): array
+	{
+		return $this->createSearchQueryBuilder( $search, $criteria, $orderBy, $limit, $offset )->getQuery()->getResult();
+	}
+
+	public function searchOneBy( array $search, array $criteria, array $orderBy = null, $offset = null ): mixed
+	{
+		return $this->createSearchQueryBuilder( $search, $criteria, $orderBy, 1, $offset )->getQuery()->getOneOrNullResult();
+	}
+}
