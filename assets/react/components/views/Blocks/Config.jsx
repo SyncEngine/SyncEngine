22c77646bc4092ee5e15d929a1b2498712cec5db
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-07-25 15:18:22 +0200

    Improve config type labels

--- assets/react/components/views/Blocks/Config.jsx
+++ assets/react/components/views/Blocks/Config.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { objectToMappable } from '../../../utils/data';
 import { ListGroup } from 'react-bootstrap';
+import { ucfirst } from '../../../utils/globals';
 
 export default function Config( props ) {
 	const {
 		item,
 		prop,
 		type,
+		typeLabel,
 	} = props;
 
 	if ( ! item.config || ! item.config.hasOwnProperty( prop ) ) {
@@ -20,17 +22,20 @@ export default function Config( props ) {
 			{
 				list.map( ( value, index ) => {
 
+					console.log( value );
+
 					const {
 						_class,
-						label,
+						_label,
+						label = _label,
 						name,
 					} = value;
 
 					return (
-						<ListGroup.Item key={ index }>
+						<ListGroup.Item key={ index } className="d-flex justify-content-between align-items-center gap-2">
 							{ label ?? name ?? '--' }
 							{ type &&
-								<span className={ "badge rounded-pill" + ( type && " text-bg-" + type.toLowerCase() ) }>{ type } { _class }</span>
+								<span className={ "badge rounded-pill" + ( type && " text-bg-" + type.toLowerCase() ) }>{ typeLabel ?? type }: { _class }</span>
 							}
 						</ListGroup.Item>
 					);
