c1b47c3a22c502ec9f0923f7072486d6977d700c
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-07-28 17:00:01 +0200

    Should all extend DefaultController

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -12,7 +12,6 @@ use App\Service\Execute;
 use App\Service\ExecutionContext;
 use App\Service\Tasks;
 use App\Service\Webservices;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -5,14 +5,13 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\RegistrationFormType;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 
-class RegistrationController extends AbstractController
+class RegistrationController extends DefaultController
 {
 	#[Route('/register', name: 'app_register')]
 	public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
