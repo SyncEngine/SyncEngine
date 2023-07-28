b046e30a37380b92bed21b8cd9776ab81af2c65b
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-07-28 13:52:49 +0200

    Docblocks & build assets

--- assets/react/components/form/Select/index.jsx
+++ assets/react/components/form/Select/index.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import { InputGroup } from 'react-bootstrap';
-import { debounce } from "lodash";
 import { default as AsyncSelect } from 'react-select/async';
 
 import SelectFilters from './SelectFilters';
 import { FloatingLabel as FloatingLabelSelect } from './FloatingLabel';
 import { objectToMappable, mapGroupBy, mapSortBy, mapFilter, listRenameProp } from '../../../utils/data';
 import { isEmpty } from '../../../utils/conditionals';
+import { debounce } from '../../../utils/events';
+
 
 //import "./styles.scss";
 
