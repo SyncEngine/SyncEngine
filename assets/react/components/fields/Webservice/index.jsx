cc3ceb65373ab19a3e43372a00bba175aa2d7b71
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-07-30 23:55:13 +0200

    Load on init and show loading placeholder
    + format default returns

--- assets/react/components/fields/Webservice/index.jsx
+++ assets/react/components/fields/Webservice/index.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
-import { Alert, Stack } from "react-bootstrap";
+import { Alert, Placeholder, Stack } from 'react-bootstrap';
 import Fields from "../../form/Fields";
 import useWebservices from '../../../hooks/useWebservices';
 
 export default function Webservice( props ) {
 
-	const [ webservices, callbacks ] = useWebservices();
-	const webservice = callbacks.get( props.webservice ? ( props.webservice._class ?? props.webservice ) : '' );
+	const [ webservices ] = useWebservices( null, true );
+	const webservice = ( webservices && props.webservice ) ? ( webservices[ props.webservice._class ?? props.webservice ] ?? null ) : null;
+
+	if ( ! webservices ) {
+		return (
+			<Placeholder animation="glow">
+				<Placeholder xs={12} size="lg" />
+			</Placeholder>
+		);
+	}
 
 	if ( ! webservice ) {
-		return (
-			<Alert variant="danger">Webservice not found</Alert>
-		)
+		return <Alert variant="danger">Webservice not found</Alert>
 	}
 
 	if ( ! webservice.hasOwnProperty( 'fields' ) ) {
-		return (
-			<Alert variant="warning">Task has no fields</Alert>
-		)
+		return <Alert variant="warning">Webservice has no fields</Alert>
 	}
 
 	return (
