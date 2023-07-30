b90be49bcfb69b5e0cd2dd184e7b54a2fb07d4ea
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-07-31 01:26:11 +0200

    Collapsible container

--- assets/react/components/form/Field/Container.jsx
+++ assets/react/components/form/Field/Container.jsx
@@ -1,24 +1,40 @@
-import React from "react";
-import { Card } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Card, Collapse } from 'react-bootstrap';
+import { createRefId } from '../../../utils/globals';
 
 export default function FieldContainer( {
+	id,
 	label,
 	help,
 	description,
+	collapsed,
 	children,
 } ) {
+
+	const [ open, setOpen ] = useState( ! label ? true : ! collapsed );
+
+	if ( ! id ) {
+		id = createRefId();
+	}
 
 	return (
 		<Card className="shadow-none border-input">
 			{ label &&
-				<Card.Header className="bg-body">
+				<Card.Header
+					className="bg-body btn d-flex border-bottom-0"
+					onClick={ () => { setOpen( ! open ) } }
+					aria-controls={ id }
+					aria-expanded={ open }
+				>
 					<span>{ label }</span>{ help }
 				</Card.Header>
 			}
-			<Card.Body>
-				{ description }
-				{ children }
-			</Card.Body>
+			<Collapse in={ open } dimension="height">
+				<Card.Body id={ id }>
+					{ description }
+					{ children }
+				</Card.Body>
+			</Collapse>
 		</Card>
 	);
 }
