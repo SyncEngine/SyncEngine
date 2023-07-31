7e9a8ec8e32c9ee5b96518617174a650205c9197
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-07-31 16:19:27 +0200

    Move slug handler to service and model

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -39,31 +39,4 @@ class DefaultController extends AbstractController
 	{
 		return parent::json( ( new ModelNormalizer() )->normalize( $data ), $status, $headers, $context );
 	}
-
-	public function slugify( $text )
-	{
-		// replace non letter or digits by -
-		$text = preg_replace( '~[^\pL\d]+~u', '-', $text );
-
-		// transliterate
-		$text = iconv( 'utf-8', 'us-ascii//TRANSLIT', $text );
-
-		// remove unwanted characters
-		$text = preg_replace( '~[^-\w]+~', '', $text );
-
-		// trim
-		$text = trim( $text, '-' );
-
-		// remove duplicated - symbols
-		$text = preg_replace( '~-+~', '-', $text );
-
-		// lowercase
-		$text = strtolower( $text );
-
-		if ( empty( $text ) ) {
-			return 'n-a';
-		}
-
-		return $text;
-	}
 }
