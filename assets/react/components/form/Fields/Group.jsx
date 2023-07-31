3f27a9431f61e99658a61f7575a6d19794ac1996
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-07-31 14:41:30 +0200

    Separate fieldsitem component for performance
    Improve performance since we can now use hooks

--- assets/react/components/form/Fields/Group.jsx
+++ assets/react/components/form/Fields/Group.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import { Alert, Stack, Card } from "react-bootstrap";
-
-import Field from "../../form/Field";
-import Fields from "./index";
-import TabGroup from "./TabGroup";
+import { Alert, Stack } from "react-bootstrap";
 
 import { validate } from "../../../utils/conditionals";
 import { objectToMappable } from "../../../utils/data";
-import { createRefId } from "../../../utils/globals";
+import FieldsItem from './Item';
+import { createRefId } from '../../../utils/globals';
 
 export default function Group( props ) {
 
@@ -29,68 +26,9 @@ export default function Group( props ) {
 		);
 	} ) ).map( ( field, index ) => {
 
-		field = { ...field }; // Remove reference.
-		field.id = field.id ?? createRefId() + index;
+		field.id = field.id ?? createRefId();
 
-		let subComponents = null;
-		if ( 'object' === typeof field.tabs ) {
-			subComponents = (
-				<TabGroup tabs={ field.tabs } updateField={ updateField } values={ values } />
-			);
-		} else if ( 'object' === typeof field.fields ) {
-			subComponents = (
-				<Group fields={ field.fields } updateField={ updateField } values={ values } />
-			);
-		} else if ( 'object' === typeof field.nested ) {
-			subComponents = (
-				<Fields fields={ field.nested } value={ values[ field.name ] } onChange={ ( value ) => { updateField( value, field.name, field ) } } />
-			);
-		}
-
-		let fieldComponent = null;
-		if ( field.type ) {
-			fieldComponent = (
-				<Field { ...field } value={ values[ field.name ] ?? field.default } values={ values } onChange={ ( value, name, child ) => { updateField( value, name ?? field.name, child ?? field ) } } />
-			)
-		}
-
-		let items = null;
-		if ( subComponents ) {
-			if ( ! fieldComponent && ! field.label ) {
-				items = subComponents;
-			} else if ( fieldComponent ) {
-				const isSwitch = -1 < [ 'checkbox', 'switch' ].indexOf( field.type );
-				items =
-					<Card className="bg-body border-0">
-						<Card.Header className={ "bg-body " + ( isSwitch ? 'p-3 border' : ' p-0 border-0' ) }>
-							{ fieldComponent }
-						</Card.Header>
-						<Card.Body className="border p-3">
-							{ subComponents }
-						</Card.Body>
-					</Card>;
-			} else {
-				items =
-					<Card className="bg-body p-3">
-						{ ! fieldComponent &&
-							<Field { ...field } type="title" />
-						}
-						{ subComponents }
-					</Card>;
-			}
-		} else if ( fieldComponent ) {
-			items = fieldComponent;
-		}
-
-		if ( ! items ) {
-			return;
-		}
-
-		return (
-			<Stack key={ index } gap={ 0 }>
-				{ items }
-			</Stack>
-		)
+		return <FieldsItem key={ field.id } updateField={ updateField } field={ field } values={ values } />;
 
 	} ).filter( ( elem ) => React.isValidElement( elem ) );
 
---
+++ assets/react/components/form/Fields/Item.jsx
@@ -0,0 +1,84 @@
+import React, { useCallback } from 'react';
+import { Stack, Card } from "react-bootstrap";
+
+import Field from "../../form/Field";
+import Fields from "./index";
+import Group from './Group';
+import TabGroup from "./TabGroup";
+
+export default function FieldsItem( props ) {
+
+	const {
+		field,
+		values,
+		updateField,
+	} = props;
+
+	const updateNested = useCallback( ( value ) => {
+		updateField( value, field.name, field )
+	}, [ field ] );
+
+	const update = useCallback( ( value, name, child ) => {
+		updateField( value, name ?? field.name, child ?? field )
+	}, [ field ] );
+
+	let subComponents = null;
+	if ( 'object' === typeof field.tabs ) {
+		subComponents = (
+			<TabGroup tabs={ field.tabs } updateField={ updateField } values={ values } />
+		);
+	} else if ( 'object' === typeof field.fields ) {
+		subComponents = (
+			<Group fields={ field.fields } updateField={ updateField } values={ values } />
+		);
+	} else if ( 'object' === typeof field.nested ) {
+		subComponents = (
+			<Fields fields={ field.nested } value={ values[ field.name ] } onChange={ updateNested } />
+		);
+	}
+
+	let fieldComponent = null;
+	if ( field.type ) {
+		fieldComponent = (
+			<Field { ...field } value={ values[ field.name ] ?? field.default } values={ values } onChange={ update } />
+		)
+	}
+
+	let items = null;
+	if ( subComponents ) {
+		if ( ! fieldComponent && ! field.label ) {
+			items = subComponents;
+		} else if ( fieldComponent ) {
+			const isSwitch = -1 < [ 'checkbox', 'switch' ].indexOf( field.type );
+			items =
+				<Card className="bg-body border-0">
+					<Card.Header className={ "bg-body " + ( isSwitch ? 'p-3 border' : ' p-0 border-0' ) }>
+						{ fieldComponent }
+					</Card.Header>
+					<Card.Body className="border p-3">
+						{ subComponents }
+					</Card.Body>
+				</Card>;
+		} else {
+			items =
+				<Card className="bg-body p-3">
+					{ ! fieldComponent &&
+					  <Field { ...field } type="title" />
+					}
+					{ subComponents }
+				</Card>;
+		}
+	} else if ( fieldComponent ) {
+		items = fieldComponent;
+	}
+
+	if ( ! items ) {
+		return;
+	}
+
+	return (
+		<Stack gap={ 0 }>
+			{ items }
+		</Stack>
+	)
+}
