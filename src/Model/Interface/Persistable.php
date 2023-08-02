f5639a5442e085503941e085841670719202c29d
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-08-02 17:47:02 +0200

    getEntity is required for persistable

--- src/Model/Interface/Persistable.php
+++ src/Model/Interface/Persistable.php
@@ -6,6 +6,7 @@ use Doctrine\ORM\EntityManagerInterface;
 
 interface Persistable
 {
+	public function getEntity(): object;
 	public function update( EntityManagerInterface $entityManager, $flush = false ): void;
 	public function persist( EntityManagerInterface $entityManager, $flush = false ): void;
 }
