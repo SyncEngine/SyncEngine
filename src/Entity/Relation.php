9d92fc4f71c05b91e5236230a3ad44ed6824e03a
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-08-02 18:30:46 +0200

    No need for Turbo support

--- src/Entity/Relation.php
+++ src/Entity/Relation.php
@@ -7,7 +7,6 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\UX\Turbo\Attribute\Broadcast;
 
 #[ORM\Entity(repositoryClass: RelationRepository::class)]
-#[Broadcast]
 class Relation
 {
     #[ORM\Id]
