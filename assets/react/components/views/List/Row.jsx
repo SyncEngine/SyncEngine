528889f00ceec949dc21e4654261565c2d348415
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-08-03 13:50:42 +0200

    Make wrapping optional

--- assets/react/components/views/List/Row.jsx
+++ assets/react/components/views/List/Row.jsx
@@ -8,26 +8,29 @@ export default function ListRow( {
 	type,
 	columns,
 	callbacks,
+	wrap = true,
 } ) {
-	return (
-		<ListGroupItem>
-			{ columns &&
-				objectToMappable( columns, 'key', 'label' ).map( ( column, index ) => {
-					const columnName = column.key ?? column.name ?? '';
-					const content = ( item.hasOwnProperty( columnName ) ) ? item[ columnName ] : '';
 
-					return (
-						<ListCol
-							key={ item.key ?? index }
-							column={ column }
-							type={ type }
-							item={ item.entity ?? item }
-							content={ content }
-							callbacks={ callbacks }
-						/>
-					)
-				} )
-			}
-		</ListGroupItem>
-	);
+	let row = columns &&
+		objectToMappable( columns, 'key', 'label' ).map( ( column, index ) => {
+			const columnName = column.key ?? column.name ?? '';
+			const content = ( item.hasOwnProperty( columnName ) ) ? item[ columnName ] : '';
+
+			return (
+				<ListCol
+					key={ item.key ?? index }
+					column={ column }
+					type={ type }
+					item={ item.entity ?? item }
+					content={ content }
+					callbacks={ callbacks }
+				/>
+			)
+		} );
+
+	if ( wrap ) {
+		row = <ListGroupItem>{ row }</ListGroupItem>
+	}
+
+	return row;
 }
