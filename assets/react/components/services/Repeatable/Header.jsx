1858f7d5be8f3fd4b1d1082f456458fcbc5f9f73
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-08-03 14:25:12 +0200

    Option to use columns in header

--- assets/react/components/services/Repeatable/Header.jsx
+++ assets/react/components/services/Repeatable/Header.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { Stack } from "react-bootstrap";
 import Info from '../../views/Blocks/Info';
+import ListRow from '../../views/List/Row';
 
 export default function Header( props ) {
 
-	const {
-		label,
-		description,
-	} = props;
+	const classes = 'justify-content-center ' + ( ( props._disabled ?? false ) ? 'opacity-50' : '' );
+
+	if ( props.columns ) {
+		return (
+			<Stack direction="horizontal" className={ classes }>
+				<ListRow { ...props } />
+			</Stack>
+		)
+	}
 
 	return (
-		<Info
-			className={ 'justify-content-center ' + ( ( props._disabled ?? false ) ? 'opacity-50' : '' ) }
-			item={ props.item ?? { label: label, description: description } }
-		/>
+		<Info className={ classes } item={ props.item ?? { ...props } } />
 	);
 }
