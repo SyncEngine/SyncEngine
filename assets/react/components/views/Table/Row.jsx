0b5367af3d408c0358e0a635e6f61053790c3bec
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-08-03 13:50:42 +0200

    Make wrapping optional

--- assets/react/components/views/Table/Row.jsx
+++ assets/react/components/views/Table/Row.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Row from 'react-bootstrap/Row';
 import TableCol from "./Col";
 import { objectToMappable } from '../../../utils/data';
 
@@ -8,26 +7,28 @@ export default function TableRow( {
 	type,
 	columns,
 	callbacks,
+	wrap = true,
 } ) {
-	return (
-		<tr>
-			{
-				objectToMappable( columns, 'key' ).map( ( column, index ) => {
-					const columnName = column.key ?? column.name ?? '';
-					const content = ( item.hasOwnProperty( columnName ) ) ? item[ columnName ] : '';
 
-					return (
-						<TableCol
-							key={ item.key ?? index }
-							column={ column }
-							type={ type }
-							item={ item.entity ?? item }
-							content={ content }
-							callbacks={ callbacks }
-						/>
-					)
-				} )
-			}
-		</tr>
-	);
+	let row = objectToMappable( columns, 'key' ).map( ( column, index ) => {
+		const columnName = column.key ?? column.name ?? '';
+		const content = ( item.hasOwnProperty( columnName ) ) ? item[ columnName ] : '';
+
+		return (
+			<TableCol
+				key={ item.key ?? index }
+				column={ column }
+				type={ type }
+				item={ item.entity ?? item }
+				content={ content }
+				callbacks={ callbacks }
+			/>
+		)
+	} )
+
+	if ( wrap ) {
+		row = <tr>{ row }</tr>;
+	}
+
+	return row;
 }
