f873aaaa3c9ec453dceaf570721422753cfeb649
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-08-03 16:26:53 +0200

    Flow fields config

--- src/Form/FlowFormType.php
+++ src/Form/FlowFormType.php
@@ -3,6 +3,8 @@
 namespace App\Form;
 
 use App\Entity\Flow;
+use App\Form\Type\JsonType;
+use App\Model\FlowModel;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -23,6 +25,18 @@ class FlowFormType extends AbstractType
 				'row_attr' => [
 					'class' => 'form-floating mb-3',
 				],
+			] )
+			->add( 'config', JsonType::class, [
+				'row_attr' => [
+					'class' => 'form-floating mb-3',
+				],
+				'attr' => [
+					'data-controller' => 'react',
+					'data-type'       => 'config',
+					'data-args'       => json_encode([
+						'fields' => FlowModel::getFields()
+					]),
+				]
 			] );
 	}
 
