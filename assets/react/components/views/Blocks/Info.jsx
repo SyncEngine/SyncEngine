b9663da63266b9323e08632d0dbdcd01fbdff2ec
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-08-03 15:57:37 +0200

    Option to provide badge text with sprintf support for item

--- assets/react/components/views/Blocks/Info.jsx
+++ assets/react/components/views/Blocks/Info.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Stack } from 'react-bootstrap';
+import { sprintf } from '../../../utils/globals';
 
 export default function Info( props ) {
 	const {
 		item,
 		type,
+		badge,
 	} = props;
 
 	const classes = props.className ?? 'justify-content-center';
@@ -12,9 +14,9 @@ export default function Info( props ) {
 	return (
 		<Stack className={ classes }>
 			<span>
-				{ item.label ?? item.name }
-				{ item.type &&
-					<span className={ "badge rounded-pill ms-2 " + ( type ? "text-bg-" + type : '' ) }>{ item.type }</span>
+				{ item.label ?? item.name ?? '--' }
+				{ ( badge ?? item.type ) &&
+					<span className={ "badge rounded-pill ms-2 " + ( type ? "text-bg-" + type : '' ) }>{ badge ? sprintf( badge, item ) : item.type }</span>
 				}
 				{ item._class &&
 					<span className={ "badge rounded-pill ms-2 " + ( type ? "text-bg-" + type : '' ) }>{ item._class }</span>
