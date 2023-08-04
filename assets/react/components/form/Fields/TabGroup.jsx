441eac34cd741de56317a3fef26f427e704b223b
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-08-04 10:28:10 +0200

    Allow fields to be tabs without wrappers

--- assets/react/components/form/Fields/TabGroup.jsx
+++ assets/react/components/form/Fields/TabGroup.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Alert, Tabs, Tab } from "react-bootstrap";
 import Group from "./Group";
-import { objectToMappable } from "../../../utils/data";
 import Fields from "./index";
+import Field from '../Field';
+import { objectToMappable } from "../../../utils/data";
 
 export default function TabGroup( props ) {
 
@@ -31,6 +32,9 @@ export default function TabGroup( props ) {
 						<Tab key={ name } eventKey={ name } title={ label } className="p-3 border bg-body">
 							{ tab.description &&
 								<p className="form-text text-muted">{ tab.description }</p>
+							}
+							{ tab.type &&
+								<Field { ...tab } wrap={ false } value={ values[ tab.name ] ?? {} } onChange={ ( value ) => { updateField( value, tab.name ) } } />
 							}
 							{ tab.fields &&
 								<Group fields={ tab.fields } updateField={ updateField } values={ values }></Group>
