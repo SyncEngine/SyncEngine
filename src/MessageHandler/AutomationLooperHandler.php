446ea5ebe8220f90c22d859c4a5d6cfde58db219
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-08-08 11:06:19 +0200

    Do not use API controller

--- src/MessageHandler/AutomationLooperHandler.php
+++ src/MessageHandler/AutomationLooperHandler.php
@@ -4,19 +4,25 @@ namespace App\MessageHandler;
 
 use App\Controller\ApiController;
 use App\Message\AutomationLooper;
+use App\Model\AutomationModel;
 use App\Repository\AutomationRepository;
 use App\Service\Execute;
+use App\Service\ExecutionContext;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
 #[AsMessageHandler]
 class AutomationLooperHandler
 {
-	public function __construct( private AutomationRepository $automationRepository, private ApiController $apiController, private Execute $execute ) {}
+	public function __construct( private Execute $execute ) {}
 
 	public function __invoke( AutomationLooper $message ): void
 	{
-		$automation = $this->automationRepository->find( $message->getAutomationID() );
+		$model = AutomationModel::get( $message->getAutomationId() );
 
-		$this->apiController->endpoint( $automation, $this->execute );
+		// @todo Provide context about previous loop?
+		$context = new ExecutionContext( $model, $this->execute );
+
+		// @todo provide request of previous loop?
+		$this->execute->execute( $model, $context, [] );
 	}
 }
