e077770d53f354a93b57d2d709191f8c1d026e70
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-08-08 11:16:15 +0200

    We require the default controller

--- src/MessageHandler/AutomationLooperHandler.php
+++ src/MessageHandler/AutomationLooperHandler.php
@@ -3,6 +3,7 @@
 namespace App\MessageHandler;
 
 use App\Controller\ApiController;
+use App\Controller\DefaultController;
 use App\Message\AutomationLooper;
 use App\Model\AutomationModel;
 use App\Repository\AutomationRepository;
@@ -13,7 +14,7 @@ use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 #[AsMessageHandler]
 class AutomationLooperHandler
 {
-	public function __construct( private Execute $execute ) {}
+	public function __construct( private readonly Execute $execute, private readonly DefaultController $controller ) {}
 
 	public function __invoke( AutomationLooper $message ): void
 	{
