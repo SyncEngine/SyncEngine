7f1b4a67494c27d53ee07beb5595742b39df1b6e
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-08-21 15:46:56 +0200

    Environment service

---
+++ src/Service/Env.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Service;
+
+class Env
+{
+	private string $file;
+	private array $vars = [];
+
+	public function __construct(
+		private readonly string $rootDir,
+		public readonly string $type = ''
+	) {
+		$this->file = $this->rootDir . '/' . ( $type ) ? '.env.' . $type : '.env';
+	}
+
+	public function get( $name ): mixed
+	{
+		if ( ! $this->vars ) {
+			$this->vars = $this->read();
+		}
+
+		return $this->vars[ $name ] ?? null;
+	}
+
+	public function set( $name, $value ): void
+	{
+		$this->vars[ $name ] = $value;
+	}
+
+	public function persist( bool $replace = false ): void
+	{
+		if ( ! $replace ) {
+			$this->vars = array_merge( $this->read(), $this->vars );
+		}
+
+		$this->write( $this->vars );
+	}
+
+	private function read(): array
+	{
+		if ( file_exists( $this->file ) ) {
+			$env = parse_ini_file( $this->file );
+			if ( $env ) {
+				return $env;
+			}
+		}
+
+		return [];
+	}
+
+	private function write( array $vars = [], ): void
+	{
+		ksort( $vars );
+
+		foreach ( $vars as $key => $value ) {
+			$vars[ $key ] = $key . '=' . ( is_numeric( $value ) ) ? $value : '"' . $value . '"';
+		}
+
+		$vars = implode( PHP_EOL, $vars );
+
+		file_put_contents( $this->file, $vars );
+	}
+}
