2b802febebeaac70dd3f814e6023f6415d4936e5
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-08-21 15:47:47 +0200

    Env route and improvements

--- src/Controller/SystemController.php
+++ src/Controller/SystemController.php
@@ -5,17 +5,37 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class SystemController extends AdminController
 {
 	#[Route( '/system', name: 'system_index' )]
-	public function system_index( Request $request ): Response
+	public function system_index( Request $request, TranslatorInterface $translator ): Response
 	{
 		return $this->render( 'admin/system/index.html.twig', [
+			'header' => 'System',
 			'breadcrumbs' => [
 				[
-					'link'  => $this->generateUrl( 'import_entities' ),
-					'title' => 'Import',
+					'title' => 'System',
+					'current' => true,
+				],
+			],
+		] );
+	}
+
+	#[Route( '/system/env', name: 'system_env' )]
+	public function system_env( Request $request, TranslatorInterface $translator ): Response
+	{
+		return $this->render( 'admin/system/index.html.twig', [
+			'header' => 'Environment',
+			'breadcrumbs' => [
+				[
+					'link'  => $this->generateUrl( 'system_index' ),
+					'title' => 'System',
+				],
+				[
+					'title' => 'Environment',
+					'current' => true,
 				],
 			],
 		] );
