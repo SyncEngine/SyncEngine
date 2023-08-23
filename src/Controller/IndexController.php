348845ccd3e5552c44b456fe0b2af44d7de603c2
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-08-23 14:15:00 +0200

    Set to local env + Codestyle

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -14,25 +14,26 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends DefaultController
 {
-	#[Route('/', name: 'app_index')]
-	public function index(EntityManagerInterface $entityManager, Env $env ): Response
+	#[Route( '/', name: 'app_index' )]
+	public function index( EntityManagerInterface $entityManager, Env $env ): Response
 	{
+		$env->setType( 'local' );
 		if ( ! $env->get( 'DATABASE_URL' ) ) {
 			$this->redirectToRoute( 'install_index' );
 		}
 
-		$connections = $entityManager->getRepository(Connection::class)->findAll();
-		$automations = $entityManager->getRepository(Automation::class)->findAll();
-		$flows = $entityManager->getRepository(Flow::class)->findAll();
-		$steps = $entityManager->getRepository(Step::class)->findAll();
-		$datasets = $entityManager->getRepository(Dataset::class)->findAll();
+		$connections = $entityManager->getRepository( Connection::class )->findAll();
+		$automations = $entityManager->getRepository( Automation::class )->findAll();
+		$flows       = $entityManager->getRepository( Flow::class )->findAll();
+		$steps       = $entityManager->getRepository( Step::class )->findAll();
+		$datasets    = $entityManager->getRepository( Dataset::class )->findAll();
 
-		return $this->render('admin/dashboard.html.twig', [
+		return $this->render( 'admin/dashboard.html.twig', [
 			'connections' => $connections,
 			'automations' => $automations,
-			'flows' => $flows,
-			'steps' => $steps,
-			'datasets' => $datasets,
-		]);
+			'flows'       => $flows,
+			'steps'       => $steps,
+			'datasets'    => $datasets,
+		] );
 	}
 }
