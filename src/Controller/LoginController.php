29ee29433f06c7cc02ce0d5f9796cb018422bd09
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-08-23 14:12:32 +0200

    Codestyle

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -8,15 +8,15 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends DefaultController
 {
-	#[Route('/login', name: 'app_login')]
-	public function index(AuthenticationUtils $authenticationUtils): Response
+	#[Route( '/login', name: 'app_login' )]
+	public function index( AuthenticationUtils $authenticationUtils ): Response
 	{
-		$error = $authenticationUtils->getLastAuthenticationError();
+		$error        = $authenticationUtils->getLastAuthenticationError();
 		$lastUsername = $authenticationUtils->getLastUsername();
 
-		return $this->render('login/index.html.twig', [
+		return $this->render( 'login/index.html.twig', [
 			'last_username' => $lastUsername,
-			'error' => $error,
-		]);
+			'error'         => $error,
+		] );
 	}
 }
