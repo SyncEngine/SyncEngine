d357e6d70a62022dba64c9b8afba45b45c5c37dc
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-08-23 14:12:48 +0200

    Redirect to install script if not database URL is found

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -7,6 +7,7 @@ use App\Entity\Connection;
 use App\Entity\Flow;
 use App\Entity\Step;
 use App\Entity\Dataset;
+use App\Service\Env;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,8 +15,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class IndexController extends DefaultController
 {
 	#[Route('/', name: 'app_index')]
-	public function index(EntityManagerInterface $entityManager): Response
+	public function index(EntityManagerInterface $entityManager, Env $env ): Response
 	{
+		if ( ! $env->get( 'DATABASE_URL' ) ) {
+			$this->redirectToRoute( 'install_index' );
+		}
+
 		$connections = $entityManager->getRepository(Connection::class)->findAll();
 		$automations = $entityManager->getRepository(Automation::class)->findAll();
 		$flows = $entityManager->getRepository(Flow::class)->findAll();
