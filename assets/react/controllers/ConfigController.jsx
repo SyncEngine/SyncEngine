2f9dd22a6804559a5fe9e4a0c8b0aef8ca6f72d8
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-09-11 16:04:08 +0200

    Parse tags based on form element and provide it as context

--- assets/react/controllers/ConfigController.jsx
+++ assets/react/controllers/ConfigController.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { Stack } from "react-bootstrap";
 import Fields from "../components/form/Fields";
 import { publish, subscribe, unsubscribe } from '../utils/events';
+import { TagsContext } from '../context/TagsContext';
 
 export default function ConfigController( props ) {
 
@@ -15,6 +16,25 @@ export default function ConfigController( props ) {
 	const {
 		fields,
 	} = args;
+
+	const form = element && element.closest( 'form' );
+
+	const parseTags = ( tags ) => {
+		for ( const tag in tags ) {
+			if ( tags.hasOwnProperty( tag ) ) {
+				if ( 'object' === typeof tags[ tag ] ) {
+					tags[ tag ] = parseTags( tags[ tag ] );
+					continue;
+				}
+				switch ( tags[ tag ] ) {
+					case '_entity':
+						tags[ tag ] = ( form && form.dataset.entity ) ? JSON.parse( form.dataset.entity ) : '';
+						break;
+				}
+			}
+		}
+		return tags;
+	}
 
 	const update = ( newValue ) => {
 		onChange( newValue );
@@ -42,8 +62,10 @@ export default function ConfigController( props ) {
 	}, [ element, value ] );
 
 	return (
-		<Stack className="mt-2">
-			<Fields fields={ fields } value={ { ...value } } onChange={ update } />
-		</Stack>
+		<TagsContext.Provider value={ parseTags( args.tags ?? {} ) }>
+			<Stack className="mt-2">
+				<Fields fields={ fields } value={ { ...value } } onChange={ update } />
+			</Stack>
+		</TagsContext.Provider>
 	)
 }
