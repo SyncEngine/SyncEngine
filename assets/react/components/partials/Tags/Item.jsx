48572ba8fa2fa251b047703932a70816b5777986
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-09-12 16:34:43 +0200

    Option to add placeholder for tag name

--- assets/react/components/partials/Tags/Item.jsx
+++ assets/react/components/partials/Tags/Item.jsx
@@ -23,12 +23,13 @@ export default function TagsItem( props ) {
 
 	const tag = parent ? parent + separator + props.tag : props.tag;
 	const hasChildren = 'object' === typeof props.children && ! isEmpty( props.children );
+	const inputPlaceholder = ( 'object' !== typeof props.children && ! isEmpty( props.children ) ) ? '.' + props.children : '';
 	const collapseId = tag.replaceAll( '.', '-' ) + '_collapse';
 
 	return (
 		<ListGroup.Item className="p-0 ps-1">
 			<div className="d-flex justify-content-between align-items-center ps-1">
-				<Button variant="link" className={ 'text-decoration-none p-0 py-1' + ( parent ? ' pe-3' : '' ) } onClick={ () => onClick( tag ) }>{ startChar + tag + endChar }</Button>
+				<Button variant="link" className={ 'text-decoration-none p-0 py-1' + ( parent ? ' pe-3' : '' ) } onClick={ () => onClick( tag + inputPlaceholder ) }>{ startChar + tag + inputPlaceholder + endChar }</Button>
 				{ hasChildren &&
 				  <Button variant="link" className={ "p-0 px-1 ms-2 bi bi-" + ( open ? "chevron-up" : "chevron-down" ) } onClick={ toggleChildren } aria-controls={ collapseId } aria-expanded={ open }></Button>
 				}
