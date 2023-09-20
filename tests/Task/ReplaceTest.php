2c8ac87e0a64ff8199fdbfb9fee8e179e896741f
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-09-20 14:24:11 +0200

    Recursive tests

--- tests/Task/ReplaceTest.php
+++ tests/Task/ReplaceTest.php
@@ -66,4 +66,90 @@ class ReplaceTest extends TaskTestCase
 
 		$this->assertEquals( $expected, $result );
 	}
+
+	public function testReplaceRecursive(): void
+	{
+		$this->bootstrap();
+		$task = $this->getTask();
+
+		$data = [
+			'name' => 'Test',
+			'price' => 12.34,
+			'test' => [
+				'name' => 'Test',
+				'test' => [
+					'name' => 'Test',
+					'test' => 'value',
+				],
+			],
+		];
+
+		$config = [
+			'recursive' => true,
+			'params' => [
+				[
+					'find' => 'est',
+					'replace' => 'esting',
+				]
+			],
+		];
+
+		// Default (value).
+
+		$expected = [
+			'name' => 'Testing',
+			'price' => 12.34,
+			'test' => [
+				'name' => 'Testing',
+				'test' => [
+					'name' => 'Testing',
+					'test' => 'value',
+				],
+			],
+		];
+
+		$result = $task->execute( $config, $this->getContext(), $data );
+
+		$this->assertEquals( $expected, $result );
+
+		// Replace Key.
+
+		$config['action'] = 'key';
+
+		$expected = [
+			'name' => 'Test',
+			'price' => 12.34,
+			'testing' => [
+				'name' => 'Test',
+				'testing' => [
+					'name' => 'Test',
+					'testing' => 'value',
+				],
+			],
+		];
+
+		$result = $task->execute( $config, $this->getContext(), $data );
+
+		$this->assertEquals( $expected, $result );
+
+		// Replace Both.
+
+		$config['action'] = 'both';
+
+		$expected = [
+			'name' => 'Testing',
+			'price' => 12.34,
+			'testing' => [
+				'name' => 'Testing',
+				'testing' => [
+					'name' => 'Testing',
+					'testing' => 'value',
+				],
+			],
+		];
+
+		$result = $task->execute( $config, $this->getContext(), $data );
+
+		$this->assertEquals( $expected, $result );
+	}
 }
