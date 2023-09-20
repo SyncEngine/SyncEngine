dea6b32537fa421dfa3c9b61e1b0fffa6c1ef0ab
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-09-20 11:47:20 +0200

    Map task value tests

--- tests/Task/MapTest.php
+++ tests/Task/MapTest.php
@@ -63,4 +63,60 @@ class MapTest extends TaskTestCase
 
 		$this->assertEquals( $expected, $result );
 	}
+
+	public function testMapValue(): void
+	{
+		$this->bootstrap();
+		$task = $this->getTask();
+
+		$data = [
+			'name' => 'Test',
+			'price' => 12.34,
+		];
+
+		$config = [
+			'action' => 'value',
+			'key' => 'name',
+			'map' => [
+				[
+					// Only replaces exact matches. For partials, use Replace task.
+					'source' => 'Test',
+					'target' => 'Testing',
+				]
+			],
+		];
+
+		// Default.
+
+		$expected = [
+			'name' => 'Testing',
+			'price' => 12.34,
+		];
+
+		$result = $task->execute( $config, $this->getContext(), $data );
+
+		$this->assertEquals( $expected, $result );
+
+		// Key.
+
+		$data['groups'] = [ 'Sync', 'Motor' ];
+
+		$config['key'] = 'groups';
+		$config['map'] = [
+			[
+				'source' => 'Motor',
+				'target' => 'Engine',
+			]
+		];
+
+		$expected = [
+			'name' => 'Test',
+			'price' => 12.34,
+			'groups' => [ 'Sync', 'Engine' ],
+		];
+
+		$result = $task->execute( $config, $this->getContext(), $data );
+
+		$this->assertEquals( $expected, $result );
+	}
 }
