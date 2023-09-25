98469a57e142f27c07648f11f50bdafb87e94d8d
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-09-25 16:09:39 +0200

    Rename onClick to callback

--- assets/react/components/partials/Tags/index.jsx
+++ assets/react/components/partials/Tags/index.jsx
@@ -11,7 +11,7 @@ export default function Tags( props ) {
 		endChar = ' }}',
 		separator = '.',
 		tags = useContext( TagsContext ),
-		onClick,
+		callback,
 		trigger,
 	} = props;
 
@@ -20,7 +20,7 @@ export default function Tags( props ) {
 	}
 
 	const selectTag = ( tag ) => {
-		onClick( startChar + tag + endChar );
+		callback( startChar + tag + endChar );
 	}
 
 	const body = (
