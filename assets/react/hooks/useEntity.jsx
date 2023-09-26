7788b448594c1789cd758bd3e9714421125e4066
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-09-26 14:40:09 +0200

    useEntity for single entities

---
+++ assets/react/hooks/useEntity.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import useEntities from './useEntities';
+
+/**
+ *
+ * @param {String} type
+ * @param {int|string} id_or_ref
+ * @param {Object[]} items
+ * @param {Object} query
+ * @param {String} endpoint
+ * @return {[*,function]}
+ */
+export default function useEntity( type, id_or_ref = 0, items = [], query = null, endpoint = null ) {
+	const [ entities, callbacks ] = useEntities( type, items, query, endpoint );
+	const [ current, setCurrent ] = useState( id_or_ref );
+
+	useEffect( () => {
+		if ( ! callbacks.get( current ) ) {
+			getEntity( current );
+		}
+	}, [] );
+
+	const getEntity = ( id_or_ref ) => {
+		setCurrent( id_or_ref );
+
+		if ( ! callbacks.get( id_or_ref ) ) {
+			callbacks.fetch( { search: id_or_ref } );
+		}
+	}
+
+	return [ callbacks.get( current ), getEntity ];
+}
