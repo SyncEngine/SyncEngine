3bb964d650b2aa386cbf3c9e6017a46b4e1ee7d2
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-10-19 16:28:53 +0200

    Option to pass data

--- assets/react/components/modals/RequestModal/index.jsx
+++ assets/react/components/modals/RequestModal/index.jsx
@@ -28,36 +28,34 @@ export default function RequestModal( props ) {
 		return title + ( entity ? entity.name ?? '' : '' );
 	}
 
-	const getData = () => {
-		const data = {
-			action: action,
-		}
-
-		for ( const key in params ) {
-			if ( ! params.hasOwnProperty( key ) ) {
-				continue;
-			}
-			switch ( params[ key ] ) {
-				case 'item':
-					data[ key ] = JSON.stringify( item );
-					break;
-				case 'element':
-					data[ key ] = element.value;
-					break;
-				case 'entity':
-					data[ key ] = entity ? JSON.stringify( entity ) : null;
-					break;
-				case 'entityId':
-					if ( entity && entity.id ) {
-						data[ key ] = entity.id;
-					} else {
-						// @todo Enhance this.
-						data[ key ] = element.parentNode.parentNode.dataset.id;
-					}
-					break;
-				default:
-					data[ key ] = params[ key ];
-					break;
+	const getData = ( data = { action: action } ) => {
+		if ( params ) {
+			for ( const key in params ) {
+				if ( ! params.hasOwnProperty( key ) ) {
+					continue;
+				}
+				switch ( params[ key ] ) {
+					case 'item':
+						data[ key ] = JSON.stringify( item );
+						break;
+					case 'element':
+						data[ key ] = element.value;
+						break;
+					case 'entity':
+						data[ key ] = entity ? JSON.stringify( entity ) : null;
+						break;
+					case 'entityId':
+						if ( entity && entity.id ) {
+							data[ key ] = entity.id;
+						} else {
+							// @todo Enhance this.
+							data[ key ] = element.parentNode.parentNode.dataset.id;
+						}
+						break;
+					default:
+						data[ key ] = params[ key ];
+						break;
+				}
 			}
 		}
 
