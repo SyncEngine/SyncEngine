f5882ef2240251a72fd1971500faed1c89d9eb11
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-10-19 19:00:57 +0200

    Add more info to the context refs

--- assets/react/components/form/FormStatic/index.jsx
+++ assets/react/components/form/FormStatic/index.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from 'react';
 import { createRefId } from "../../../utils/globals";
 import { TagsContext } from '../../../context/TagsContext';
+import { EntityContext } from '../../../context/EntityContext';
 
 export default function FormStatic( props ) {
 	const {
@@ -12,12 +13,18 @@ export default function FormStatic( props ) {
 
 	const contextRef = createRefId();
 	const tags = useContext( TagsContext );
+	const parent = useContext( EntityContext );
 
 	useEffect( () => {
 		const form = document.querySelector( '#form_' + entity + '_' + id + ' form' );
 		form.id = contextRef;
 		window.app.forms.register( form );
-		window.app.context.register( contextRef, { tags: tags } );
+
+		window.app.context.register( contextRef, {
+			tags: structuredClone( tags ),
+			execute: structuredClone( tags.context ),
+			parentEntity: parent,
+		} );
 
 		return function cleanup() {
 			window.app.context.clear( contextRef );
