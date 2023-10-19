2a66506882b10fd57a1f1078e7886a3241ea3288
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-10-19 20:02:05 +0200

    Support non-object returns

--- assets/react/components/modals/TestModal/index.jsx
+++ assets/react/components/modals/TestModal/index.jsx
@@ -74,7 +74,7 @@ export default function TestModal( props ) {
 									objectToMappable( response.data, 'name', 'content', true ).map( tab => {
 										return (
 											<Tab eventKey={ tab.name } key={ tab.name } title={ ucfirst( tab.name ) }>
-												<pre className="bg-body-tertiary p-3">{ JSON.stringify( tab.content, null, 2 ) }</pre>
+												<pre className="bg-body-tertiary p-3">{ ( 'object' === typeof tab.content ) ? JSON.stringify( tab.content, null, 2 ) : tab.content }</pre>
 											</Tab>
 										)
 									} )
