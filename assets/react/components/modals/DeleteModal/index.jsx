37ae4d60c690e85d1ed55fa95dc0040f90b49932
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-10-25 16:00:25 +0200

    Fix default trigger
    The content could also be false in rare occasions plus this will validate the actual param.

--- assets/react/components/modals/DeleteModal/index.jsx
+++ assets/react/components/modals/DeleteModal/index.jsx
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import ConfirmModal from '../ConfirmModal';
 
 export default function DeleteModal( props ) {
 	const {
 		text = 'Are you sure?',
 		confirm = 'Delete',
-		children = <span className="bi bi-trash-fill icon-link border-0 link-danger" />
 	} = props;
+
+	const getTrigger = () => {
+		if ( 'function' === typeof props.children ) {
+			return props.children;
+		} else if ( React.isValidElement( props.children ) ) {
+			return props.children;
+		}
+		return <span className="bi bi-trash-fill icon-link border-0 link-danger" />;
+	}
 
 	return (
 		<ConfirmModal
 			{ ...props }
 			text={ text }
 			confirm={ confirm }
-			children={ children }
-		/>
+		>
+			{ getTrigger() }
+		</ConfirmModal>
 	);
 }
