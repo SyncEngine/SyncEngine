449a35a124e7d2fe317bd5a77c46221cddf40f08
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-10-27 14:34:49 +0200

    Parse default values
    Fixes conditionals

--- assets/react/components/form/Fields/index.jsx
+++ assets/react/components/form/Fields/index.jsx
@@ -11,20 +11,36 @@ export default function Fields( props ) {
 		onChange,
 	} = props;
 
-	const [ values, setValues ] = useState( ( 'object' === typeof value ) ? { ...value } : {} );
+	const getDefault = useCallback( ( field ) => {
+		if ( -1 < [ 'checkbox', 'switch'].indexOf( field.type ) ) {
+			// Keep checkbox value if default is set.
+			return false;
+		}
+		return field.default ?? null;
+	}, [] );
+
+	const parseValue = useCallback( ( values, fields ) => {
+		if ( ! fields || ( 'object' !== typeof value ) ) {
+			return {};
+		}
+		for ( const key in fields ) {
+			const name = fields[ key ].name ?? key;
+			if ( isEmpty( values[ name ] ) && fields[ key ].hasOwnProperty( 'default' ) ) {
+				values[ name ] = getDefault( fields[ key ] );
+			}
+		}
+		return values;
+	}, [] );
+
+	const [ values, setValues ] = useState( parseValue( value, props.fields ) );
 
 	const updateField = useCallback( ( input, key, field ) => {
 		let newValues = { ...values };
 		if ( ! isEmpty( input ) ) {
 			newValues[ key ] = input;
 		} else {
-			if ( field.hasOwnProperty( 'default' ) ) {
-				if ( -1 < [ 'checkbox', 'switch'].indexOf( field.type ) ) {
-					// Keep checkbox value if default is set.
-					newValues[ key ] = false;
-				} else {
-					newValues[ key ] = field.default;
-				}
+			if ( field && field.hasOwnProperty( 'default' ) ) {
+				newValues[ key ] = getDefault( field );
 			} else {
 				// @todo Allow empty?
 				delete newValues[ key ];
