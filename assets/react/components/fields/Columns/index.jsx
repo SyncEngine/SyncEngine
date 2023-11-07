c80a17bee38ae8d1fcd3a86ccd43433dc58a0b7e
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-11-07 12:46:45 +0100

    Sortable columns (opt-in)

--- assets/react/components/fields/Columns/index.jsx
+++ assets/react/components/fields/Columns/index.jsx
@@ -1,26 +1,43 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { Stack } from 'react-bootstrap';
 
-import ColumnsHead from "./Head";
-import ColumnsRow from "./Row";
+import ColumnsHead from './Head';
+import ColumnsRow from './Row';
+import Sortable from '../../services/Sortable';
 
-import { objectToMappable } from "../../../utils/data";
-import { isEmpty } from "../../../utils/conditionals";
+import { objectToMappable } from '../../../utils/data';
+import { isEmpty } from '../../../utils/conditionals';
+import { createRefId } from '../../../utils/globals';
 
 export default function Columns( props ) {
-	const [ value, setValue ] = useState( ( Array.isArray( props.value ) && props.value.length ) ? [ ...props.value ] : [] );
 
 	const {
 		columns = {},
 		nest = false,
+		sortable = false,
+		removable = true,
+		taggable = true,
 		onChange,
 	} = props;
 
 	const columnMap = objectToMappable( columns, 'name', 'label' );
 
+	const parseValue = useCallback( ( value ) => {
+		return objectToMappable( value ).map( ( row ) => {
+			if ( ! row.hasOwnProperty( '_ref' ) ) {
+				row._ref = createRefId();
+			}
+			return row;
+		} )
+	}, [] );
+
+	const [ value, setValue ] = useState( ( Array.isArray( props.value ) && props.value.length ) ? parseValue( props.value ) : [] );
+
 	const updateValue = ( newValue ) => {
 		setValue( newValue );
-		onChange( newValue );
+
+		// Remove refs.
+		onChange( newValue.map( item => { item = { ...item }; delete item._ref; return item } ) );
 	}
 
 	const updateIndex = ( index, newValue ) => {
@@ -43,18 +60,57 @@ export default function Columns( props ) {
 		value.push( {} );
 	}
 
+	let items = [];
+	if ( sortable ) {
+		items = (
+			<Sortable
+				setItems={ updateValue }
+				items={ value.map( ( row, index ) => {
+					return {
+						_ref: row._ref,
+						value: row,
+						component: ColumnsRow,
+						attributes: {
+							data: row,
+							columnMap: columnMap,
+							nest: nest,
+							onChange: ( value ) => { updateIndex( index, value ) },
+							taggable: taggable,
+							removable: removable,
+						},
+						handle: 'custom',
+					}
+				} ) }
+			/>
+		);
+	} else {
+		items = (
+			<>
+				{
+					value.map( ( row, index ) => {
+						return (
+							<ColumnsRow
+								key={ index }
+								data={ row }
+								columnMap={ columnMap }
+								nest={ nest }
+								onChange={ ( value ) => { updateIndex( index, value ) } }
+								taggable={ taggable }
+								removable={ removable }
+							/>
+						)
+					} )
+				}
+			</>
+		);
+	}
+
 	return (
 		<Stack gap="1">
 			{ columnMap &&
-				<ColumnsHead columnMap={ columnMap } />
+				<ColumnsHead columnMap={ columnMap } sortable={ sortable } removable={ removable } />
 			}
-			{
-				value.map( ( row, index ) => {
-					return (
-						<ColumnsRow key={ index } data={ row } columnMap={ columnMap } nest={ nest } onChange={ ( value ) => { updateIndex( index, value ) } } taggable={ props.taggable } />
-					)
-				} )
-			}
+			{ items }
 		</Stack>
 	);
 }
