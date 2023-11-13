9c5a97f76209111002b23a5e9cf4fe34f3f9be7d
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-11-13 23:21:39 +0100

    Blueprint version

--- src/Model/BlueprintModel.php
+++ src/Model/BlueprintModel.php
@@ -10,6 +10,13 @@ class BlueprintModel implements Configurable
 	use Config;
 
 	/**
+	 * The version of this blueprint.
+	 *
+	 * @var string
+	 */
+	public string $version = '';
+
+	/**
 	 * The type of blueprint, can be used for categorizing.
 	 *
 	 * @var string
@@ -57,12 +64,18 @@ class BlueprintModel implements Configurable
 			// @todo Load blueprint file.
 		}
 
+		$this->version     = $blueprint['version'];
 		$this->type        = $blueprint['type'];
 		$this->entity      = $blueprint['entity'];
 		$this->name        = $blueprint['name'];
 		$this->description = $blueprint['description'] ?? '';
 		$this->fields      = $blueprint['fields'] ?? [];
 		$this->template    = $blueprint['template'];
+	}
+
+	public function getVersion(): string
+	{
+		return $this->version;
 	}
 
 	public function getFields(): array
