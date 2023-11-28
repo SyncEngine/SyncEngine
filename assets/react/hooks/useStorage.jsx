fab87b6fb12e351f967248b68f9427f414446559
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-11-28 14:50:43 +0100

    useStorage hook for any type of setting

---
+++ assets/react/hooks/useStorage.jsx
@@ -0,0 +1,52 @@
+import { useCallback, useState } from 'react';
+import { isEmpty } from '../utils/conditionals';
+import { publish, subscribe } from '../utils/events';
+
+/**
+ * @param {string} type
+ * @param {string} namespace
+ * @param {string} key
+ * @param {*} initial
+ * @param {boolean} json
+ * @returns {*,function,boolean}
+ */
+export default function useStorage( type = 'local', namespace = '', key = '', initial = null, json = false ) {
+	const storage = 'session' === type ? sessionStorage : localStorage;
+	const setting = namespace ? namespace + '/' + key : key;
+
+	const get = useCallback( ( fallback = null ) => {
+		let value = storage.getItem( setting );
+		if ( null === value ) {
+			return fallback;
+		}
+		if ( ! json ) {
+			return value;
+		}
+		try {
+			return JSON.parse( value );
+		} catch ( e ) {
+			// @todo debug message?
+		}
+		return fallback;
+	}, [ setting, json ] );
+
+	const set = useCallback( ( value ) => {
+		if ( json ) {
+			value = JSON.stringify( value );
+		}
+		storage.setItem( setting, value );
+	}, [ setting, json ] );
+
+	const update = ( key, value, json ) => {
+		set( value );
+		publish( 'update:' + type + 'Storage:' + setting, value );
+	}
+
+	subscribe( 'update:' + type + 'Storage:' + setting, () => {
+		setValue( get() );
+	} );
+
+	const [ value, setValue ] = useState( get( initial ) );
+
+	return [ value, update, ! isEmpty( storage.getItem( setting ) ) ];
+}
