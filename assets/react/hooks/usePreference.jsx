6c00d529b9d83e9f1ef7168ee10eea65c8831bc5
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-11-28 14:57:12 +0100

    Optionally define JSON type

--- assets/react/hooks/usePreference.jsx
+++ assets/react/hooks/usePreference.jsx
@@ -7,8 +7,9 @@ import useStorage from './useStorage';
  * @param {string} namespace
  * @param {string} key
  * @param {*} initial
+ * @param {boolean} json
  * @returns {*,function,boolean}
  */
-export default function usePreference( namespace, key, initial ) {
+export default function usePreference( namespace, key, initial, json = false ) {
 	return useStorage( 'local', namespace, key, initial, false );
 }
