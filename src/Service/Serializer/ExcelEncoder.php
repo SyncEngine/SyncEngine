ef52c41b18e3c6d11b34c1df097f775356d05cf2
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-11-30 09:11:49 +0100

    Custom decoder to support file path as data

--- src/Service/Serializer/ExcelEncoder.php
+++ src/Service/Serializer/ExcelEncoder.php
@@ -2,4 +2,180 @@
 
 namespace App\Service\Serializer;
 
-class ExcelEncoder extends \Ang3\Component\Serializer\Encoder\ExcelEncoder {}
+use PhpOffice\PhpSpreadsheet\Exception as PhpSpreadsheetException;
+use PhpOffice\PhpSpreadsheet\Reader as Readers;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Serializer\Encoder\CsvEncoder;
+use Symfony\Component\Serializer\Exception\InvalidArgumentException;
+use Symfony\Component\Serializer\Exception\NotEncodableValueException;
+use Symfony\Component\Serializer\Exception\RuntimeException;
+
+class ExcelEncoder extends \Ang3\Component\Serializer\Encoder\ExcelEncoder
+{
+	/**
+	 * Formats constants.
+	 */
+	public const XLS = 'xls';
+	public const XLSX = 'xlsx';
+
+	/**
+	 * Context constants.
+	 */
+	public const AS_COLLECTION_KEY = CsvEncoder::AS_COLLECTION_KEY;
+	public const FLATTENED_HEADERS_SEPARATOR_KEY = 'flattened_separator_key';
+	public const HEADERS_IN_BOLD_KEY = 'headers_in_bold';
+	public const HEADERS_HORIZONTAL_ALIGNMENT_KEY = 'headers_horizontal_alignment';
+	public const COLUMNS_AUTOSIZE_KEY = 'columns_autosize';
+	public const COLUMNS_MAXSIZE_KEY = 'columns_maxsize';
+
+	/**
+	 * @static
+	 */
+	private static array $formats = [
+		self::XLS,
+		self::XLSX,
+	];
+
+	private array $defaultContext = [
+		self::AS_COLLECTION_KEY => true,
+		self::FLATTENED_HEADERS_SEPARATOR_KEY => '.',
+		self::HEADERS_IN_BOLD_KEY => true,
+		self::HEADERS_HORIZONTAL_ALIGNMENT_KEY => 'center',
+		self::COLUMNS_AUTOSIZE_KEY => true,
+		self::COLUMNS_MAXSIZE_KEY => 50,
+	];
+
+	private Filesystem $filesystem;
+
+	public function __construct(array $defaultContext = [])
+	{
+		$this->defaultContext = array_merge($this->defaultContext, $defaultContext);
+		$this->filesystem = new Filesystem();
+	}
+
+	/**
+	 * {@inheritdoc}.
+	 *
+	 * @throws NotEncodableValueException When data are not valid
+	 * @throws InvalidArgumentException   When the format or data not supported
+	 * @throws RuntimeException           When data reading failed
+	 * @throws PhpSpreadsheetException    On data failure
+	 */
+	public function decode($data, $format, array $context = [])
+	{
+		if (!\is_scalar($data) && ! is_resource( $data )) {
+			throw new NotEncodableValueException(sprintf('Expected data of type scalar or resource, %s given', \gettype($data)));
+		}
+
+		if ( is_file( $data ) ) {
+			$tmpFile = $data;
+		} else {
+			$tmpFile = (string) tempnam(sys_get_temp_dir(), $format);
+			$this->filesystem->dumpFile($tmpFile, $data);
+		}
+
+		$context = $this->normalizeContext($context);
+
+		switch ($format) {
+			// Excel 2007
+			case self::XLSX:
+				$reader = new Readers\Xlsx();
+			break;
+
+			// Excel 2003
+			case self::XLS:
+				$reader = new Readers\Xls();
+			break;
+
+			default:
+				throw new InvalidArgumentException(sprintf('The format "%s" is not supported', $format));
+		}
+
+		try {
+			$spreadsheet = $reader->load($tmpFile);
+			$this->filesystem->remove($tmpFile);
+		} catch (\Exception $e) {
+			throw new RuntimeException(sprintf('Excel decoding failed - %s', $e->getMessage()), 0, $e);
+		}
+
+		$loadedSheetNames = $spreadsheet->getSheetNames();
+		$data = [];
+
+		foreach ($loadedSheetNames as $sheetIndex => $loadedSheetName) {
+			$worksheet = $spreadsheet->getSheet($sheetIndex);
+			$sheetData = $worksheet->toArray(
+			);
+
+			if (0 === \count($sheetData)) {
+				continue;
+			}
+
+			if (false === $context[self::AS_COLLECTION_KEY]) {
+				$data[$loadedSheetName] = $sheetData;
+
+				continue;
+			}
+
+			$labelledRows = [];
+			$headers = null;
+
+			foreach ($sheetData as $rowIndex => $cells) {
+				$rowIndex = (int) $rowIndex;
+
+				if (null === $headers) {
+					$headers = [];
+
+					foreach ($cells as $key => $value) {
+						if (null === $value || '' === $value) {
+							continue;
+						}
+
+						$headers[$key] = $value;
+						unset($sheetData[$rowIndex][$key]);
+					}
+
+					continue;
+				}
+
+				foreach ($cells as $key => $value) {
+					if (\array_key_exists($key, $headers)) {
+						$labelledRows[$rowIndex - 1][(string) $headers[$key]] = $value;
+					} else {
+						$labelledRows[$rowIndex - 1][''][$key] = $value;
+					}
+
+					unset($sheetData[$rowIndex][$key]);
+				}
+
+				unset($sheetData[$rowIndex]);
+			}
+
+			$data[$loadedSheetName] = $labelledRows;
+		}
+
+		return $data;
+	}
+
+	/**
+	 * @internal
+	 */
+	private function normalizeContext(array $context = []): array
+	{
+		return [
+			self::AS_COLLECTION_KEY => (bool) $this->getContextValue($context, self::AS_COLLECTION_KEY),
+			self::FLATTENED_HEADERS_SEPARATOR_KEY => (string) $this->getContextValue($context, self::FLATTENED_HEADERS_SEPARATOR_KEY),
+			self::HEADERS_IN_BOLD_KEY => (bool) $this->getContextValue($context, self::HEADERS_IN_BOLD_KEY),
+			self::HEADERS_HORIZONTAL_ALIGNMENT_KEY => (string) $this->getContextValue($context, self::HEADERS_HORIZONTAL_ALIGNMENT_KEY),
+			self::COLUMNS_AUTOSIZE_KEY => (bool) $this->getContextValue($context, self::COLUMNS_AUTOSIZE_KEY),
+			self::COLUMNS_MAXSIZE_KEY => (int) $this->getContextValue($context, self::COLUMNS_MAXSIZE_KEY),
+		];
+	}
+
+	/**
+	 * @internal
+	 */
+	private function getContextValue(array $context, int|string $key): bool|int|float|string|null
+	{
+		return $context[$key] ?? $this->defaultContext[$key];
+	}
+}
