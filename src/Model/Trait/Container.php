ab1387f2c1b174e1c0353f9e901f07b818d6d07d
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-12-05 15:13:14 +0100

    Enforce var type
    Co-Authored-By: Inform-All <NKoonen@users.noreply.github.com>

--- src/Model/Trait/Container.php
+++ src/Model/Trait/Container.php
@@ -2,16 +2,18 @@
 
 namespace App\Model\Trait;
 
+use Psr\Container\ContainerInterface;
+
 trait Container
 {
-	protected object $container;
+	protected ContainerInterface $container;
 
-	public function setContainer( $container )
+	public function setContainer( ContainerInterface $container ): void
 	{
 		$this->container = $container;
 	}
 
-	public function getContainer()
+	public function getContainer(): ?ContainerInterface
 	{
 		return $this->container ?? null;
 	}
