a1818363512d0a75983b3b26673e9665b7690487
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-12-05 15:11:00 +0100

    Codestyle
    Co-Authored-By: Inform-All <NKoonen@users.noreply.github.com>

--- src/Service/ExecutePreview.php
+++ src/Service/ExecutePreview.php
@@ -289,8 +289,13 @@ class ExecutePreview extends Execute
 			protected mixed $data;
 			protected ExecutionContext $context;
 
-			public function __construct( $data, ExecutionContext $context, string $message = "SyncEngine Exit Preview", int $code = 0, ?\Throwable $previous = null )
-			{
+			public function __construct(
+				$data,
+				ExecutionContext $context,
+				string $message = "SyncEngine Exit Preview",
+				int $code = 0,
+				?\Throwable $previous = null
+			) {
 				parent::__construct( $message, $code, $previous );
 				$this->data    = $data;
 				$this->context = $context;
