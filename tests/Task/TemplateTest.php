51e153605b072d3c976a684adb488db8e5d4f540
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-12-07 12:12:52 +0100

    Advanced template test

--- tests/Task/TemplateTest.php
+++ tests/Task/TemplateTest.php
@@ -50,4 +50,139 @@ class TemplateTest extends TaskTestCase
 		$this->assertArrayHasKey( 'price_incl', $result );
 		$this->assertEquals( $incl, $result['price_incl'] );
 	}
+
+	public function testAdvancedCalc(): void
+	{
+		$task = $this->getTask();
+
+		$config = [
+			'template' => "
+
+{% set result = [] %}
+{% for row in data %}
+
+    {% set price = row.price|replace({',':'.'}) %}
+
+    {% if price < 5 %}
+
+        {% set price = price|round(1, 'floor') %}
+
+    {% elseif price < 10 %}
+
+        {% set floor = price|round(0, 'floor') %}
+        {% set fraction = price - floor %}
+
+        {% if fraction < 0.25 %}
+            {% set price = floor %}
+        {% elseif fraction < 0.50 %}
+            {% set price = floor + 0.25 %}
+        {% elseif fraction < 0.75 %}
+            {% set price = floor + 0.50 %}
+        {% else %}
+            {% set price = floor + 0.75 %}
+        {% endif %}
+
+    {% elseif price < 50 %}
+
+        {% set floor = price|round(0, 'floor') %}
+        {% set fraction = price - floor %}
+
+        {% if fraction < 0.50 %}
+            {% set price = floor %}
+        {% else %}
+            {% set price = floor + 0.50 %}
+        {% endif %}
+
+    {% else %}
+
+        {% set price = price|round(0, 'floor') %}
+
+    {% endif %}
+
+    {% set price = price|number_format(2, ',', '') %}
+    {% set row = row|merge( { price: price } ) %}
+    {% set result = result|merge( { (loop.index0): row } ) %}
+
+{% endfor %}
+{% set data = result %}
+
+			"
+		];
+
+		$data = [
+			[
+				"Keep me" => 'Yay!',
+				"price" => "1,98",
+			],
+			[
+				"price" => "2,97",
+			],
+			[
+				"price" => "3,59",
+			],
+			[
+				"Keep me" => 'Foo!',
+				"price" => "14,00",
+			],
+			[
+				"price" => "21,01",
+			],
+			[
+				"Keep me" => [
+					'Foo' => 'Bar',
+				],
+				"price" => "25,52",
+			],
+			[
+				"price" => "160,39",
+			],
+			[
+				"Keep me" => 'Bar!',
+				"price" => "240,58",
+			],
+			[
+				"price" => "291,10",
+			]
+		];
+
+		$expected = [
+			[
+				"Keep me" => 'Yay!',
+				"price" => "1,90",
+			],
+			[
+				"price" => "2,90",
+			],
+			[
+				"price" => "3,50",
+			],
+			[
+				"Keep me" => 'Foo!',
+				"price" => "14,00",
+			],
+			[
+				"price" => "21,00",
+			],
+			[
+				"Keep me" => [
+					'Foo' => 'Bar',
+				],
+				"price" => "25,50",
+			],
+			[
+				"price" => "160,00",
+			],
+			[
+				"Keep me" => 'Bar!',
+				"price" => "240,00",
+			],
+			[
+				"price" => "291,00",
+			]
+		];
+
+		$result = $task->execute( $config, $this->getContext(), $data );
+
+		$this->assertEquals( $expected, $result );
+	}
 }
