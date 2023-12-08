db6e08196e4b81c8d0a588591a2185f4ad1249f6
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-12-08 22:16:53 +0100

    ResponseTabs component

---
+++ assets/react/components/modals/ResponseTabs.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Tab, Tabs } from 'react-bootstrap';
+import Code from '../fields/Code';
+import { objectToMappable } from '../../utils/data';
+import { ucfirst } from '../../utils/globals';
+
+export default function ResponseTabs( props ) {
+	return (
+			<Tabs>
+				{
+					objectToMappable( props.data, 'name', 'content', true ).map( tab => {
+						const json = 'object' === typeof tab.content;
+						return (
+							<Tab eventKey={ tab.name } key={ tab.name } title={ ucfirst( tab.name ) } className="overflow-hidden w-100">
+								<Code
+									contained
+									height="100%"
+									language={ json && 'json' }
+									editable={ false }
+									value={ json ? JSON.stringify( tab.content, null, 2 ) : tab.content }
+								/>
+							</Tab>
+						)
+					} )
+				}
+			</Tabs>
+	)
+}
