b0dbc18b922d02e0657959104814f32cb1e55b00
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-12-14 19:45:54 +0100

    Enhance  tracer

--- src/Service/ExecuteTrace.php
+++ src/Service/ExecuteTrace.php
@@ -2,34 +2,73 @@
 
 namespace SyncEngine\Service;
 
+use SyncEngine\Model\Abstract\EntityModel;
+
 class ExecuteTrace extends ResourceData
 {
-	public function addTrace( $message, ExecutionContext $context, $model = null, $data = [] ): void
+	private array $current = [];
+
+	public function addLog( $message ): void
 	{
-		$key = [];
-		foreach ( $context->getAncestors() as $ancestor ) {
-			$key[] = $ancestor->getIndex();
+		$key = implode( '.trace.', $this->current );
+
+		$trace = $this->get( $key );
+
+		if ( ! isset( $trace['trace'] ) ) {
+			$trace['trace'] = [];
 		}
-		$key[] = $context->getIndex();
-
-		$trace = (array) $this->get( $key, [] );
-
-		if ( $model ) {
-
-			if ( is_array( $model ) ) {
-				$name = $model['_label'] ?? '';
-				$ref  = $model['_ref'];
-			} else {
-				$name = $model->getName() ?? '';
-				$ref  = $model->getRef();
-			}
-
-			$label = $name ? $name . ' (' . $ref . ')' : $ref;
-			$message .= ': ' . $label . ' | ' . count( $data );
-		}
-
-		$trace[] = $message;
+		$trace['trace'][] = $message;
 
 		$this->set( $trace, $key );
+	}
+
+	public function enterTrace( $model )
+	{
+		if ( is_array( $model ) ) {
+			$ref = $model['_ref'];
+			$name = $model['_label'] ?? '';
+			$type = $model['_class'] ?? '';
+		} elseif ( is_object( $model ) ) {
+			$ref = $model->getRef();
+			$name = $model->getName();
+			if ( $model instanceof EntityModel ) {
+				$type = $model::getEntityClass();
+			} else {
+				$type = $model::getClassName();
+			}
+		} else {
+			$ref = (string) $model;
+			$name = '';
+			$type = '';
+		}
+
+		$key = implode( '.trace.', $this->current );
+		$current = $this->get( $key );
+		if ( ! isset( $current['trace'] ) ) {
+			$current['trace'] = [];
+			$this->set( $current, $key );
+		}
+
+		$this->current[] = $ref;
+
+		$trace = [];
+
+		if ( $name !== $ref || $type ) {
+			$trace['info'] = $name . ( $type ? ' (' . $type . ')' : '' );
+		}
+
+		$key = implode( '.trace.', $this->current );
+
+		$this->set( $trace, $key );
+	}
+
+	public function leaveTrace()
+	{
+		array_pop( $this->current );
+	}
+
+	public function resetTrace()
+	{
+		$this->current = [];
 	}
 }
