18295131e534db9bcff14208ab765d600e0fd581
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-12-18 16:43:13 +0100

    Create blueprint select component

---
+++ assets/react/components/form/SelectBlueprint/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import Select from "../../fields/Select/Advanced";
+
+export default function SelectBlueprint( props ) {
+	const { t } = useTranslation();
+
+	return (
+		<Select
+			choices={ props.options }
+			filters={ { key: 'type', label: t('All') } }
+			label={ t('Select Blueprint') }
+			placeholder={ '-- ' + t('Select Blueprint') + ' --' }
+			group="module"
+			value=""
+			{ ...props }
+		/>
+	)
+}
