b1fb7790211b7fbe7c096134896833142cee0427
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-12-19 21:49:32 +0100

    Support focus + enforce re-render when changing format

--- assets/react/components/modals/ExportModal/ExportContent.jsx
+++ assets/react/components/modals/ExportModal/ExportContent.jsx
@@ -36,14 +36,14 @@ export default function ExportModalContent( props ) {
 	return <>
 		<div className="text-end position-sticky top-0 z-1">
 			<ButtonGroup className="justify-content-end">
-				<OverlayTrigger overlay={ <Tooltip id="export-format">{ ( formatted ) ? t('Compact') : t('Format') }</Tooltip> } trigger="hover">
+				<OverlayTrigger key={ formatted ? 'formatted' : 'compact' } overlay={ <Tooltip id="export-format">{ ( formatted ) ? t('Compact') : t('Format') }</Tooltip> }>
 					<Button variant={ ( formatted ) ? 'secondary' : 'outline-secondary ' } onClick={() => { setFormatted( !formatted ) }}>
 						{ formatted && <span className="bi bi-code" /> }
 						{ ! formatted && <span className="bi bi-chevron-expand" /> }
 					</Button>
 				</OverlayTrigger>
 				{ null !== clipboard &&
-				    <OverlayTrigger overlay={ <Tooltip id="export-copy">{ t('Copy') }</Tooltip> } trigger="hover">
+				    <OverlayTrigger overlay={ <Tooltip id="export-copy">{ t('Copy') }</Tooltip> }>
 					    <Button variant={ ( copied ) ? 'secondary' : 'outline-secondary' } onClick={ handleCopy }>
 						    { copied && <span className="bi bi-check" /> }
 						    { ! copied && <span className="bi bi-clipboard" /> }
