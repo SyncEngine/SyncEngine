7a1fcf516c278f4e019408998712d86b8cc78e54
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-12-20 15:28:28 +0100

    Pass trace to looper

--- src/MessageHandler/AutomationLooperHandler.php
+++ src/MessageHandler/AutomationLooperHandler.php
@@ -17,8 +17,8 @@ class AutomationLooperHandler
 	public function __construct(
 		private readonly Execute $execute,
 		// Load required to instantiate container.
-		private readonly DefaultController $controller )
-	{}
+		private readonly DefaultController $controller
+	) {}
 
 	public function __invoke( AutomationLooper $message ): void
 	{
@@ -26,6 +26,8 @@ class AutomationLooperHandler
 
 		// @todo Provide context about previous loop?
 		$context = new ExecutionContext( $this->execute, $model );
+
+		$this->execute->trace()->init( $model, $message->getTraceId() );
 
 		// @todo provide request of previous loop?
 		$this->execute->execute( $model, $context );
