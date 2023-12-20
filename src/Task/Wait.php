4cd06e4f29545c41cf9f1b8c7a5906b727a51993
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-12-20 10:46:44 +0100

    Wait task

---
+++ src/Task/Wait.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace SyncEngine\Task;
+
+use SyncEngine\Model\AutomationModel;
+use SyncEngine\Model\FlowModel;
+use SyncEngine\Model\StepModel;
+use SyncEngine\Model\TaskModel;
+use SyncEngine\Service\ExecuteData;
+use SyncEngine\Service\ExecutionContext;
+
+class Wait extends TaskModel
+{
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->type        = 'utility';
+		$this->name        = $this->trans( 'Wait' );
+		$this->description = $this->trans( 'Wait for time to pass' );
+	}
+
+	public function getFields(): array
+	{
+		return [
+			'seconds' => [
+				'label'    => $this->trans( 'Seconds to wait' ),
+				'type'     => 'number',
+				'required' => true,
+			],
+		];
+	}
+
+	public function execute( array $config, ExecutionContext $context, ExecuteData $data ): ExecuteData
+	{
+		sleep( (int) ( $config['seconds'] ?? 0 ) );
+
+		return $data;
+	}
+}
