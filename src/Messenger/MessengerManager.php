eb03574b8180c3a5f1b3427af5fe276d636f492c
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-12-22 16:51:45 +0100

    Support setting max messages per worker

--- src/Messenger/MessengerManager.php
+++ src/Messenger/MessengerManager.php
@@ -18,6 +18,8 @@ class MessengerManager
 	public function __construct(
 		#[Autowire( '%kernel.project_dir%' )]
 		private readonly string $projectDir,
+		#[Autowire( '%env(MESSENGER_LIMIT)%' )]
+		private readonly int $limit,
 		#[Autowire( '%env(MESSENGER_TIME_LIMIT)%' )]
 		private readonly int $timeLimit,
 		private readonly KernelInterface $kernel,
@@ -39,6 +41,10 @@ class MessengerManager
 	public function start(): void
 	{
 		$command = 'php bin/console messenger:consume async --time-limit=' . $this->timeLimit ?? 3600;
+
+		if ( $this->limit ) {
+			$command .= ' --limit=' . $this->limit;
+		}
 
 		// @todo WTF WINDOWS.
 		if ( str_starts_with( strtoupper( PHP_OS ), "WIN" ) ) {
