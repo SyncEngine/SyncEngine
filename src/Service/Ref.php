e163a84efcbd2359617f13662215c19a4d753b67
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-12-27 13:20:46 +0100

    Comments

--- src/Service/Ref.php
+++ src/Service/Ref.php
@@ -6,8 +6,10 @@ class Ref
 {
 	public function create(): string
 	{
-		$ref = base_convert( (string) ( time() * 1000 ), 10, 36 );
-		$ref = $ref . substr( base_convert( (string) preg_replace( '/\D/', '', rand() / getrandmax() ), 10, 36 ), 1 );
+		// Time based.
+		$ref  = base_convert( (string) ( time() * 1000 ), 10, 36 );
+		// Random.
+		$ref .= substr( base_convert( (string) preg_replace( '/\D/', '', rand() / getrandmax() ), 10, 36 ), 1 );
 
 		return $ref;
 	}
