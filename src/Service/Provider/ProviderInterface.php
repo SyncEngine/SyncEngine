0c97a3bef6e04a711e8fe9989a33289d5b31c73a
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-12-28 08:29:05 +0100

    Not nullable

--- src/Service/Provider/ProviderInterface.php
+++ src/Service/Provider/ProviderInterface.php
@@ -7,7 +7,7 @@ use SyncEngine\Model\Abstract\ServiceModel;
 interface ProviderInterface
 {
 	public function get( $name ): ?ServiceModel;
-	public function getAll(): ?array;
+	public function getAll(): array;
 	public function getTypes(): array;
 	public function getNormalized(): array;
 }
