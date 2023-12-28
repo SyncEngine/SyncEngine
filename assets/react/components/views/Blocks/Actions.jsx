29d82d30e84470c21f653b0ed377aef9de992db2
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-12-28 16:02:24 +0100

    Improve icon links

--- assets/react/components/views/Blocks/Actions.jsx
+++ assets/react/components/views/Blocks/Actions.jsx
@@ -56,7 +56,7 @@ export default function Actions( props ) {
 								<EntityModal key={ action.action } entity={ item } savable { ...action }>
 									{ button
 										? <Button variant={ buttonVariant }><span className={ iconClasses } /></Button>
-										: <span className={ iconClasses + "icon-link" } />
+										: <span className={ iconClasses + 'btn p-0' } />
 									}
 								</EntityModal>
 							)
@@ -67,7 +67,7 @@ export default function Actions( props ) {
 								<ExportModal key={ action.action } entity={ item } { ...action }>
 									{ button
 										? <Button variant={ buttonVariant }><span className={ iconClasses } /></Button>
-										: <span className={ iconClasses + "icon-link" } />
+										: <span className={ iconClasses + 'btn p-0' } />
 									}
 								</ExportModal>
 							)
