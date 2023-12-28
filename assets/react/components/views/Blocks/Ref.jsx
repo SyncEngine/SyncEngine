89d87e3ecb0d89699e24efb1429a153ebf2dbd5d
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-12-28 16:02:37 +0100

    Option to copy the ref

--- assets/react/components/views/Blocks/Ref.jsx
+++ assets/react/components/views/Blocks/Ref.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
-import { Tooltip, OverlayTrigger } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Tooltip, OverlayTrigger, Stack } from 'react-bootstrap';
+import useClipboard from '../../../hooks/useClipboard';
 
 export default function Ref( props ) {
+	const [ clipboard, setClipboard ] = useClipboard( 'ref' );
+	const [ copied, setCopied ] = useState( false );
+
+	useEffect( () => {
+		if ( copied ) {
+			setTimeout( () => { setCopied( false ) }, 500 )
+		}
+	}, [ copied ] );
+
 	const {
 		item,
 	} = props;
 
 	return (
 		<OverlayTrigger overlay={ <Tooltip id={ item.ref }>{ item.ref }</Tooltip> }>
-			<span>{ item.id }</span>
+			<Stack direction="horizontal" gap={1}>
+				<span>{ item.id }</span>
+				<small
+					className={ 'btn btn-sm p-0 scale-110-hover transition-all transition-fast bi ' + ( copied ? 'bi-check' : 'bi-copy' ) }
+					onClick={ () => { setClipboard( item.ref ); setCopied( true ) } }
+				/>
+			</Stack>
 		</OverlayTrigger>
 	)
 }
