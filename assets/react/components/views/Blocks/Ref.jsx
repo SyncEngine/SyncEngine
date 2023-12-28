df0f08376d20b8bc0134dfb0f625ed7852f7fa07
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2023-12-28 16:25:23 +0100

    Create CopyToClipboard component

--- assets/react/components/views/Blocks/Ref.jsx
+++ assets/react/components/views/Blocks/Ref.jsx
@@ -1,31 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Tooltip, OverlayTrigger, Stack } from 'react-bootstrap';
-import useClipboard from '../../../hooks/useClipboard';
+import CopyToClipboard from '../../partials/CopyToClipboard';
 
 export default function Ref( props ) {
-	const [ clipboard, setClipboard ] = useClipboard( 'ref' );
-	const [ copied, setCopied ] = useState( false );
-
-	useEffect( () => {
-		if ( copied ) {
-			setTimeout( () => { setCopied( false ) }, 500 )
-		}
-	}, [ copied ] );
-
 	const {
 		item,
 	} = props;
-
-	let btnClass = 'btn btn-sm p-0 scale-110-hover transition-all transition-fast bi ';
-	btnClass += ( copied ? 'bi-check' : ( item.ref === clipboard ) ? 'bi-clipboard-check' : 'bi-copy' );
 
 	return (
 		<OverlayTrigger overlay={ <Tooltip id={ item.ref }>{ item.ref }</Tooltip> }>
 			<Stack direction="horizontal" gap={1}>
 				<span className="bi-c">{ item.id }</span>
-				<small
-					className={ btnClass }
-					onClick={ () => { setClipboard( item.ref ); setCopied( true ) } }
+				<CopyToClipboard
+					className="btn btn-sm p-0"
+					value={ item.ref }
 				/>
 			</Stack>
 		</OverlayTrigger>
