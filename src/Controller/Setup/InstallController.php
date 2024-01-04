a46779fa1bbfe7c358764dafb150891092e0d436
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-01-04 14:36:52 +0100

    Remove unused

--- src/Controller/Setup/InstallController.php
+++ src/Controller/Setup/InstallController.php
@@ -5,12 +5,10 @@ namespace SyncEngine\Controller\Setup;
 use SyncEngine\Controller\DefaultController;
 use SyncEngine\Controller\SystemController;
 use SyncEngine\Service\System;
-use Composer\XdebugHandler\Process;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
 class InstallController extends DefaultController
 {
