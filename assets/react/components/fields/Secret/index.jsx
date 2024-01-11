9571124084638404981149b99168da8cd52d896f
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-01-11 16:41:01 +0100

    Vault service for storing secrets

---
+++ assets/react/components/fields/Secret/index.jsx
@@ -0,0 +1,60 @@
+import React, { useCallback, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { InputGroup } from 'react-bootstrap';
+
+import Text from '../Text';
+import { getTagParts } from '../../../utils/tags';
+import { isEmpty } from '../../../utils/conditions';
+import SelectAdvanced from '../Select/Advanced';
+import { createRefId } from '../../../utils/globals';
+import useGlobal from '../../../hooks/useGlobal';
+
+export default function Secret( props ) {
+	const { t } = useTranslation();
+	const app = useGlobal();
+
+	const {
+		attr = {},
+		id = attr.id ?? createRefId(),
+		value,
+		onChange,
+	} = props;
+
+	const parseSecret = useCallback( ( tag ) => {
+		const parts = getTagParts( tag );
+
+		if ( 'secret' !== parts[0] ) {
+			return null;
+		}
+		return parts[1] ?? null;
+	}, [] );
+
+	const handleChangeSecret = useCallback( ( value ) => {
+		onChange( '{{ secret.' + value + ' }}' );
+	}, [ onChange, id, props.name ] );
+
+	const secret = parseSecret( value );
+
+	const [ custom, setCustom ] = useState( ( ! isEmpty( value ) && isEmpty( secret ) ) );
+
+	const toggleCustom = useCallback( () => setCustom( ! custom ), [ custom ] );
+
+	const customToggleLabel = custom ? t('Switch to secret') :  t('Switch to custom input');
+
+	return (
+		<InputGroup>
+			{ custom ?
+				<Text { ...props } type="text" onChange={ onChange } />
+				:
+				<SelectAdvanced { ...props } onChange={ handleChangeSecret } choices={ app.secrets ?? {} } />
+			}
+			<InputGroup.Text role="button" onClick={ toggleCustom } aria-label={ customToggleLabel } title={ customToggleLabel }>
+				{ custom ?
+					<span className="bi bi-key" />
+					:
+					<span className="bi bi-input-cursor-text" />
+				}
+			</InputGroup.Text>
+		</InputGroup>
+	);
+}
