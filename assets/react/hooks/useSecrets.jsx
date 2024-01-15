46e1e25b20522af318609081ea83b700924ade63
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-01-15 16:24:58 +0100

    useSecrets hook

---
+++ assets/react/hooks/useSecrets.jsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import { fetchPost } from '../utils/fetch';
+import { isEmpty, validate } from '../utils/conditions';
+import useGlobal from './useGlobal';
+import useSyncedState from './useSyncedState';
+
+export default function useSecrets() {
+	const app = useGlobal();
+	const [ secrets, setSecrets ] = useSyncedState( 'update:Secrets', app.secrets ?? {} );
+	const [ loading, setLoading ] = useState( false );
+
+	const endpoints = app.endpoints.secrets;
+
+	const query = async ( endpoint, data, updateState = false ) => {
+		const results =
+			await fetchPost( endpoint, data );
+
+		if ( results.success ) {
+
+			let result = results.data ?? {};
+
+			if ( updateState ) {
+				setSecrets( result );
+			}
+			setLoading( false );
+			return results;
+		}
+
+		setLoading( false );
+		return results.error ?? null;
+	}
+
+	/**
+	 * @returns {Promise<void>}
+	 */
+	const fetch = async () => {
+		return query( endpoints.list, {}, true );
+	}
+
+	const reveal = async () => {
+		return query( endpoints.export, {}, false );
+	}
+
+	const add = async ( key, value ) => {
+		return query( endpoints.add, { key: key, value: value }, true );
+	}
+
+	const remove = async ( key ) => {
+		return query( endpoints.remove, { key: key }, true );
+	}
+
+	const callbacks = {
+		fetch: fetch,
+		add: add,
+		remove: remove,
+		reveal: reveal,
+	}
+
+	return [ secrets, callbacks, loading ];
+}
