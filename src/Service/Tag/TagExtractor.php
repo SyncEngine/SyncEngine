00248b06764d0f3d17aa29aaf8d4444362d766ac
Inform-All <NKoonen@users.noreply.github.com>
2024-01-15 14:49:17 +0100

    Extra check if a single value contains multiple openings

--- src/Service/Tag/TagExtractor.php
+++ src/Service/Tag/TagExtractor.php
@@ -100,7 +100,7 @@ class TagExtractor
 	{
 		$tags  = [];
 
-		if ( str_starts_with( $value, $this->tagStartChar ) && str_ends_with( $value, $this->tagEndChar ) ) {
+		if ( str_starts_with( $value, $this->tagStartChar ) && str_ends_with( $value, $this->tagEndChar ) && substr_count($value,$this->tagEndChar) === 1  ) {
 			// Just a single tag.
 			$value = trim( $value, $this->tagStartChar . ' ' . $this->tagEndChar );
 
