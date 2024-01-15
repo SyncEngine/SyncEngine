6baeb18f8bdf39384ed1fba637f972ab70ee546b
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-01-15 12:07:56 +0100

    Create mediaquery hook instead of global handler

--- assets/react/hooks/useBreakpoint.jsx
+++ assets/react/hooks/useBreakpoint.jsx
@@ -1,23 +1,11 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import useModels from './useModels';
 import useGlobal from './useGlobal';
+import useMediaQuery from './useMediaQuery';
 
 export default function useBreakpoint( breakpoint ) {
 	const breakpoints = useGlobal( 'breakpoints' );
-	const [ match, setMatch ] = useState( breakpoints.match( breakpoint ) );
+	const query = '(min-width: ' + breakpoints.get( breakpoint ) + 'px)';
 
-	const callback = useCallback( ( matches ) => {
-		setMatch( matches );
-	}, [ setMatch ] );
-
-	// Subscribe
-	breakpoints.subscribe( breakpoint, callback );
-
-	useEffect( () => {
-		return function cleanup() {
-			breakpoints.unsubscribe( breakpoint, callback );
-		}
-	}, [] );
-
-	return match;
+	return useMediaQuery( query );
 }
