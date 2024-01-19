7376368518a8066f97a0b53321dda3c90a27773b
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-01-19 12:09:54 +0100

    Let AJAX handle loading

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,16 +2,9 @@
 
 namespace SyncEngine\Controller;
 
-use SyncEngine\Entity\Automation;
-use SyncEngine\Entity\Connection;
-use SyncEngine\Entity\Flow;
-use SyncEngine\Entity\Step;
-use SyncEngine\Entity\Dataset;
-use SyncEngine\Model\TraceModel;
-use SyncEngine\Service\Env;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use SyncEngine\Model\TraceModel;
 
 class IndexController extends DefaultController
 {
@@ -25,14 +18,10 @@ class IndexController extends DefaultController
 			'order' => [ 'created' => 'DESC' ],
 		];
 
-		$traces = TraceModel::getAll( $query );
-
-		$items = array_map( function ( $item ) { return $item->normalize( true ); }, $traces );
-
 		return $this->render( 'admin/dashboard.html.twig', [
 			'traces' => [
 				'query'   => $query,
-				'items'   => $items,
+				'items'   => [],
 				'total'   => TraceModel::getTotalCount( $query ),
 			],
 		] );
