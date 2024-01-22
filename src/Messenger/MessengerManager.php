049def4d20cd4bb66663730350d91d62379630b8
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-01-22 19:44:16 +0100

    Revert "Already done by event listeners"
    This reverts commit 51f3b0e040107ffb59a5222e1c82ee028bfe490e.

--- src/Messenger/MessengerManager.php
+++ src/Messenger/MessengerManager.php
@@ -159,7 +159,6 @@ class MessengerManager implements EventSubscriberInterface
 
 	public function stopAllWorkers(): void
 	{
-		$this->setWorkers( [] );
 		$this->callCommand( [ 'messenger:stop-workers' ] );
 	}
 
