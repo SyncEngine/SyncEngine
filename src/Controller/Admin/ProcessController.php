8014e345188ec6ebeb67470b6e8d6cb6e5c59b3d
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-01-22 21:51:53 +0100

    Simple enable/disable handler in front-end

---
+++ src/Controller/Admin/ProcessController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace SyncEngine\Controller\Admin;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use SyncEngine\Controller\DefaultController;
+use SyncEngine\Messenger\MessengerManager;
+
+class ProcessController extends DefaultController
+{
+	#[Route('system/processes', name: 'system_processes' )]
+	function renderIndex( Request $request, MessengerManager $manager ): Response
+	{
+		$this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+		$html = '';
+
+		if ( $manager->isEnabled() ) {
+			$html .= '<a class="btn btn-warning" href="' . $this->generateUrl( 'system_disable_manager' ) . '">Disable</a>';
+		} else {
+			$html .= '<a class="btn btn-primary" href="' . $this->generateUrl( 'system_enable_manager' ) . '">Enable</a>';
+		}
+
+		return $this->render( 'admin/system/index.html.twig', [
+			'backlink'    => $this->generateUrl( 'system_index' ),
+			'header'      => $this->trans( 'Processes' ),
+			'html'        => $html,
+			'breadcrumbs' => [
+				[
+					'link'  => $this->generateUrl( 'system_index' ),
+					'title' => $this->trans( 'System' ),
+				],
+				[
+					'title'   => $this->trans( 'Vault' ),
+					'current' => true,
+				],
+			],
+		] );
+	}
+
+	#[Route('system/processes/enable', name: 'system_enable_manager' )]
+	function handleEnable( Request $request, MessengerManager $manager ): Response
+	{
+		$this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+		$manager->enableManager();
+
+		return $this->redirectToRoute( 'system_processes' );
+	}
+
+	#[Route('system/processes/disable', name: 'system_disable_manager' )]
+	function handleDisable( Request $request, MessengerManager $manager ): Response
+	{
+		$this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+		$manager->disableManager();
+
+		return $this->redirectToRoute( 'system_processes' );
+	}
+}
