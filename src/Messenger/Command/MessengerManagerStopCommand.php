688906db625a1e85578391c7031dea96aa557b2f
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-01-22 19:30:01 +0100

    Messenger manager start/stop commands

---
+++ src/Messenger/Command/MessengerManagerStopCommand.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace SyncEngine\Messenger\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use SyncEngine\Messenger\MessengerManager;
+
+#[AsCommand(
+	name: 'syncengine:messenger:manager:stop',
+	description: 'Stop SyncEngine Messenger Manager',
+)]
+class MessengerManagerStopCommand extends Command
+{
+	public function __construct( private readonly MessengerManager $manager )
+	{
+		parent::__construct();
+	}
+
+	protected function execute( InputInterface $input, OutputInterface $output )
+	{
+		$this->manager->disableManager();
+		$this->manager->stopAllWorkers();
+
+		return Command::SUCCESS;
+	}
+}
