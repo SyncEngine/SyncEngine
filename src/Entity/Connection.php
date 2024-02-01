6098c95764cf02e42c3e760a090525d406ee79f5
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-02-01 14:19:44 +0100

    Extend entities from abstract
    Combine default props into single abstract entity

--- src/Entity/Connection.php
+++ src/Entity/Connection.php
@@ -3,101 +3,14 @@
 namespace SyncEngine\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use SyncEngine\Entity\Abstract\EngineEntity;
 use SyncEngine\Repository\ConnectionRepository;
 
 #[ORM\Entity( repositoryClass: ConnectionRepository::class )]
-#[UniqueEntity( fields: [ 'ref' ], message: 'There is already a connection with this ref, please enter a different ref' )]
-#[UniqueEntity( fields: [ 'name' ], message: 'There is already a connection with this name, please enter a different name' )]
-class Connection
+class Connection extends EngineEntity
 {
-	#[ORM\Id]
-	#[ORM\GeneratedValue]
-	#[ORM\Column]
-	private ?int $id = null;
-
-	#[ORM\Column( length: 255, unique: true )]
-	private ?string $ref = null;
-
-	#[ORM\Column( length: 255, unique: true )]
-	private ?string $name = null;
-
-	#[ORM\Column( length: 255, nullable: true )]
-	private ?string $description = null;
-
-	#[ORM\Column( length: 255, nullable: true )]
-	private ?string $supervisor = null;
-
-	#[ORM\Column( nullable: true )]
-	private array $config = [];
-
 	#[ORM\Column( nullable: true )]
 	private array $data = [];
-
-	public function getId(): ?int
-	{
-		return $this->id;
-	}
-
-	public function getRef(): ?string
-	{
-		return $this->ref;
-	}
-
-	public function setRef( string $ref ): self
-	{
-		$this->ref = $ref;
-
-		return $this;
-	}
-
-	public function getName(): ?string
-	{
-		return $this->name;
-	}
-
-	public function setName( string $name ): self
-	{
-		$this->name = $name;
-
-		return $this;
-	}
-
-	public function getDescription(): ?string
-	{
-		return $this->description;
-	}
-
-	public function setDescription( ?string $description ): self
-	{
-		$this->description = $description;
-
-		return $this;
-	}
-
-	public function getSupervisor(): ?string
-	{
-		return $this->supervisor;
-	}
-
-	public function setSupervisor( ?string $supervisor ): self
-	{
-		$this->supervisor = $supervisor;
-
-		return $this;
-	}
-
-	public function getConfig(): array
-	{
-		return $this->config;
-	}
-
-	public function setConfig( ?array $config ): self
-	{
-		$this->config = $config;
-
-		return $this;
-	}
 
 	public function getData(): array
 	{
