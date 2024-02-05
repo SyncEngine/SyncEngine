24c2688696d747702ceb0c3548f42a7540af0727
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-02-05 11:31:28 +0100

    Preferences class for user settings/preferences

---
+++ src/Service/Preferences.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace SyncEngine\Service;
+
+use Symfony\Bundle\SecurityBundle\Security;
+use SyncEngine\Entity\User;
+use SyncEngine\Repository\UserRepository;
+use SyncEngine\Service\Interface\SettingsInterface;
+
+class Preferences implements SettingsInterface
+{
+	public function __construct(
+		private readonly Security $security,
+		private readonly UserRepository $repository,
+	) {}
+
+	private function getUser(): ?User
+	{
+		$userId = $this->security->getUser()->getUserIdentifier();
+
+		return $this->repository->find( $userId );
+	}
+
+	public function exists(): bool
+	{
+		return ! empty( $this->getUser() );
+	}
+
+	public function get( string $key ): mixed
+	{
+		return $this->getUser()->getSetting( $key );
+	}
+
+	public function set( string $key, mixed $value ): static
+	{
+		$this->getUser()->setSetting( $key, $value );
+
+		return $this;
+	}
+
+	public function fetch(): ?array
+	{
+		return $this->getUser()->getSettings();
+	}
+
+	public function update( string $key, mixed $value ): bool
+	{
+		$this->set( $key, $value );
+		return $this->persist();
+	}
+
+	public function persist(): bool
+	{
+		$this->repository->save( $this->getUser(), true );
+	}
+}
