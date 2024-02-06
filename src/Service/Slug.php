d4e00cd0e995c9640a2ebc5bc5254e4260fcf17b
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-02-06 14:15:59 +0100

    Use Symfony string component

--- src/Service/Slug.php
+++ src/Service/Slug.php
@@ -2,32 +2,12 @@
 
 namespace SyncEngine\Service;
 
+use Symfony\Component\String\Slugger\AsciiSlugger;
+
 class Slug
 {
 	public function slugify( string $text ): string
 	{
-		// replace non letter or digits by -
-		$text = preg_replace( '~[^\pL\d]+~u', '-', $text );
-
-		// transliterate
-		$text = iconv( 'utf-8', 'us-ascii//TRANSLIT', $text );
-
-		// remove unwanted characters
-		$text = preg_replace( '~[^-\w]+~', '', $text );
-
-		// trim
-		$text = trim( $text, '-' );
-
-		// remove duplicated - symbols
-		$text = preg_replace( '~-+~', '-', $text );
-
-		// lowercase
-		$text = strtolower( $text );
-
-		if ( empty( $text ) ) {
-			return 'n-a';
-		}
-
-		return $text;
+		return ( new AsciiSlugger() )->slug( $text );
 	}
 }
