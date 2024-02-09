5cbd88e683f7a3da47632e1172227f23f28e6fc9
Inform-All <NKoonen@users.noreply.github.com>
2024-02-09 11:43:38 +0100

    Add info log for retrieve and send

--- src/Task/Send.php
+++ src/Task/Send.php
@@ -53,6 +53,10 @@ class Send extends AbstractRequest
 			}
 
 			$context->addLog( 'Response info for Task: ' . $config['_ref'], $result->getResponse() );
+			if(!empty($result->getInfo()))
+			{
+				$context->addLog($result->getInfo());
+			}
 		} catch ( \Throwable $e ) {
 			$context->addError( $e, $data->get() );
 		}
