519e9639ec1458a351610690a6a39b7111c6a6dd
Inform-All <NKoonen@users.noreply.github.com>
2024-02-15 16:18:04 +0100

    wsdl not wdsl

--- src/Webservice/Soap.php
+++ src/Webservice/Soap.php
@@ -28,18 +28,18 @@ class Soap extends WebserviceModel
 				'label' => $this->trans( 'Host',[],"soap+intl-icu" ),
 				'type'  => 'text',
 			],
-			'wdsl_mode'     => [
+			'wsdl_mode'     => [
 				'label'    => $this->trans( 'Wsdl_mode',[],"soap+intl-icu" ),
 				'type'     => 'switch',
 				'expanded' => false,
 				'help'     => $this->trans( 'Use_wsdl',[],"soap+intl-icu" ),
 			],
-			'wdsl_url'      => [
+			'wsdl_url'      => [
 				'label'      => $this->trans( 'Wsdl_file_url',[],"soap+intl-icu" ),
 				'type'       => 'text',
 				'help'       => $this->trans( 'Wsdl_url_info',[],"soap+intl-icu" ),
 				'conditions' => [
-					'wdsl_mode' => true,
+					'wsdl_mode' => true,
 				],
 			],
 			'soap_initiate' => [
@@ -84,8 +84,8 @@ class Soap extends WebserviceModel
 
 	public function retrieve( array $config, $data = null ): Result
 	{
-		$wdsl_url   = empty( $config['wdsl_mode'] ) ? null : $config['wdsl_url'];
-		$soapClient = new \SoapClient( $wdsl_url, [ 'trace' => 1, 'exception' => 0, 'features' => SOAP_SINGLE_ELEMENT_ARRAYS ] );
+		$wsdl_url   = empty( $config['wsdl_mode'] ) ? null : $config['wsdl_url'];
+		$soapClient = new \SoapClient( $wsdl_url, [ 'trace' => 1, 'exception' => 0, 'features' => SOAP_SINGLE_ELEMENT_ARRAYS ] );
 
 		$soapClient->__setSoapHeaders( $this->setSoapHeaders( $config ) );
 		$result = $soapClient->__soapCall( $config['soap_initiate'], [$config['soap_initiate'] => $config['call_data']] );
