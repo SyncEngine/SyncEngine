99252a5ac1d0e862f00cdc405e38699ba284c3f3
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-03-04 14:32:55 +0100

    Create ColumnModel.php

---
+++ src/Model/ColumnModel.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace SyncEngine\Model;
+
+use SyncEngine\Column\Type\ColumnTypeInterface;
+use SyncEngine\Model\Abstract\ServiceModel;
+use SyncEngine\Model\Interface\Configurable;
+use SyncEngine\Model\Trait\Config;
+use SyncEngine\Service\Interface\FormatInterface;
+
+abstract class ColumnModel extends ServiceModel implements Configurable
+{
+	use Config;
+
+	const SERVICE = 'Columns';
+
+	/**
+	 * The type of task, can be used for categorizing.
+	 *
+	 * @var string|ColumnTypeInterface
+	 */
+	public string|ColumnTypeInterface $type = '';
+
+	/**
+	 * Human-readable name used in the interface.
+	 *
+	 * @var string
+	 */
+	public string $name = '';
+
+	/**
+	 * Human-readable description used in the interface.
+	 *
+	 * @var string
+	 */
+	public string $description = '';
+
+	public function getType(): string
+	{
+		return $this->type instanceof ColumnTypeInterface ? $this->type->getType() : $this->type;
+	}
+
+	public function getName(): string
+	{
+		return $this->name;
+	}
+
+	public function getDescription(): string
+	{
+		return $this->description;
+	}
+
+	abstract public function format( $value );
+	abstract public function getFormatter(): FormatInterface;
+
+	public function getFields(): array
+	{
+		return [];
+	}
+
+	public function normalize(): array
+	{
+		$props = [
+			'_class'      => $this->getClassLocator(),
+			'type'        => $this->getType(),
+			'name'        => $this->getName(),
+			'description' => $this->getDescription(),
+			'fields'      => $this->getFields(),
+		];
+
+		if ( $this->isFromModule() ) {
+			$props['module'] = $this->getModule()->getName();
+		}
+
+		return $props;
+	}
+}
