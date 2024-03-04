c575f44da85948ae26cfa463672bdbbee67a4245
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-03-04 14:32:04 +0100

    Basic column types

---
+++ src/Column/Type/StringColumnType.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace SyncEngine\Column\Type;
+
+class StringColumnType extends GenericColumnType
+{
+	const TYPE = 'string';
+	const NAME = 'String';
+	const DESCRIPTION = 'Columns that are text based.';
+}
