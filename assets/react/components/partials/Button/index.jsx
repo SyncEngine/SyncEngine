e9fbdbacc4954822ecc140969bc98744a04b301c
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-03-05 21:30:12 +0100

    Fix prop fallbacks

--- assets/react/components/partials/Button/index.jsx
+++ assets/react/components/partials/Button/index.jsx
@@ -3,25 +3,25 @@ import { Button as ButtonBootstrap } from 'react-bootstrap';
 
 export default forwardRef( function Button( props, ref ) {
 
-	const overrides = {};
-	if ( props.variant ) {
-		overrides.variant = props.variant;
-		overrides.className = props.className ?? '';
-		// Bootstrap compatibility.
-		const outline = overrides.variant.startsWith('outline');
-		const variant  = ( outline ) ? overrides.variant.substring( 0, 8 ) : overrides.variant;
-		if ( props.subtle ) {
-			overrides.className = overrides.className + ' btn-subtle';
-			overrides.subtle = null;
-		}
-		if ( props.outline && ! outline ) {
-			overrides.variant = 'outline-' + variant;
-			overrides.outline = null;
-		}
-		if ( props.emphasis ) {
-			overrides.className += ' btn-emphasis';
-			overrides.emphasis = null;
-		}
+	const overrides = {
+		variant: props.variant ?? 'primary'
+	};
+
+	overrides.className = props.className ?? '';
+	// Bootstrap compatibility.
+	const outline = overrides.variant.startsWith('outline');
+	const variant  = ( outline ) ? overrides.variant.substring( 0, 8 ) : overrides.variant;
+	if ( props.subtle ) {
+		overrides.className = overrides.className + ' btn-subtle';
+		overrides.subtle = null;
+	}
+	if ( props.outline && ! outline ) {
+		overrides.variant = 'outline-' + variant;
+		overrides.outline = null;
+	}
+	if ( props.emphasis ) {
+		overrides.className += ' btn-emphasis';
+		overrides.emphasis = null;
 	}
 
 	return (
