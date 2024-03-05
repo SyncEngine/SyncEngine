54a46c75a0980d10162a6bceb3567f1788f9199f
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-03-05 21:30:12 +0100

    Fix prop fallbacks

--- assets/react/components/partials/Badge/index.jsx
+++ assets/react/components/partials/Badge/index.jsx
@@ -3,29 +3,29 @@ import { Badge as BadgeBootstrap } from 'react-bootstrap';
 
 export default forwardRef( function Badge( props, ref ) {
 
-	const overrides = {};
-	if ( props.bg ) {
-		overrides.bg = props.bg;
-		if ( props.subtle ) {
-			if ( ! props.text ) {
-				overrides.text = overrides.bg + '-emphasis';
-			}
-			overrides.bg = overrides.bg + '-subtle';
-			overrides.subtle = null;
+	const overrides = {
+		bg: props.bg ?? 'primary'
+	};
+
+	if ( props.subtle ) {
+		if ( ! props.text ) {
+			overrides.text = overrides.bg + '-emphasis';
 		}
-		if ( props.outline ) {
-			if ( ! props.text && ! overrides.text ) {
-				overrides.text = overrides.bg;
-			}
-			if ( ! props.subtle ) {
-				overrides.bg = '';
-			}
-			overrides.className = props.className + ' border border-' + props.bg;
-			if ( 1 < props.outline ) {
-				overrides.className += ' border-' + props.outline;
-			}
-			overrides.outline = null;
+		overrides.bg = overrides.bg + '-subtle';
+		overrides.subtle = null;
+	}
+	if ( props.outline ) {
+		if ( ! props.text && ! overrides.text ) {
+			overrides.text = overrides.bg;
 		}
+		if ( ! props.subtle ) {
+			overrides.bg = '';
+		}
+		overrides.className = props.className + ' border border-' + props.bg;
+		if ( 1 < props.outline ) {
+			overrides.className += ' border-' + props.outline;
+		}
+		overrides.outline = null;
 	}
 
 	return (
