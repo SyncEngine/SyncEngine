fa05758771a513904617b296123c544d6605dce5
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-03-06 20:23:08 +0100

    Improve iterator info popover

--- assets/react/components/services/Trace/index.jsx
+++ assets/react/components/services/Trace/index.jsx
@@ -2,8 +2,8 @@ import React, { useRef } from 'react';
 import { Stack, Tab, Tabs } from 'react-bootstrap';
 import { objectToMappable } from '../../../utils/data';
 import Trace from './Trace';
-import { ParentContext } from '../../../context/ParentContext';
 import OverlayToggle from '../OverlayToggle';
+import TraceLog from './Log';
 
 export default function TraceControl( props ) {
 	const {
@@ -13,31 +13,29 @@ export default function TraceControl( props ) {
 	const parent = useRef();
 
 	return (
-		<Stack className="pt-2 mw-100" ref={ parent }>
-			<ParentContext.Provider value={ { container: parent } }>
-				<Tabs gap={2}>
-					{
-						objectToMappable( data ).map( ( row, index ) => {
-							const {
-								iterator = {},
-								trace = {},
-							} = row;
+		<Stack className="pt-2 mw-100 overflow-hidden" ref={ parent }>
+			<Tabs gap={2}>
+				{
+					objectToMappable( data ).map( ( row, index ) => {
+						const {
+							iterator = {},
+							trace = {},
+						} = row;
 
-							const title = iterator ? (
-								<OverlayToggle overlay={ iterator } trigger={ [ 'hover', 'focus' ] } raw>
-									<span>{ iterator.current }<span className="bi bi-info"/></span>
-								</OverlayToggle>
-							) : index;
+						const title = iterator ? (
+							<OverlayToggle overlay={ <div><TraceLog data={ iterator } /></div> } trigger={ [ 'hover' ] }>
+								<div>{ iterator.current }<span className="bi bi-info"/></div>
+							</OverlayToggle>
+						) : index;
 
-							return (
-								<Tab key={ index } eventKey={ index } title={ title }>
-									<Trace data={ trace } />
-								</Tab>
-							)
-						} )
-					}
-				</Tabs>
-			</ParentContext.Provider>
+						return (
+							<Tab key={ index } eventKey={ index } title={ title }>
+								<Trace data={ trace } />
+							</Tab>
+						)
+					} )
+				}
+			</Tabs>
 		</Stack>
 	);
 }
