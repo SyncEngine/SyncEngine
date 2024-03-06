33593975ba18383e3b32ad98986e2f500e137f00
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-03-06 13:05:46 +0100

    Togglable view type

---
+++ assets/react/components/services/ResponseTabs/Content.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import Code from '../../fields/Code';
+import { deepClone } from '../../../utils/data';
+import TraceControl from '../Trace';
+import useToggle from '../../../hooks/useToggle';
+import { Button, ButtonGroup, OverlayTrigger, Stack, Tooltip } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
+
+export default function ResponseTabContent( props ) {
+	const { t } = useTranslation();
+	const [ raw, toggleRaw ] = useToggle( false );
+
+	const {
+		name,
+		content,
+		contained,
+	} = props;
+
+	const json = 'object' === typeof content;
+
+	let view = null;
+	switch ( name.toLowerCase() ) {
+		case 'trace':
+			view = <TraceControl data={ deepClone( content ) } />
+			break;
+	}
+
+	const toggle = view && (
+		<div className="position-absolute top-0 end-0 mt-2 z-1">
+			<ButtonGroup className="justify-content-end">
+				<OverlayTrigger
+					key={ raw ? 'raw' : 'display' }
+					overlay={ <Tooltip id="export-format">{ raw ? t( 'Raw' ) : t( 'Display' ) }</Tooltip> }
+				>
+					<Button variant={ raw ? 'secondary' : 'outline-secondary ' } onClick={ toggleRaw }>
+						<span className={ "bi bi-" + ( raw ? 'eye' : 'code' ) } />
+					</Button>
+				</OverlayTrigger>
+			</ButtonGroup>
+		</div>
+	)
+
+	return (
+		<Stack gap={2} className="mw-100 position-relative">
+		{ toggle }
+		{
+			( raw || ! view )
+			?
+			<Code
+				contained={ contained }
+				height="100%"
+				language={ json ? 'json' : undefined }
+				editable={ false }
+				value={ json ? JSON.stringify( content, null, 2 ) : content }
+			/>
+			:
+			view
+		}
+		</Stack>
+	);
+}
