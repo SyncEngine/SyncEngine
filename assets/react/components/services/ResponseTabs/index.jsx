e66800f6e5d190aca5835b479b7f7ae76dc89ecf
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-03-06 13:05:46 +0100

    Togglable view type

--- assets/react/components/services/ResponseTabs/index.jsx
+++ assets/react/components/services/ResponseTabs/index.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { Tab, Tabs } from 'react-bootstrap';
-import Code from '../../fields/Code';
 import { deepClone, objectToMappable } from '../../../utils/data';
 import { ucfirst } from '../../../utils/globals';
-import TraceControl from '../Trace';
+import ResponseTabContent from './Content';
 
 export default function ResponseTabs( props ) {
 	return (
@@ -11,28 +10,9 @@ export default function ResponseTabs( props ) {
 			<Tabs>
 				{
 					objectToMappable( deepClone( props.data ), 'name', 'content', true ).map( tab => {
-						const json = 'object' === typeof tab.content;
-
-						let view = null;
-						switch ( tab.name.toLowerCase() ) {
-							case 'trace':
-								view = <TraceControl data={ deepClone( tab.content ) } />
-								break;
-							default:
-								view =
-									<Code
-										contained={ props.contained }
-										height="100%"
-										language={ json ? 'json' : undefined }
-										editable={ false }
-										value={ json ? JSON.stringify( tab.content, null, 2 ) : tab.content }
-									/>;
-								break;
-						}
-
 						return (
 							<Tab eventKey={ tab.name } key={ tab.name } title={ ucfirst( tab.name ) } className={ props.contained && "overflow-hidden w-100" }>
-								{ view }
+								<ResponseTabContent { ...tab } contained={ props.contained } />
 							</Tab>
 						)
 					} )
