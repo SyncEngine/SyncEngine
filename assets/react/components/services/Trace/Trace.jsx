7e90a3aa036574ed72073945ddd804f7dc5f5107
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-03-06 09:21:52 +0100

    Support for start and end time

--- assets/react/components/services/Trace/Trace.jsx
+++ assets/react/components/services/Trace/Trace.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { objectToMappable } from '../../../utils/data';
-import TraceLog from './Log';
 import Badge from '../../partials/Badge';
 import AccordionSticky from '../../partials/AccordionSticky';
+import TraceLog from './Log';
+import { objectToMappable } from '../../../utils/data';
 
 export default function Trace( props ) {
 	const {
@@ -18,14 +18,28 @@ export default function Trace( props ) {
 						info,
 						count = 1,
 						trace = {},
+						time_enter,
+						time_leave,
 					} = step;
+
+					const start = time_enter && new Date( time_enter * 1000 );
+					const end = time_leave && new Date( time_leave * 1000 );
 
 					const title = step.message ?? ( 'string' === typeof info ? info : step._key );
 
 					return (
 						<AccordionSticky.Item eventKey={ step._key } key={ step._key }>
-							<AccordionSticky.Header>{ title }<Badge className="ms-2" subtle>{ count }x</Badge></AccordionSticky.Header>
+							<AccordionSticky.Header>
+								{ title }
+								<Badge className="ms-2" subtle>{ count }x</Badge>
+							</AccordionSticky.Header>
 							<AccordionSticky.Body>
+								{ start &&
+								    <small className="d-block mb-1">
+									    { start.toLocaleString() }
+									    { end && ' - ' + end.toLocaleString() }
+								    </small>
+								}
 								{
 									( step._key.startsWith( 'Log:' ) || step._key.startsWith( 'Error:' ) )
 									?
