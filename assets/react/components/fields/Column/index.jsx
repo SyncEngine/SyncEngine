b8b24957a69733438948fb33552d84a9f0adb7ae
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-03-07 14:53:31 +0100

    Column field control

--- assets/react/components/fields/Column/index.jsx
+++ assets/react/components/fields/Column/index.jsx
@@ -1,10 +1,64 @@
-import React from 'react';
-import Input from '../Input';
-
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { InputGroup } from 'react-bootstrap';
+import LoadingPlaceholder from '../../partials/Loading/Placeholder';
+import useColumns from '../../../hooks/useColumns';
+import Fields from '../../form/Fields';
+import OverlayToggle from '../../services/OverlayToggle';
+import SelectSimple from '../Select/Simple';
 
 export default function Column( props ) {
+	const { t } = useTranslation();
 
+	const {
+		onChange,
+	} = props;
 
+	const config = { ...props.value };
+	const [ selectedColumn, setSelectedColumn ] = useState( ( config._class ?? '' ) );
+	const [ columnTypes ] = useColumns( props.columnTypes, props.query ?? {} );
 
-	return <Input />
+	if ( null === columnTypes ) {
+		return <LoadingPlaceholder/>
+	}
+
+	const selectColumn = ( type ) => {
+		setSelectedColumn( type );
+
+		config._class = type;
+		onChange( config );
+	}
+
+	const updateColumn = ( webservice ) => {
+		if ( selectedColumn ) {
+			webservice._class = selectedColumn;
+		}
+		onChange( webservice );
+	}
+
+	const getColumnFields = () => {
+		if ( columnTypes[ selectedColumn ] ) {
+			return {
+				...columnTypes[ selectedColumn ].fields ?? null,
+			}
+		}
+		return null;
+	}
+
+	const fields = getColumnFields();
+
+	return (
+		<div className="flex-grow-1">
+			<InputGroup className="flex-nowrap">
+				<SelectSimple options={ columnTypes } onChange={ selectColumn } value={ selectedColumn } />
+				{ fields &&
+					<OverlayToggle overlay={ <div><Fields fields={ fields } value={ config } onChange={ updateColumn } /></div> }>
+						<InputGroup.Text role="button">
+							<span className="bi bi-gear" />
+						</InputGroup.Text>
+					</OverlayToggle>
+				}
+			</InputGroup>
+		</div>
+	);
 }
