5f52fdaa446fc0e1dd66d3d7eb5618216847561f
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-03-11 19:43:22 +0100

    Improve trace UI and data

--- assets/react/components/services/Trace/Trace.jsx
+++ assets/react/components/services/Trace/Trace.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
+import { Stack } from 'react-bootstrap';
 import Badge from '../../partials/Badge';
+import useDateFormatter from '../../../hooks/useDateFormatter';
 import AccordionSticky from '../../partials/AccordionSticky';
 import TraceLog from './Log';
 import { objectToMappable } from '../../../utils/data';
-import useDateFormatter from '../../../hooks/useDateFormatter';
+import { useTranslation } from 'react-i18next';
 
 export default function Trace( props ) {
+	const { t } = useTranslation();
 	const dateFormatter = useDateFormatter();
 
 	const {
 		data,
+		accordionProps = {},
 	} = props;
 
 	return (
-		<AccordionSticky className="w-100">
+		<AccordionSticky className="w-100" { ...accordionProps }>
 			{
 				objectToMappable( data, '_key', 'message' ).map( ( step, index ) => {
 
@@ -34,19 +38,26 @@ export default function Trace( props ) {
 						start = step._key.split( ' ' )[1] * 1000;
 					}
 
-
 					return (
-						<AccordionSticky.Item eventKey={ step._key } key={ step._key }>
+						<AccordionSticky.Item eventKey={ index } key={ step._key }>
 							<AccordionSticky.Header>
 								{ title }
 								<Badge className="ms-2" subtle>{ count }x</Badge>
 							</AccordionSticky.Header>
 							<AccordionSticky.Body>
 								{ start &&
-								    <small className="d-block mb-1">
-									    { dateFormatter.format( start ) }
-									    { end && ' - ' + dateFormatter.format( end ) }
-								    </small>
+									<Stack direction="horizontal" gap={2} className="flex-wrap mb-2">
+										<Badge bg="info" subtle title={ t( 'Start' ) }>
+											<span className="bi bi-calendar me-2" />
+											{ dateFormatter.format( start ) }
+										</Badge>
+										{ end &&
+										    <Badge bg="info" subtle title={ t( 'End' ) }>
+												<span className="bi bi-calendar-check-fill me-2" />
+												{ dateFormatter.format( end ) }
+											</Badge>
+										}
+									</Stack>
 								}
 								{ isLog ? <TraceLog data={ step } /> : <Trace data={ trace } /> }
 							</AccordionSticky.Body>
