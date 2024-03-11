4cb3d4ff59946a554b78e7d4954247dd43e18cf2
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-03-11 19:43:22 +0100

    Improve trace UI and data

--- assets/react/components/services/Trace/index.jsx
+++ assets/react/components/services/Trace/index.jsx
@@ -1,11 +1,15 @@
 import React, { useRef } from 'react';
-import { Stack, Tab, Tabs } from 'react-bootstrap';
-import { objectToMappable } from '../../../utils/data';
-import Trace from './Trace';
+import { Card, Stack, Tab, Tabs } from 'react-bootstrap';
+import useDateFormatter from '../../../hooks/useDateFormatter';
+import Badge from '../../partials/Badge';
 import OverlayToggle from '../OverlayToggle';
+import Trace from './Trace';
 import TraceLog from './Log';
+import { objectToMappable } from '../../../utils/data';
 
 export default function TraceControl( props ) {
+	const dateFormatter = useDateFormatter();
+
 	const {
 		data,
 	} = props;
@@ -20,17 +24,33 @@ export default function TraceControl( props ) {
 						const {
 							iterator = {},
 							trace = {},
+							time_start,
 						} = row;
 
-						const title = iterator ? (
-							<OverlayToggle overlay={ <div><TraceLog data={ iterator } /></div> } trigger={ [ 'hover' ] }>
-								<div>{ iterator.current }<span className="bi bi-info"/></div>
-							</OverlayToggle>
-						) : index;
-
 						return (
-							<Tab key={ index } eventKey={ index } title={ title }>
-								<Trace data={ trace } />
+							<Tab key={ index } eventKey={ index } title={ iterator.current }>
+								<Card className="w-100">
+									<Card.Header>
+										<Stack direction="horizontal" gap={2} className="flex-wrap">
+											{ iterator &&
+												<OverlayToggle overlay={ <div><TraceLog data={ iterator } /></div> } trigger={ [ 'hover' ] }>
+													<Badge>
+														<span className="bi bi-info-circle me-2"/>
+														Iteration { iterator.current }
+														<span className="bi bi-hash mx-2" />
+														{ iterator.offset } - { iterator.offset + iterator.limit }
+													</Badge>
+												</OverlayToggle>
+											}
+											{ time_start &&
+											    <Badge><span className="bi bi-calendar me-2" />{ dateFormatter.format( time_start * 1000 ) }</Badge>
+											}
+										</Stack>
+									</Card.Header>
+									<Card.Body>
+										<Trace data={ trace } accordionProps={ { defaultActiveKey: 0 } } />
+									</Card.Body>
+								</Card>
 							</Tab>
 						)
 					} )
