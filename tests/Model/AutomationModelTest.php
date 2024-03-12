40b4aa5d0527638b92865aa0e65b1b26c12206a5
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-03-12 15:31:49 +0100

    Create iterator tests

---
+++ tests/Model/AutomationModelTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace SyncEngine\Tests\Model;
+
+
+use SyncEngine\Model\AutomationModel;
+use SyncEngine\Tests\TestCase\BaseTestCase;
+
+class AutomationModelTest extends BaseTestCase
+{
+	public function testIterator(): void
+	{
+		$model = AutomationModel::create();
+
+		// Without limit.
+
+		$compare = [
+			'current' => 0,
+			'index'   => -1, // @todo implement index.
+			'limit'   => 0,
+			'offset'  => 0,
+		];
+
+		$this->assertEquals( $compare, $model->getIterator() );
+
+		$model->nextIteration();
+
+		$compare = [
+			'current' => 1,
+			'index'   => 0, // @todo implement index.
+			'limit'   => 0,
+			'offset'  => 0,
+		];
+
+		$this->assertEquals( $compare, $model->getIterator() );
+
+		// With limit.
+
+		$model->endIterator();
+		$model->setLimit( 50 );
+
+		$compare = [
+			'current' => 0,
+			'index'   => -1, // @todo implement index.
+			'limit'   => 50,
+			'offset'  => 0,
+		];
+
+		$this->assertEquals( $compare, $model->getIterator() );
+
+		$model->nextIteration();
+
+		$compare = [
+			'current' => 1,
+			'index'   => 0, // @todo implement index.
+			'limit'   => 50,
+			'offset'  => 0,
+		];
+
+		$this->assertEquals( $compare, $model->getIterator() );
+
+		$model->nextIteration();
+
+		$compare = [
+			'current' => 2,
+			'index'   => 1, // @todo implement index.
+			'limit'   => 50,
+			'offset'  => 50,
+		];
+
+		$this->assertEquals( $compare, $model->getIterator() );
+	}
+}
