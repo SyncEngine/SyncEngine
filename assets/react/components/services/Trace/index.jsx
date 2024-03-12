00cec12a16de2d7150a6c148e96f8ded11fc99d3
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-03-12 13:52:10 +0100

    Improve trace components

--- assets/react/components/services/Trace/index.jsx
+++ assets/react/components/services/Trace/index.jsx
@@ -1,16 +1,9 @@
 import React, { useCallback, useRef, useState } from 'react';
-import { Card, Stack, Tab, Tabs } from 'react-bootstrap';
-import useDateFormatter from '../../../hooks/useDateFormatter';
-import Badge from '../../partials/Badge';
-import OverlayToggle from '../OverlayToggle';
-import Trace from './Trace';
-import TraceLog from './Log';
+import { Stack, Tab, Tabs } from 'react-bootstrap';
 import { objectToMappable } from '../../../utils/data';
-import { useTranslation } from 'react-i18next';
+import TraceContainer from './Container';
 
 export default function TraceControl( props ) {
-	const { t } = useTranslation();
-	const dateFormatter = useDateFormatter();
 	const [ selected, setSelected ] = useState();
 
 	const {
@@ -67,7 +60,6 @@ export default function TraceControl( props ) {
 							time_start,
 						} = row;
 
-
 						const errors = [];
 						const logs = [];
 
@@ -84,41 +76,8 @@ export default function TraceControl( props ) {
 						}  );
 
 						return (
-							<Tab key={ index } eventKey={ index } title={ iterator.current }>
-								<Card className="w-100">
-									<Card.Header>
-										<Stack direction="horizontal" gap={2} className="flex-wrap">
-											{ iterator &&
-												<OverlayToggle overlay={ <div><TraceLog data={ iterator } /></div> } trigger={ [ 'hover' ] }>
-													<Badge>
-														<span className="bi bi-info-circle me-2"/>
-														Iteration { iterator.current }
-														<span className="bi bi-hash mx-2" />
-														{ iterator.offset } - { iterator.offset + iterator.limit }
-													</Badge>
-												</OverlayToggle>
-											}
-											{ time_start &&
-											    <Badge><span className="bi bi-calendar me-2" />{ dateFormatter.format( time_start * 1000 ) }</Badge>
-											}
-										</Stack>
-									</Card.Header>
-									<Card.Body>
-										<Trace data={ traceData } accordionProps={ { defaultActiveKey: 0 } } />
-									</Card.Body>
-									{ ( logs.length > 0 ) &&
-										<Card.Body>
-											<Card.Title>{ t('Logs') }</Card.Title>
-											<Trace data={ logs } />
-										</Card.Body>
-									}
-									{ ( errors.length > 0 ) &&
-										<Card.Body>
-											<Card.Title>{ t('Errors') }</Card.Title>
-											<Trace data={ errors } />
-										</Card.Body>
-									}
-								</Card>
+							<Tab key={ index } eventKey={ index } title={ iterator.current ?? index }>
+								<TraceContainer { ...row } trace={ traceData } errors={ errors } logs={ logs } />
 							</Tab>
 						)
 					} )
