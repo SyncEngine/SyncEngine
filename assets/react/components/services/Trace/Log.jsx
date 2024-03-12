445944edb0134bf221ee05996e049b0c5c511d6e
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-03-12 16:33:02 +0100

    Toggle raw view

--- assets/react/components/services/Trace/Log.jsx
+++ assets/react/components/services/Trace/Log.jsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import Code from '../../fields/Code';
 
 export default function TraceLog( props ) {
+
+	const removeRefs = useCallback( ( data ) => {
+		if ( Array.isArray( data ) ) {
+			return data.map( item => removeRefs( item ) );
+		}
+		if ( 'object' !== typeof data ) {
+			return data;
+		}
+		data = { ...data };
+
+		if ( data.hasOwnProperty( 'trace' ) ) {
+			data.trace = removeRefs( data.trace );
+		}
+		if ( data.hasOwnProperty( '_ref' ) ) {
+			delete data._ref;
+		}
+
+		return data;
+	} )
+
 	return (
-		<Code value={ JSON.stringify( props.data, null, 2 ) } language="json" readOnly />
+		<Code value={ JSON.stringify( removeRefs( props.data ), null, 2 ) } language="json" readOnly />
 	);
 }
--- assets/react/components/services/Trace/Trace.jsx
+++ assets/react/components/services/Trace/Trace.jsx
@@ -1,77 +1,64 @@
 import React from 'react';
-import { Stack } from 'react-bootstrap';
-import Badge from '../../partials/Badge';
+import { Button, OverlayTrigger, Stack, Tooltip } from 'react-bootstrap';
 import useDateFormatter from '../../../hooks/useDateFormatter';
-import AccordionSticky from '../../partials/AccordionSticky';
 import TraceLog from './Log';
+import Traces from './Traces';
 import { useTranslation } from 'react-i18next';
+import useToggle from '../../../hooks/useToggle';
 
 export default function Trace( props ) {
 	const { t } = useTranslation();
 	const dateFormatter = useDateFormatter();
 
 	const {
-		data = [],
+		item = {},
 		ancestors = [],
-		accordionProps = {},
 	} = props;
 
+	const {
+		trace = {},
+		_timestamp: timestamp,
+		_isLog: isLog,
+		_isError: isError,
+	} = item;
+
+	const [ raw, toggleRaw ] = useToggle( isLog || isError );
+
+	const toggle = ! ( isLog || isError ) && (
+		<OverlayTrigger
+			key={ raw ? 'raw' : 'display' }
+			overlay={ <Tooltip id="export-format">{ raw ? t( 'Display' ) : t( 'Raw' ) }</Tooltip> }
+		>
+			<Button size="sm" variant={ raw ? 'secondary' : 'outline-secondary ' } onClick={ toggleRaw }>
+				<span className={ "bi bi-" + ( raw ? 'eye' : 'code' ) } />
+			</Button>
+		</OverlayTrigger>
+	)
+
+	let start = timestamp[0] ?? timestamp;
+	let end = timestamp[1] ?? null;
+
 	return (
-		<AccordionSticky className="w-100" { ...accordionProps }>
-			{
-				data.map( ( step, index ) => {
-
-					const {
-						title,
-						info,
-						name,
-						type,
-						ref,
-						count = 1,
-						trace = {},
-						_timestamp: timestamp,
-						_isLog: isLog,
-						_isError: isError,
-					} = step;
-
-					ancestors.push( step );
-
-					let start = timestamp[0] ?? timestamp;
-					let end = timestamp[1] ?? null;
-
-					return (
-						<AccordionSticky.Item eventKey={ index } key={ step._key } ref={ step._ref }>
-							<AccordionSticky.Header>
-								{ title }
-								{ type && <Badge className="ms-2" subtle>{ type }</Badge> }
-								{ ref && <Badge className="ms-2" subtle>{ ref }</Badge> }
-								<Badge className="ms-2" subtle>{ count }x</Badge>
-								{ end &&
-								  <Badge className="ms-2" subtle>{ Math.round( end - start ) }ms</Badge>
-								}
-							</AccordionSticky.Header>
-							<AccordionSticky.Body>
-								{ start &&
-									<Stack direction="horizontal" gap={2} className="flex-wrap mb-2">
-										<small title={ t( 'Start' ) }>
-											<span className="bi bi-calendar me-2" />
-											{ dateFormatter.format( start ) }
-										</small>
-										{ end &&
-										    <small title={ t( 'End' ) }>
-												<span className="bi bi-calendar-check-fill me-2" />
-												{ dateFormatter.format( end ) }
-											</small>
-										}
-									</Stack>
-								}
-								{ step.message && <small className="mb-2">{ step.message }</small> }
-								{ ( isLog || isError ) ? <TraceLog data={ step } /> : <Trace data={ trace } ancestors={ ancestors } /> }
-							</AccordionSticky.Body>
-						</AccordionSticky.Item>
-					);
-				} )
-			}
-		</AccordionSticky>
+		<div className="position-relative">
+			<Stack direction="horizontal" gap={2} className="mb-2 justify-content-between">
+				<Stack direction="horizontal" gap={2} className="flex-wrap">
+				{ start &&
+					<small title={ t( 'Start' ) }>
+						<span className="bi bi-calendar me-2" />
+						{ dateFormatter.format( start ) }
+					</small>
+				}
+				{ end &&
+					<small title={ t( 'End' ) }>
+						<span className="bi bi-calendar-check-fill me-2" />
+						{ dateFormatter.format( end ) }
+					</small>
+				}
+				</Stack>
+				{ toggle }
+			</Stack>
+			{ item.message && <small className="mb-2">{ item.message }</small> }
+			{ raw ? <TraceLog data={ item } /> : <Traces data={ trace } ancestors={ ancestors } /> }
+		</div>
 	);
 }
