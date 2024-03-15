d464ad7109b778b9ffacd9b798ef0bd2369bc5a0
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-03-15 14:53:53 +0100

    Create FormatInterface.php

---
+++ src/Service/Interface/FormatInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace SyncEngine\Service\Interface;
+
+interface FormatInterface
+{
+	public function formatFrom( mixed $var, ?FormatInterface $fromFormat = null );
+	public function format( mixed $var );
+	public function toInt( mixed $var ): ?int;
+	public function toFloat( mixed $var ): ?float;
+	public function toString( mixed $var ): ?string;
+	public function toBool( mixed $var ): ?bool;
+	public function toArray( mixed $var ): ?array;
+	public function toObject( mixed $var ): ?object;
+	public function supportsFormat( FormatInterface $format ): bool;
+}
