c49356102e88d34c0217fc089625aab51e8ebbb6
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-03-17 15:50:11 +0100

    Use raw format as default

--- src/Column/Numeric.php
+++ src/Column/Numeric.php
@@ -4,6 +4,7 @@ namespace SyncEngine\Column;
 
 use SyncEngine\Column\Type\NumericColumnType;
 use SyncEngine\Model\ColumnModel;
+use SyncEngine\Service\Format\FloatFormatter;
 use SyncEngine\Service\Format\NumberFormatter;
 use SyncEngine\Service\Interface\FormatInterface;
 
@@ -21,23 +22,51 @@ class Numeric extends ColumnModel
 	public function getFields(): array
 	{
 		return [
+			'type'                => [
+				'label'   => $this->trans( 'Type' ),
+				'type'    => 'select',
+				'default' => 'raw',
+				'choices' => [
+					'format' => [
+						'label'       => $this->trans( 'Formatted' ),
+						'description' => $this->trans( 'Format to a numeric string.' ),
+					],
+					'raw'    => [
+						'label'       => $this->trans( 'Raw' ),
+						'description' => $this->trans( 'Format to a number type like integer or float/double.' ),
+					],
+				],
+			],
 			'decimals'            => [
-				'label' => 'Decimals',
-				'type'  => 'number',
+				'label'      => $this->trans( 'Decimals' ),
+				'type'       => 'number',
+			],
+			'round'               => [
+				'label'      => $this->trans( 'Rounding' ),
+				'type'       => 'select',
+				'choices'    => [
+					'up'   => $this->trans( 'Round up' ),
+					'down' => $this->trans( 'Round down' ),
+					'even' => $this->trans( 'Round even' ),
+					'odd'  => $this->trans( 'Round odd' ),
+				],
 			],
 			'decimal_separator'   => [
-				'label'        => 'Format',
+				'label'        => $this->trans( 'Decimal separator' ),
 				'type'         => 'select',
 				'customizable' => true,
+				'taggable'     => true,
 				'choices'      => [
 					'.' => '.',
 					',' => ',',
 				],
+				'conditions'   => [ 'type' => 'format' ],
 			],
 			'thousands_separator' => [
-				'label'        => 'Format',
+				'label'        => $this->trans( 'Thousands separator' ),
 				'type'         => 'select',
 				'customizable' => true,
+				'taggable'     => true,
 				'choices'      => [
 					','  => ',',
 					'.'  => '.',
@@ -45,23 +74,30 @@ class Numeric extends ColumnModel
 					' '  => ' ',
 					'\'' => '\'',
 				],
+				'conditions'   => [ 'type' => 'format' ],
 			],
 		];
 	}
 
-	public function format( $value, array $config, ?FormatInterface $source = null )
+	public function getFormatter( array $config = [] ): FormatInterface
 	{
-		$context = [
-			NumberFormatter::DECIMALS            => $config['decimals'] ?? 0,
-			NumberFormatter::DECIMAL_SEPARATOR   => $config['decimal_separator'] ?? '.',
-			NumberFormatter::THOUSANDS_SEPARATOR => $config['thousands_separator'] ?? ',',
-		];
+		switch ( $config['type'] ?? '' ) {
+			case 'format':
+				$context = [
+					NumberFormatter::DECIMALS            => $config['decimals'] ?? 0,
+					NumberFormatter::DECIMAL_SEPARATOR   => $config['decimal_separator'] ?? '.',
+					NumberFormatter::THOUSANDS_SEPARATOR => $config['thousands_separator'] ?? ',',
+					NumberFormatter::ROUND               => $config['round'] ?? null,
+				];
 
-		return $this->getFormatter( $context )->formatFrom( $value, $source );
-	}
+				return new NumberFormatter( $context );
+			default:
+				$context = [
+					FloatFormatter::PRECISION => $config['decimals'] ?? 0,
+					FloatFormatter::MODE      => $config['round'] ?? null,
+				];
 
-	public function getFormatter( $context = [] ): FormatInterface
-	{
-		return new NumberFormatter( $context );
+				return new FloatFormatter( $context );
+		}
 	}
 }
