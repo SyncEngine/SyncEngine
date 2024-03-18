5dc8af3ea8e1f2b621621d0e6949041a99062f62
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-03-18 11:27:55 +0100

    Add price format tests

---
+++ tests/Column/PriceTest.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace SyncEngine\Tests\Column;
+
+use SyncEngine\Service\Format\PriceFormatter;
+use SyncEngine\Tests\TestCase\BaseTestCase;
+
+class PriceTest extends BaseTestCase
+{
+	public function testFormatSchema(): void
+	{
+		// Remove decimals and convert type.
+		$value = 12.34;
+
+		$targetSchema = [
+			'decimals' => 1,
+			'decimal_separator' => ',',
+			'thousands_separator' => '.',
+		];
+
+		$formatted = ( new PriceFormatter( $targetSchema ) )->format( $value );
+
+		$this->assertEquals( '12,3', $formatted );
+
+		// Rounding up.
+		$value = 11.5;
+
+		$targetSchema = [
+			'round' => 'up',
+			'decimals' => 0,
+			'decimal_separator' => ',',
+			'thousands_separator' => '.',
+			'decimal_dash' => true,
+		];
+
+		$formatted = ( new PriceFormatter( $targetSchema ) )->format( $value );
+
+		$this->assertEquals( '12,-', $formatted );
+
+		// Rounding down.
+		$value = 12.5;
+
+		$targetSchema = [
+			'round' => 'down',
+			'decimals' => 0,
+			'decimal_separator' => ',',
+			'thousands_separator' => '.',
+			'decimal_dash' => true,
+		];
+
+		$formatted = ( new PriceFormatter( $targetSchema ) )->format( $value );
+
+		$this->assertEquals( '12,-', $formatted );
+
+		// Currency prefix
+		$value = '12.356,125';
+
+		$targetSchema = [
+			'round' => 'down',
+			'decimals' => 2,
+			'currency_sign' => '€',
+			'currency_space' => true,
+			'thousands_separator' => '\'',
+		];
+
+		$formatted = ( new PriceFormatter( $targetSchema ) )->format( $value );
+
+		$this->assertEquals( '€ 12\'356.12', $formatted );
+
+		// Currency postfix + dash
+		$value = '12.356,125';
+
+		$targetSchema = [
+			'round' => 'down',
+			'decimals' => 0,
+			'decimal_dash' => true,
+			'currency_sign' => 'US$',
+			'currency_space' => true,
+			'currency_after' => true,
+		];
+
+		$formatted = ( new PriceFormatter( $targetSchema ) )->format( $value );
+
+		$this->assertEquals( '12,356.- US$', $formatted );
+	}
+}
