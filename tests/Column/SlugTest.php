c0d421161d8036f6966ce299114cc2a8a97fb874
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-03-18 16:27:39 +0100

    Add slug tests

--- tests/Column/SlugTest.php
+++ tests/Column/SlugTest.php
@@ -2,15 +2,85 @@
 
 namespace SyncEngine\Tests\Column;
 
-use Symfony\Component\String\Slugger\AsciiSlugger;
-use Symfony\Component\String\UnicodeString;
+use SyncEngine\Service\Format\SlugFormatter;
 use SyncEngine\Tests\TestCase\BaseTestCase;
 
 class SlugTest extends BaseTestCase
 {
 	public function testFormatSchema(): void
 	{
-		$var = ( new UnicodeString( 'oneTwoThree' ) );
-		var_dump( ( new AsciiSlugger() )->slug( $var, ' ' )->snake()->title() );
+		// Basic
+		$value = "Wôrķšƥáçè ~~sèťtïñğš~~ ";
+
+		$targetSchema = [];
+
+		$formatted = ( new SlugFormatter( $targetSchema ) )->format( $value );
+
+		$this->assertEquals( 'Workspace-settings', $formatted );
+
+		/**
+		 * Simple tests.
+		 */
+		$value = "One two^^ three! four>.";
+
+		// Snake Case
+		$targetSchema = [
+			'case' => 'lower',
+			'separator' => '_',
+		];
+
+		$formatted = ( new SlugFormatter( $targetSchema ) )->format( $value );
+
+		$this->assertEquals( 'one_two_three_four', $formatted );
+
+		// Screaming Snake Case
+		$targetSchema = [
+			'case' => 'upper',
+			'separator' => '_',
+		];
+
+		$formatted = ( new SlugFormatter( $targetSchema ) )->format( $value );
+
+		$this->assertEquals( 'ONE_TWO_THREE_FOUR', $formatted );
+
+		// Capitalized Snake Case
+		$targetSchema = [
+			'case' => 'capitalize',
+			'separator' => '_',
+		];
+
+		$formatted = ( new SlugFormatter( $targetSchema ) )->format( $value );
+
+		$this->assertEquals( 'One_Two_Three_Four', $formatted );
+
+		// Camel Case
+		$targetSchema = [
+			'case' => 'camel',
+			'separator' => '',
+		];
+
+		$formatted = ( new SlugFormatter( $targetSchema ) )->format( $value );
+
+		$this->assertEquals( 'oneTwoThreeFour', $formatted );
+
+		// Capitalized Camel Case
+		$targetSchema = [
+			'case' => 'capitalize',
+			'separator' => '',
+		];
+
+		$formatted = ( new SlugFormatter( $targetSchema ) )->format( $value );
+
+		$this->assertEquals( 'OneTwoThreeFour', $formatted );
+
+		// Namespace Case
+		$targetSchema = [
+			'case' => 'capitalize',
+			'separator' => '/',
+		];
+
+		$formatted = ( new SlugFormatter( $targetSchema ) )->format( $value );
+
+		$this->assertEquals( 'One/Two/Three/Four', $formatted );
 	}
 }
