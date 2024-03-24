7062e5e32144779589e713f6824d88150f404e84
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-03-24 14:42:57 +0100

    Tests for convert formats

--- tests/Column/DateTimeTest.php
+++ tests/Column/DateTimeTest.php
@@ -48,4 +48,46 @@ class DateTimeTest extends BaseTestCase
 
 		$this->assertEquals( $expected, $formatted );
 	}
+
+	public function testConvertSchema(): void
+	{
+		/**
+		 * Change format from source format.
+		 */
+		$value = '02.14.07 8:25';
+
+		$sourceSchema = [
+			'format' => 'm.d.y g:i',
+		];
+
+		$targetSchema = [
+			'format' => 'd/m/Y h:i:s a',
+		];
+
+		$expected = '14/02/2007 08:25:00 am';
+
+		$formatted = ( new DateTimeFormatter( $targetSchema ) )->convert( $value, new DateTimeFormatter( $sourceSchema ) );
+
+		$this->assertEquals( $expected, $formatted );
+
+		/**
+		 * Change format from source format timestamp.
+		 */
+		$value = '1171484725';
+
+		$sourceSchema = [
+			'format' => 'U',
+		];
+
+		$targetSchema = [
+			'format' => 'D M j G:i:s T Y',
+		];
+
+		// @todo Translations?
+		$expected = 'Wed Feb 14 20:25:25 GMT+0000 2007';
+
+		$formatted = ( new DateTimeFormatter( $targetSchema ) )->convert( $value, new DateTimeFormatter( $sourceSchema ) );
+
+		$this->assertEquals( $expected, $formatted );
+	}
 }
