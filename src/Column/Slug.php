af991f556d20fe1f8f59f219d6c705b490199d51
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-03-24 17:10:00 +0100

    Create basic slug column

---
+++ src/Column/Slug.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace SyncEngine\Column;
+
+use SyncEngine\Column\Type\TextColumnType;
+use SyncEngine\Model\ColumnModel;
+use SyncEngine\Service\Format\SlugFormatter;
+use SyncEngine\Service\Interface\FormatInterface;
+
+class Slug extends ColumnModel
+{
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->type        = TextColumnType::TYPE;
+		$this->name        = $this->trans( 'Slug' );
+		$this->description = $this->trans( 'Format as a slug' );
+	}
+
+	public function getFields(): array
+	{
+		return [
+			'format'    => [
+				'label'   => $this->trans( 'Format' ),
+				'type'    => 'select',
+				'choices' => [
+					'default'  => $this->trans( 'lowercase-dash' ),
+					'constant' => $this->trans( 'UPPERCASE_UNDERSCORE' ),
+					'snake'    => $this->trans( 'snake_case' ),
+					'camel'    => $this->trans( 'camelCase' ),
+					'custom'   => $this->trans( 'Custom' ),
+				],
+			],
+			'case'      => [
+				'label'      => $this->trans( 'Case' ),
+				'type'       => 'select',
+				'choices'    => [
+					'lower'   => $this->trans( 'lowercase' ),
+					'upper'   => $this->trans( 'UPPERCASE' ),
+					'ucwords' => $this->trans( 'Capitalize Words' ),
+					'ucfirst' => $this->trans( 'Capitalize first' ),
+					'camel'   => $this->trans( 'camel Style' ),
+				],
+				'conditions' => [ 'format' => 'custom' ],
+			],
+			'separator' => [
+				'label'        => $this->trans( 'Word separator' ),
+				'type'         => 'select',
+				'customizable' => true,
+				'taggable'     => true,
+				'choices'      => [
+					''  => '{ ' . $this->trans( 'none' ) . ' }',
+					' ' => '{ ' . $this->trans( 'space' ) . ' }',
+					'-' => '- { ' . $this->trans( 'dash' ) . ' }',
+					'_' => '_ { ' . $this->trans( 'underscore' ) . ' }',
+				],
+				'conditions'   => [ 'format' => 'custom' ],
+			],
+		];
+	}
+
+	public function format( $value, array $config, ?FormatInterface $source = null )
+	{
+		parent::format( $value, $config, $source );
+	}
+
+	public function getFormatter( $config = [] ): FormatInterface
+	{
+		if ( 'custom' !== $config['format'] ) {
+			switch ( $config['format'] ) {
+				case 'constant':
+					$config['case']      = 'upper';
+					$config['separator'] = '_';
+				break;
+				case 'snake':
+					$config['case']      = 'lower';
+					$config['separator'] = '_';
+				break;
+				case 'camel':
+					$config['case']      = 'camel';
+					$config['separator'] = '';
+				break;
+				default:
+				case 'slug':
+					$config['case']      = 'lower';
+					$config['separator'] = '-';
+				break;
+			}
+		}
+
+		$context = [
+			SlugFormatter::CASE      => $config['case'],
+			SlugFormatter::SEPARATOR => $config['separator'],
+		];
+
+		return new SlugFormatter( $context );
+	}
+}
