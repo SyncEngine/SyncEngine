b398006073f3f903b404034ac6cd07c87e2d31a9
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-03-25 20:26:17 +0100

    Unmount on exit + pagination for large traces

--- assets/react/components/services/Trace/Traces.jsx
+++ assets/react/components/services/Trace/Traces.jsx
@@ -3,6 +3,7 @@ import Badge from '../../partials/Badge';
 import AccordionSticky from '../../partials/AccordionSticky';
 import Trace from './Trace';
 import { Stack } from 'react-bootstrap';
+import usePagination from '../../../hooks/usePagination';
 
 export default function Traces( props ) {
 	const {
@@ -11,44 +12,53 @@ export default function Traces( props ) {
 		accordionProps = {},
 	} = props;
 
+	const { items, toolbar } = usePagination( data );
+
 	return (
-		<AccordionSticky className="w-100 accordion-sm" { ...accordionProps } alwaysOpen>
-			{
-				data.map( ( item, index ) => {
+		<Stack className={ props.className }>
+			<AccordionSticky className="w-100 accordion-sm" { ...accordionProps } alwaysOpen>
+				{
+					items.map( ( item, index ) => {
 
-					const {
-						title,
-						type,
-						ref,
-						count = 1,
-						_timestamp: timestamp,
-					} = item;
+						const {
+							title,
+							type,
+							ref,
+							count = 1,
+							_timestamp: timestamp,
+						} = item;
 
-					ancestors.push( item );
+						ancestors.push( item );
 
-					let start = timestamp[0] ?? timestamp;
-					let end = timestamp[1] ?? null;
+						let start = timestamp[0] ?? timestamp;
+						let end = timestamp[1] ?? null;
 
-					return (
-						<AccordionSticky.Item eventKey={ index } key={ index + item._key } ref={ item._ref }>
-							<AccordionSticky.Header>
-								<Stack direction="horizontal" className="flex-wrap" gap={2}>
-									<Badge subtle>{ count }x</Badge>
-									{ title && <small>{ title }</small> }
-									{ type && <Badge subtle>{ type }</Badge> }
-									{ ( ref && ref !== title ) && <Badge subtle>{ ref }</Badge> }
-									{ end &&
-									  <Badge subtle>{ Math.round( end - start ) }ms</Badge>
-									}
-								</Stack>
-							</AccordionSticky.Header>
-							<AccordionSticky.Body>
-								<Trace item={ item } ancestors={ ancestors } />
-							</AccordionSticky.Body>
-						</AccordionSticky.Item>
-					);
-				} )
+						return (
+							<AccordionSticky.Item eventKey={ index } key={ index + item._key } ref={ item._ref }>
+								<AccordionSticky.Header>
+									<Stack direction="horizontal" className="flex-wrap" gap={2}>
+										<Badge subtle>{ count }x</Badge>
+										{ title && <small>{ title }</small> }
+										{ type && <Badge subtle>{ type }</Badge> }
+										{ ( ref && ref !== title ) && <Badge subtle>{ ref }</Badge> }
+										{ end &&
+										  <Badge subtle>{ Math.round( end - start ) }ms</Badge>
+										}
+									</Stack>
+								</AccordionSticky.Header>
+								<AccordionSticky.Collapse eventKey={ index } unmountOnExit>
+									<AccordionSticky.Body>
+										<Trace item={ item } ancestors={ ancestors } />
+									</AccordionSticky.Body>
+								</AccordionSticky.Collapse>
+							</AccordionSticky.Item>
+						);
+					} )
+				}
+			</AccordionSticky>
+			{ toolbar &&
+				<div className="bg-body-tertiary p-2">{ toolbar }</div>
 			}
-		</AccordionSticky>
+		</Stack>
 	);
 }
