e7b5303919c6ec77f52017122d9625d37b5fd96e
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-03-27 01:36:37 +0100

    Improve background dark coloring
    + no longer need contrast class, keep it for now but remove if deprecated

--- assets/react/controllers/MenuController.jsx
+++ assets/react/controllers/MenuController.jsx
@@ -47,7 +47,7 @@ export default function MenuController( props ) {
 	navStyles[ '--bs-nav-link-hover-color' ] = 'var(--bs-emphasis-color)';
 
 	return (
-		<div id="menu" className={ 'shadow-lg d-flex flex-column border-end border-secondary border-opacity-10 bg-body-contrast ' + ( ( collapsed ) ? 'collapsed' : 'expanded' ) }>
+		<div id="menu" className={ 'shadow-lg d-flex flex-column border-end border-secondary border-opacity-10 bg-body ' + ( ( collapsed ) ? 'collapsed' : 'expanded' ) }>
 			<Navbar className={ "d-flex align-items-center justify-content-" + ( collapsed ? 'center flex-column' : 'between px-3' ) } expand={ ! collapsed }>
 				<a id="logo" className="navbar-brand menu-collapsible d-flex align-items-center m-0" href="/">
 					<span className={ "d-flex" + ( collapsed ? '' : ' me-1' ) }>{ logo }</span>
