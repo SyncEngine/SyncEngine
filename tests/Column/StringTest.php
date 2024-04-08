184023d9d1bcf0566e19a9435dd4c0f67a5b1a21
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-04-08 14:48:27 +0200

    Add basic string tests

---
+++ tests/Column/StringTest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Column;
+
+use SyncEngine\Service\Format\StringFormatter;
+use SyncEngine\Tests\TestCase\BaseTestCase;
+
+class StringTest extends BaseTestCase
+{
+	public function testString(): void
+	{
+		$value = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce hendrerit cursus lacus sit amet dignissim.';
+
+		$targetSchema = [
+			'case' => 'ucwords',
+		];
+
+		$formatted = ( new StringFormatter( $targetSchema ) )->format( $value );
+
+		$expected = 'Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit. Fusce Hendrerit Cursus Lacus Sit Amet Dignissim.';
+
+		$this->assertEquals( $expected, $formatted );
+
+		/**
+		 * Max characters.
+		 */
+
+		$targetSchema = [
+			'max_length' => 30,
+		];
+
+		$formatted = ( new StringFormatter( $targetSchema ) )->format( $value );
+
+		$expected = 'Lorem ipsum dolor sit amet, co';
+
+		$this->assertEquals( $expected, $formatted );
+
+		/**
+		 * Max words.
+		 */
+
+		$targetSchema = [
+			'max_length' => 10,
+			'max_length_unit' => 'words',
+		];
+
+		$formatted = ( new StringFormatter( $targetSchema ) )->format( $value );
+
+		$expected = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce hendrerit';
+
+		$this->assertEquals( $expected, $formatted );
+
+		/**
+		 * Max words ellipsis.
+		 */
+
+		$targetSchema = [
+			'max_length' => 10,
+			'max_length_unit' => 'words',
+			'ellipsis' => true,
+		];
+
+		$formatted = ( new StringFormatter( $targetSchema ) )->format( $value );
+
+		$expected = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce hendrerit ...';
+
+		$this->assertEquals( $expected, $formatted );
+
+		/**
+		 * Max words custom ellipsis.
+		 */
+
+		$targetSchema = [
+			'max_length' => 10,
+			'max_length_unit' => 'words',
+			'ellipsis' => '[***]',
+		];
+
+		$formatted = ( new StringFormatter( $targetSchema ) )->format( $value );
+
+		$expected = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce hendrerit [***]';
+
+		$this->assertEquals( $expected, $formatted );
+	}
+}
