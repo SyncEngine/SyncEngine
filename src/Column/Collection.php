1b1231c9fc430cd6b9d623927d75cb2d917b4e4f
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-04-08 19:11:48 +0200

    Base for collection column (not done)

---
+++ src/Column/Collection.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace SyncEngine\Column;
+
+use SyncEngine\Column\Type\CollectionColumnType;
+use SyncEngine\Model\ColumnModel;
+use SyncEngine\Service\Format\ArrayFormatter;
+use SyncEngine\Service\Interface\FormatInterface;
+
+class Collection extends ColumnModel
+{
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->type        = CollectionColumnType::TYPE;
+		$this->name        = $this->trans( 'Schema' );
+		$this->description = $this->trans( 'Schema definitions column' );
+	}
+
+	public function getFields(): array
+	{
+		return [
+			'associative' => [
+				'label' => $this->trans( 'Associative' ),
+				'type'  => 'switch',
+			],
+			'column' => [
+				'label' => $this->trans( 'Column Type' ),
+				'type'  => 'column',
+			],
+		];
+	}
+
+	public function format( $value, array $config, ?FormatInterface $source = null )
+	{
+		// Format each sub-field.
+
+		return parent::format( $value, $config, $source );
+	}
+
+	public function getFormatter( $config = [] ): FormatInterface
+	{
+		$context[ ArrayFormatter::LIST ] = empty( $config['associative'] );
+		return new ArrayFormatter( $context );
+	}
+}
