9312db2b37b651e5941ce2054bebe51d6001c61d
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-04-11 17:37:47 +0200

    Strict mode tests + add comments

--- tests/Column/AddressTest.php
+++ tests/Column/AddressTest.php
@@ -37,18 +37,58 @@ class AddressTest extends BaseTestCase
 			AddressFormatter::COLUMN_LOCALITY       => 'city',
 		];
 
+		$formatted = ( new AddressFormatter( $targetSchema ) )->convert(
+			$value,
+			new AddressFormatter( $sourceSchema )
+		);
+
+		// Address keys should be changed to the new format.
 		$excpected = [
 			'city'    => 'Nijmegen',
 			'address' => 'Kaasstraat 12',
 			'zip'     => '1024 NL',
 		];
 
+		$this->assertEquals( $excpected, $formatted );
+
+		/**
+		 * Convert schema but check if non-address data is kept.
+		 */
+
+		$value['foo'] = 'bar';
+
 		$formatted = ( new AddressFormatter( $targetSchema ) )->convert(
 			$value,
 			new AddressFormatter( $sourceSchema )
 		);
 
-		// Should not be changed.
+		// Non-address data should not be removed.
+		$excpected = [
+			'city'    => 'Nijmegen',
+			'address' => 'Kaasstraat 12',
+			'zip'     => '1024 NL',
+			'foo'     => 'bar',
+		];
+
+		$this->assertEquals( $excpected, $formatted );
+
+		/**
+		 * Strict mode.
+		 */
+		$targetSchema[ AddressFormatter::STRICT ] = true;
+
+		$formatted = ( new AddressFormatter( $targetSchema ) )->convert(
+			$value,
+			new AddressFormatter( $sourceSchema )
+		);
+
+		// Non-address data should be removed.
+		$excpected = [
+			'city'    => 'Nijmegen',
+			'address' => 'Kaasstraat 12',
+			'zip'     => '1024 NL',
+		];
+
 		$this->assertEquals( $excpected, $formatted );
 	}
 }
