e2986071d9b88cd381c30f60a11cb8f81c2d20f6
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-04-12 14:33:04 +0200

    Configurable blueprint column formats

--- src/Column/Boolean.php
+++ src/Column/Boolean.php
@@ -18,8 +18,71 @@ class Boolean extends ColumnModel
 		$this->description = $this->trans( 'Format as true or false' );
 	}
 
+	public function getFields(): array
+	{
+		return [
+			'format'       => [
+				'label'   => $this->trans( 'Format' ),
+				'type'    => 'select',
+				'choices' => [
+					'bool'   => $this->trans( 'Boolean' ) . ' (true/false)',
+					'int'    => $this->trans( 'Integer' ) . ' (1/0)',
+					'string' => $this->trans( 'Custom text' ),
+				],
+			],
+			'true_value'   => [
+				'label'      => $this->trans( 'True value' ),
+				'type'       => 'text',
+				'conditions' => [ 'format' => 'string' ],
+			],
+			'false_value'  => [
+				'label'      => $this->trans( 'False value' ),
+				'type'       => 'text',
+				'conditions' => [ 'format' => 'string' ],
+			],
+			'fallback_value' => [
+				'label' => $this->trans( 'Fallback value' ),
+				'type'    => 'select',
+				'choices' => [
+					''       => $this->trans( 'Null / Empty' ),
+					'true'   => $this->trans( 'True value' ),
+					'false'  => $this->trans( 'False value' ),
+					'custom' => $this->trans( 'Custom' ),
+				],
+			],
+			'fallback_custom' => [
+				'label' => $this->trans( 'Fallback custom value' ),
+				'type'    => 'text',
+			],
+		];
+	}
+
 	public function initFormatter( $config = [] ): FormatInterface
 	{
-		return new BooleanFormatter();
+		$context[ BooleanFormatter::FORMAT ] = $config['format'] ?? 'bool';
+		if ( 'string' === $context[ BooleanFormatter::FORMAT ] ) {
+			$context[ BooleanFormatter::TRUE_VALUE ] = $config['true_value'] ?? '';
+			$context[ BooleanFormatter::FALSE_VALUE ] = $config['false_value'] ?? '';
+		} elseif ( 'int' === $context[ BooleanFormatter::FORMAT ] ) {
+			$config['true_value'] = 1;
+			$config['false_value'] = 0;
+		} else {
+			$config['true_value'] = true;
+			$config['false_value'] = false;
+		}
+
+		switch ( $config['fallback_value'] ?? '' ) {
+			case 'true':
+				$context[ BooleanFormatter::NULL_VALUE ] = $config['true_value'] ?? null;
+			break;
+			case 'false':
+				$context[ BooleanFormatter::NULL_VALUE ] = $config['false_value'] ?? null;
+			break;
+			case 'custom':
+				$context[ BooleanFormatter::NULL_VALUE ] = $config['fallback_custom'] ?? null;
+			break;
+		}
+
+		return new BooleanFormatter( $context );
 	}
 }
