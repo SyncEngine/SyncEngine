94f6f2fc311aa49265cb0b015535ad210ddc3707
Inform-All <NKoonen@users.noreply.github.com>
2024-04-12 13:10:41 +0200

    Boolean column formatter

---
+++ src/Column/Boolean.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace SyncEngine\Column;
+
+use SyncEngine\Column\Type\BooleanColumnType;
+use SyncEngine\Model\ColumnModel;
+use SyncEngine\Service\Format\BooleanFormatter;
+use SyncEngine\Service\Interface\FormatInterface;
+
+class Boolean extends ColumnModel
+{
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->type        = BooleanColumnType::TYPE;
+		$this->name        = $this->trans( 'Boolean' );
+		$this->description = $this->trans( 'Format as a boolean' );
+	}
+
+	public function initFormatter( $config = [] ): FormatInterface
+	{
+		return new BooleanFormatter();
+	}
+}
