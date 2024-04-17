3d84b9e29e89ec1216a4d3bacacb711c38ecb251
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-04-17 21:38:13 +0200

    Move publishing to Fields control component

--- assets/react/components/form/Fields/index.jsx
+++ assets/react/components/form/Fields/index.jsx
@@ -4,7 +4,7 @@ import Group from './Group';
 import { createRefId } from '../../../utils/globals';
 import { FieldContext, FieldsContext } from '../../../context/FieldsContext';
 import { isObject } from '../../../utils/conditions';
-import { parseValue } from '../../../hooks/useFieldValue';
+import { parseValue, publishFieldValue } from '../../../hooks/useFieldValue';
 
 export function parseValues( values, fields ) {
 	if ( ! fields || ! isObject( values ) ) {
@@ -29,20 +29,22 @@ export default function Fields( props ) {
 		name = field.name,
 		value,
 		onChange,
-		fieldsContext,
 	} = props;
 
 	const [ values, setValues ] = useState( parseValues( value ?? {}, props.fields ) );
+
+	const fieldsContext = props.fieldsContext ?? FieldsContext.create( name, values, ref.current );
 
 	const updateField = useCallback( ( input, key, field ) => {
 		values[ key ] = parseValue( input, field );
 
 		setValues( values );
 		onChange( values );
+		publishFieldValue( key, fieldsContext, values[ key ] );
 	}, [ onChange ] );
 
 	return (
-		<FieldsContext.Provider value={ fieldsContext ?? FieldsContext.create( name, values, ref.current ) }>
+		<FieldsContext.Provider value={ fieldsContext }>
 			<Group { ...props } updateField={ updateField } value={ undefined }></Group>
 		</FieldsContext.Provider>
 	);
--- assets/react/hooks/useFieldValue.jsx
+++ assets/react/hooks/useFieldValue.jsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react';
 import { FieldsContext } from '../context/FieldsContext';
 import useSyncedState from './useSyncedState';
+import { publish } from '../utils/events';
 
 const prefix = 'update:FieldValue:';
 
@@ -15,6 +16,11 @@ export function parseValue( value, field ) {
 	return value ?? null;
 }
 
+export function publishFieldValue( name, context, state ) {
+	const event = getEvent( [ name], context );
+	publish( event, state );
+}
+
 export default function useFieldValue( key, context = null ) {
 	if ( ! context ) {
 		context = useContext( FieldsContext );
@@ -22,18 +28,7 @@ export default function useFieldValue( key, context = null ) {
 
 	const event = getEvent( [ key ], context );
 
-	const [ value, update, publish ] = useSyncedState( event, context.values && context.values[ key ] );
+	const [ value ] = useSyncedState( event, context.values && context.values[ key ] );
 
-	const updateValue = ( state, name, field ) => {
-		state = parseValue( state, field );
-
-		if ( key !== name || ( field && field.name !== key ) ) {
-			publish( getEvent( [ name ], context ), state );
-			return;
-		}
-
-		update( state, true );
-	}
-
-	return [ value, updateValue ];
+	return [ value ];
 }
