64df6b387f5833fb6a7ad3b9b3876672f71c1393
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-04-17 19:26:11 +0200

    Entity ref as base for fields context

--- assets/react/controllers/ConfigController.jsx
+++ assets/react/controllers/ConfigController.jsx
@@ -90,7 +90,7 @@ export default function ConfigController( props ) {
 	if ( entity._supports && entity._supports.blueprints ) {
 		formComponent = <BlueprintControl manualFields={ fields } value={ value } onChange={ update } entity={ entity } />;
 	} else {
-		formComponent = <Fields fields={ fields } value={ { ...value } } onChange={ update } />
+		formComponent = <Fields name={ entity.ref } fields={ fields } value={ value } onChange={ update } />
 	}
 
 	return (
