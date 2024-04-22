6cbcd20bf0979ddc9869ada269d1139cddda28ec
Inform-All <NKoonen@users.noreply.github.com>
2024-04-22 15:45:59 +0200

    create dynamic menu function

--- src/Controller/MenuController.php
+++ src/Controller/MenuController.php
@@ -3,11 +3,40 @@
 namespace SyncEngine\Controller;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouterInterface;
 
 class MenuController extends DefaultController
 {
 	public function jsonMenu(): JsonResponse
 	{
 		return $this->json( [] );
+	}
+
+	public function renderLeftMain( $currentPath, RouterInterface $router, Request $request ): Response
+	{
+		$routes    = [];
+		$allRoutes = $router->getRouteCollection()->all();
+		foreach ( $allRoutes as $route ) {
+			if ( $route->getOption( 'menu' ) === "leftMain" ) {
+				$routes[] = [
+					'title'        => $route->getOption( 'menuTitle' ),
+					'icon'         => $route->getOption( 'menuIcon' ),
+					'link'         => $route->getPath(),
+					'menuPosition' => $route->getOption( 'menuPosition' ),
+				];
+			}
+		}
+
+		array_multisort( array_column( $routes, 'menuPosition' ), SORT_ASC, $routes );
+
+		return $this->render(
+			'admin/_partials/menu.twig',
+			[
+				'items'       => $routes,
+				'currentPath' => $currentPath,
+			]
+		);
 	}
 }
