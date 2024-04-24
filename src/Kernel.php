20c5b7d424914385c443d6cccbda6ccfc24a7090
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-04-24 12:23:36 +0200

    Register attribute as service locator

--- src/Kernel.php
+++ src/Kernel.php
@@ -3,9 +3,29 @@
 namespace SyncEngine;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\DependencyInjection\ChildDefinition;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+use SyncEngine\Attribute\MenuItem;
 
 class Kernel extends BaseKernel
 {
 	use MicroKernelTrait;
+
+	protected function build( ContainerBuilder $container )
+	{
+		parent::build( $container );
+
+		$container->registerAttributeForAutoconfiguration(MenuItem::class, static function (
+			ChildDefinition $definition,
+			MenuItem $attribute,
+			\ReflectionClass|\ReflectionMethod $reflector
+		): void {
+			$args = [];
+			if ( $reflector instanceof \ReflectionMethod ) {
+				$args['method'] = $reflector->getName();
+			}
+			$definition->addTag( 'syncengine.attribute.menuitem', $args );
+		});
+	}
 }
