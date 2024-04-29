a7ca89786aa38bfb9e751edcb80728943a934703
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-04-29 15:20:36 +0200

    Create model to start on notifiers

---
+++ src/Model/NotifierModel.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace SyncEngine\Model;
+
+use Symfony\Component\Notifier\Notification\Notification;
+use SyncEngine\Model\Abstract\ServiceModel;
+use SyncEngine\Model\Interface\Configurable;
+use SyncEngine\Model\Trait\Config;
+use SyncEngine\Task\Type\TaskTypeInterface;
+
+abstract class NotifierModel extends ServiceModel implements Configurable
+{
+	use Config;
+
+	const SERVICE = 'Notifiers';
+
+	/**
+	 * The type of task, can be used for categorizing.
+	 *
+	 * @var string|TaskTypeInterface
+	 */
+	public string|TaskTypeInterface $type = '';
+
+	/**
+	 * Human-readable name used in the interface.
+	 *
+	 * @var string
+	 */
+	public string $name = '';
+
+	/**
+	 * Human-readable description used in the interface.
+	 *
+	 * @var string
+	 */
+	public string $description = '';
+
+	public function getType(): string
+	{
+		return $this->type instanceof TaskTypeInterface ? $this->type->getType() : $this->type;
+	}
+
+	public function getName(): string
+	{
+		return $this->name;
+	}
+
+	public function getDescription(): string
+	{
+		return $this->description;
+	}
+
+	abstract public function getFields(): array;
+
+	abstract public function notify( array $config, Notification $notification );
+
+	public function normalize(): array
+	{
+		$props = [
+			'_class'      => $this->getClassLocator(),
+			'type'        => $this->getType(),
+			'name'        => $this->getName(),
+			'description' => $this->getDescription(),
+			'fields'      => $this->getFields(),
+		];
+
+		if ( $this->isFromModule() ) {
+			$props['module'] = $this->getModule()->getName();
+		}
+
+		return $props;
+	}
+
+	final static function isNotifier( $class ): bool
+	{
+		return $class instanceof NotifierModel;
+	}
+}
