1cbe7a5f42f156e63ca3a46b997f9bdbddebda3d
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-05-07 18:15:05 +0200

    Support a list of help texts

--- assets/react/components/form/Help.jsx
+++ assets/react/components/form/Help.jsx
@@ -1,5 +1,6 @@
-import React from "react";
-import { InputGroup, OverlayTrigger, Tooltip } from "react-bootstrap";
+import React from 'react';
+import { InputGroup, OverlayTrigger, Tooltip } from 'react-bootstrap';
+import { objectToMappable } from '../../utils/data';
 
 export default function Help( {
 	id,
@@ -16,8 +17,22 @@ export default function Help( {
 		button = <InputGroup.Text><span className="bi bi-info-circle" /></InputGroup.Text>;
 	}
 
+	let help = text;
+
+	if ( 'object' === typeof text ) {
+		help = (
+			<ul className="text-start m-1 ms-n2">
+				{
+					objectToMappable( text, 'type', 'text' ).map( ( item, index ) => {
+						return <li key={ index }>{ item.text }</li>
+					} )
+				}
+			</ul>
+		)
+	}
+
 	return (
-		<OverlayTrigger overlay={ <Tooltip id={ id + '_help' }>{ text }</Tooltip> }>
+		<OverlayTrigger overlay={ <Tooltip id={ id + '_help' }>{ help }</Tooltip> }>
 			{ button }
 		</OverlayTrigger>
 	)
