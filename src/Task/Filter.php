da6e639073ff14cdec6292e3a94d8e687aa211b5
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-05-07 15:36:43 +0200

    Reorder fields and improve labels

--- src/Task/Filter.php
+++ src/Task/Filter.php
@@ -27,7 +27,7 @@ class Filter extends TaskModel
 		return [
 			'key'        => [
 				'type'        => 'text',
-				'label'       => $this->trans( 'Key / Column' ),
+				'label'       => $this->trans( 'Key / Column name' ),
 				'description' => $this->trans( 'Leave empty for root iteration' ),
 				'help'        => $this->trans( 'Nested keys are supported: key.nested_key' ),
 				'taggable'    => true,
