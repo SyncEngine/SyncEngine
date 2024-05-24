416f927e9869643684cb8fa2718a5bb6274aa06d
Inform-All <NKoonen@users.noreply.github.com>
2024-05-24 14:33:06 +0200

    no double {{ in translation strings

--- src/Form/ChangePasswordFormType.php
+++ src/Form/ChangePasswordFormType.php
@@ -29,7 +29,7 @@ class ChangePasswordFormType extends AbstractType
 						]),
 						new Length([
 							'min' => 6,
-							'minMessage' => 'Your password should be at least {{ limit }} characters',
+							'minMessage' => 'Your password should be at least { limit } characters',
 							// max length allowed by Symfony for security reasons
 							'max' => 4096,
 						]),
