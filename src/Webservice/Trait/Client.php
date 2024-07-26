a2a97a7ba098d462dd122f73c18030377652c552
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-07-26 17:34:53 +0200

    Path for PHP connection timeouts by implementing expiration time
    Not perfect but it's an improvement.

--- src/Webservice/Trait/Client.php
+++ src/Webservice/Trait/Client.php
@@ -7,7 +7,7 @@ trait Client
 	protected static array $clients;
 	private $client;
 
-	public function fetchClient( string $ref ): ?object
+	public function fetchClient( string $ref ): mixed
 	{
 		return static::$clients[ $ref ] ?? $this->client ?? null;
 	}
