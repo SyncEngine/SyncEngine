6269920a754307eeb93723b62269c303dc324ddc
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-08-06 12:42:45 +0200

    Enum column to predefine fixed value options

---
+++ src/Column/Enum.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace SyncEngine\Column;
+
+use SyncEngine\Column\Type\TextColumnType;
+use SyncEngine\Exception\BadConfigException;
+use SyncEngine\Exception\InvalidValueException;
+use SyncEngine\Model\ColumnModel;
+use SyncEngine\Service\Format\StringFormatter;
+use SyncEngine\Service\Interface\FormatInterface;
+
+class Enum extends ColumnModel
+{
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->type        = TextColumnType::TYPE;
+		$this->name        = $this->trans( 'Enum' );
+		$this->description = $this->trans( 'Predefined select options' );
+	}
+
+	public function getFields(): array
+	{
+		return [
+			'options' => [
+				'label' => $this->trans( 'Options' ),
+				'type'  => 'params',
+				'columns' => [
+					'value' => $this->trans( 'Value' ),
+					'label' => $this->trans( 'Label' ),
+				],
+			],
+			'parse' => [
+				'label'  => $this->trans( 'Parse before validation' ),
+				'fields' => [
+					'trim'   => [
+						'label' => $this->trans( 'Trim text' ),
+						'type'  => 'checkbox',
+					],
+					'case'   => [
+						'label'   => $this->trans( 'Convert case' ),
+						'type'    => 'select',
+						'choices' => [
+							'ucfirst' => $this->trans( 'Capitalize first' ),
+							'ucwords' => $this->trans( 'Capitalize Words' ),
+							'lower'   => $this->trans( 'lowercase' ),
+							'upper'   => $this->trans( 'UPPERCASE' ),
+						],
+					],
+				]
+			]
+		];
+	}
+
+	public function format( $value, ?array $config = null, ?ColumnModel $source = null )
+	{
+		$formatted = parent::format( $value, $config, $source );
+
+		$options = $config['options'] ?? [];
+
+		$values = array_column( $options, 'value' );
+
+		if ( ! $values ) {
+			throw new BadConfigException( $this->trans( 'Invalid enum config' ) );
+		}
+
+		if ( ! in_array( $value, $values, true ) ) {
+			throw new InvalidValueException( $this->trans( 'Invalid value: {formatted}', [ 'formatted' => $formatted ] ) );
+		}
+
+		return $formatted;
+	}
+
+	public function initFormatter( $config = [] ): FormatInterface
+	{
+		$context = [
+			StringFormatter::TRIM => ! empty( $config['trim'] ),
+		];
+
+		if ( ! empty( $config['case'] ) ) {
+			$context[ StringFormatter::CASE ] = $config['case'];
+		}
+
+		return new StringFormatter( $context );
+	}
+
+	public function normalize(): array
+	{
+		$normalize = parent::normalize();
+
+		$normalize['modalProps'] = [
+			'size' => 'lg',
+		];
+
+		return $normalize;
+	}
+}
