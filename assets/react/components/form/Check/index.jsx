91db1b59356c5b93961c6cfa64526d275bfc88b1
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-08-07 23:15:11 +0200

    Variant handling

--- assets/react/components/form/Check/index.jsx
+++ assets/react/components/form/Check/index.jsx
@@ -2,3 +2,6 @@
 export { default as CheckSingle } from './Single';
 export { default as CheckMulti } from './Multi';
 
+export function getCheckButtonVariant( props ) {
+	return props.variant ?? ( 'string' === typeof props.button ? props.button : undefined );
+}
