737ec9afa1c85c73aabe244fb8a4d761650f1e1e
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-08-08 20:41:40 +0200

    Add badge support in link column

--- assets/react/components/views/Blocks/Link.jsx
+++ assets/react/components/views/Blocks/Link.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { sprintf } from '../../../utils/globals';
+import BadgeControl from './Badge';
+import { isObject } from '../../../utils/conditions';
+import { HStack } from '../../partials/Stack';
 
 export default function Link( props ) {
 	const {
 		item,
+		type = item.type,
 		text,
 		href,
+		badge,
 	} = props;
 
 	if ( ! href ) {
@@ -13,11 +18,14 @@ export default function Link( props ) {
 	}
 
 	return (
-		<>
-			<a className="icon-link me-1" href={ sprintf( href, item ) } target="_blank">
+		<HStack gap={ 2 }>
+			<a className="icon-link" href={ sprintf( href, item ) } target="_blank">
 				<i className="bi bi-link-45deg"></i>
 			</a>
 			<small>{ sprintf( text ?? href, item ) }</small>
-		</>
+			{ badge &&
+			  <BadgeControl { ...( isObject( badge ) ? badge : { label: badge } ) } item={ item } type={ type } />
+			}
+		</HStack>
 	)
 }
