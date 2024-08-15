4346b4d1f83cc5a93c8f6c7756059bb022252192
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-08-15 13:29:07 +0200

    Add auth-flow tests to make sure it says working!

---
+++ tests/Model/ConnectionModelTest.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace SyncEngine\Tests\Model;
+
+
+use SyncEngine\Model\ConnectionModel;
+use SyncEngine\Tests\TestCase\BaseTestCase;
+
+class ConnectionModelTests extends BaseTestCase
+{
+	public $configHttp = [
+		'_class' => 'Http',
+		'host' => 'https://localhost',
+		'_connect' => [
+			'request' => [
+				'query' => [
+					'connect' => 'Test Merge',
+				],
+				'method' => 'HEAD',
+			],
+			'endpoint' => 'Test Connect Override',
+			'response' => []
+		],
+		'authorize' => [
+			'host' => 'https:127.0.0.1',
+			'request' => [
+				'headers' => [
+					'Authorization' => 'Test Authorization',
+				],
+				'format' => 'formdata',
+			],
+			'endpoint' => 'Test Authorize Override',
+			'response' => [
+				'format' => 'json',
+			]
+		]
+	];
+
+	public function clearInternalConfig( $config ) {
+		unset( $config['_class'] );
+		unset( $config['_connect'] );
+		unset( $config['authorization'] );
+		unset( $config['authorize'] );
+		unset( $config['connection'] );
+		return $config;
+	}
+
+	public function testConnectionAuthorization(): void
+	{
+		$model = new ConnectionModel();
+
+		$model->setConfig( [ 'webservice' => $this->configHttp, ] );
+
+		//* Test connect config.
+
+		$expected = [
+			'host' => 'https:127.0.0.1',
+			'request' => [
+				'headers' => [
+					'Authorization' => 'Test Authorization',// From Authorize
+				],
+				'query' => [
+					'connect' => 'Test Merge',
+				],
+				'method' => 'HEAD',
+				'format' => 'formdata',// From Authorize
+			],
+			'endpoint' => 'Test Connect Override',
+			'response' => [
+				'format' => 'json',// From Authorize
+			],
+		];
+
+		$actual = $model->handleAuthorization( $this->configHttp['_connect'], null );
+		$actual = $this->clearInternalConfig( $actual );
+
+		$this->assertEquals($expected, $actual);
+	}
+}
