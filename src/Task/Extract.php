b5ebfc10818a6112c8aa322cb943b508e1cfb767
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-08-16 10:35:54 +0200

    Create new Extract task
    Basically an enhanced array_column() function

---
+++ src/Task/Extract.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace SyncEngine\Task;
+
+use SyncEngine\Model\TaskModel;
+use SyncEngine\Service\ExecuteData;
+use SyncEngine\Service\ExecutionContext;
+use SyncEngine\Service\ResourceData;
+use SyncEngine\Task\Type\StructureTaskType;
+
+class Extract extends TaskModel
+{
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->type        = StructureTaskType::TYPE;
+		$this->name        = $this->trans( 'Extract' );
+		$this->description = $this->trans( 'Extract a column from each item in your data' );
+	}
+
+	public function getFields(): array
+	{
+		return [
+			'key'       => [
+				'label'    => $this->trans( 'Key / Column name' ),
+				'type'     => 'text', // @todo Column/Key selection field type.
+				'help'     => [
+					$this->trans( 'The data column name to extract items in' ),
+					$this->trans( 'Nested keys are supported: key.nested_key' ),
+					$this->trans( 'Leave empty for root' ),
+				],
+				'taggable' => true,
+			],
+			'column_key' => [
+				'label'       => $this->trans( 'The column to extract' ),
+				'type'        => 'text',
+				'help'        => [
+					$this->trans( 'Nested keys are supported: key.nested_key' ),
+					$this->trans( 'Leave empty for root' ),
+				],
+				// @todo Convert this to Tags (Needs big refactor in Execute service.
+				'default'     => '',
+				'taggable'    => true,
+			],
+		];
+	}
+
+	public function execute( array $config, ExecutionContext $context, ExecuteData $data ): ExecuteData
+	{
+		$column_key = $config['column_key'] ?? null;
+		if ( empty( $config['column_key'] ) ) {
+			$context->addError( $this->trans( 'No column key configured' ) );
+
+			return $data;
+		}
+
+		$key   = $config['key'] ?? null;
+		$items = $data->get( $key );
+
+		if ( ! is_iterable( $items ) ) {
+			$context->addLog( $this->trans( 'Data not iterable' ) );
+
+			return $data;
+		}
+
+		$extracted = [];
+		foreach ( $items as $index => $value ) {
+			$extracted[ $index ] = ResourceData::create( $value )->get( $column_key );
+		}
+
+		$data->set( $extracted, $key );
+
+		return $data;
+	}
+}
