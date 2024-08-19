c4d7f10b3f8ad450f84e603fd4fa1d59bdbfdafb
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-08-19 15:29:03 +0200

    Fix enum in export

--- src/Service/ModelExporter.php
+++ src/Service/ModelExporter.php
@@ -75,8 +75,14 @@ class ModelExporter
 			}
 			if ( $value ) {
 				if ( is_object( $value ) ) {
-					// Remove ref.
-					$value = clone $value;
+
+					$valueRef = new \ReflectionClass( $value );
+					if ( $valueRef->isEnum() ) {
+						$value = $value->value;
+					} elseif ( $valueRef->isCloneable() ) {
+						// Remove ref.
+						$value = clone $value;
+					}
 
 					if ( is_iterable( $value ) ) {
 						// Doctrine collections.
