126bf18f9b95c7e084e1f16386cd6c06de811976
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-08-27 12:08:52 +0200

    Fix tests for list vs assoc and add specific list tests

--- tests/Task/FilterTest.php
+++ tests/Task/FilterTest.php
@@ -12,24 +12,25 @@ class FilterTest extends TaskTestCase
 	{
 		$context = $this->getContext();
 
+		// NOTE THAT THIS ARRAY IS NOT A LIST (5th item).
 		$data = [
-			0 => [
+			0  => [
 				'title' => 'Test 1.',
 				'sku'   => 'ABCD.001/23456',
 			],
-			1 => [
+			1  => [
 				'title' => 'Test 2.',
 				'sku'   => '23456.ABCD.001',
 			],
-			2 => [
+			2  => [
 				'title' => 'Test 3.',
 				'sku'   => '',
 			],
-			3 => [
+			3  => [
 				'title' => 'Foo 4.',
 				'sku'   => '0',
 			],
-			4 => [
+			50 => [ // NOT A LIST!
 				'title' => 'Bar 5.',
 				'sku'   => '1',
 			],
@@ -59,8 +60,15 @@ class FilterTest extends TaskTestCase
 		];
 
 		$returnData = $this->execute( $config, $context, $data );
-
 		$this->assertEquals( $expected, $returnData );
+
+		// Expect the same result with a forced list.
+		$returnData = $this->execute( $config, $context, array_values( $data ) );
+		$this->assertEquals( array_values( $expected ), $returnData );
+
+		/**
+		 * Filter by invalid results.
+		 */
 
 		$config = [
 			'key'        => '',
@@ -74,8 +82,6 @@ class FilterTest extends TaskTestCase
 			],
 		];
 
-		$returnData = $this->execute( $config, $context, $data );
-
 		// Note the key indexes, these stay the same.
 		$expected = [
 			2 => [
@@ -87,6 +93,43 @@ class FilterTest extends TaskTestCase
 				'sku'   => '0',
 			],
 		];
+
+		$returnData = $this->execute( $config, $context, $data );
+		$this->assertEquals( $expected, $returnData );
+
+		// Expect the same result with a forced list.
+		$returnData = $this->execute( $config, $context, array_values( $data ) );
+		$this->assertEquals( array_values( $expected ), $returnData );
+
+		/**
+		 * Filter no-empty.
+		 */
+
+		$config = [
+			'key'        => '',
+			'method'     => 'valid',
+			'conditions' => [
+				[
+					'operator' => 'not_empty',
+				]
+			],
+		];
+
+		$data = [
+			null,
+			false,
+			1,
+			2,
+			50
+		];
+
+		$expected = [
+			1,
+			2,
+			50
+		];
+
+		$returnData = $this->execute( $config, $context, $data );
 
 		$this->assertEquals( $expected, $returnData );
 	}
