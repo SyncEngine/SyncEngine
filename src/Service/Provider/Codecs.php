7040590d925c990d8f983a394bc300cf2c906436
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-09-02 16:35:47 +0200

    Tmp fix for format names
    TODO: Use custom locator for formats.

--- src/Service/Provider/Codecs.php
+++ src/Service/Provider/Codecs.php
@@ -18,6 +18,18 @@ class Codecs extends AbstractServiceModelProvider
 	public function get( $name ): ?ServiceModel
 	{
 		// Support all lowercase.
-		return parent::get( ucfirst( $name ) );
+		$service = parent::get( ucfirst( $name ) );
+
+		if ( ! $service ) {
+			// @todo Use custom locator for formats.
+			$codecs = $this->getAll();
+			foreach ( $codecs as $codec ) {
+				if ( in_array( $name, $codec->getFormats(), true ) ) {
+					return $codec;
+				}
+			}
+		}
+
+		return $service;
 	}
 }
