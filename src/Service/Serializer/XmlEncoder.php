d88e7a1d52047accec9b6d375345e061ab170004
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-09-02 12:23:21 +0200

    Convert symfony based encoders to codec services

---
+++ src/Service/Serializer/XmlEncoder.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace SyncEngine\Service\Serializer;
+
+use SyncEngine\Service\Interface\CodecInterface;
+
+class XmlEncoder extends \Symfony\Component\Serializer\Encoder\XmlEncoder implements CodecInterface
+{
+
+}
