b6c3fce79d81af46e020eb11428afd96dc65b6b3
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-09-02 12:23:32 +0200

    Add URL/Form based codecs

---
+++ src/Codec/FormData.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace SyncEngine\Codec;
+
+use SyncEngine\Model\CodecModel;
+use SyncEngine\Service\Interface\CodecInterface;
+use SyncEngine\Service\Serializer\FormDataEncoder;
+
+class FormData extends CodecModel
+{
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->type        = 'formdata';
+		$this->name        = 'FormData';
+		$this->description = $this->trans( 'FormData encoded string' );
+	}
+
+	public function initEncoder( ?array $config = [] ): CodecInterface
+	{
+		return new FormDataEncoder();
+	}
+
+	public function getContentType( array $config = [], string $format = '' ): string
+	{
+		return 'application/x-www-form-urlencoded';
+		//return 'multipart/form-data';
+	}
+
+	public function getCodecFields( $defaults = [] ): array
+	{
+		return [];
+	}
+}
