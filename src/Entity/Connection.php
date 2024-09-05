9409c4df9bfa8d54cdfb2fb7917d37b61e0ac4cc
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-09-05 15:55:46 +0200

    Add data to all engine entities

--- src/Entity/Connection.php
+++ src/Entity/Connection.php
@@ -9,18 +9,4 @@ use SyncEngine\Repository\ConnectionRepository;
 #[ORM\Entity( repositoryClass: ConnectionRepository::class )]
 class Connection extends EngineEntity
 {
-	#[ORM\Column( nullable: true )]
-	private ?array $data = [];
-
-	public function getData(): array
-	{
-		return (array) $this->data;
-	}
-
-	public function setData( array $data ): self
-	{
-		$this->data = $data;
-
-		return $this;
-	}
 }
