148e46453f4327d99c292b49a62890fe45a64e93
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-09-10 10:48:06 +0200

    Move API v1 methods to own namespace

---
+++ src/Controller/API/v1/ApiV1Controller.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace SyncEngine\Controller\API\v1;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use SyncEngine\Controller\DefaultController;
+use SyncEngine\Model\Abstract\EntityModel;
+
+class ApiV1Controller extends DefaultController
+{
+
+	#[Route( '/api/v1/{entity}', name: 'api_list_entities', methods: [ 'GET' ] )]
+	public function list_entities( Request $request, string $entity ): JsonResponse
+	{
+		/** @var EntityModel $model */
+		$model = EntityModel::getEntityModelClass( $entity );
+
+		if ( ! $model ) {
+			return $this->json( [ 'message' => $this->trans( 'Entity not found' ) ], Response::HTTP_NOT_IMPLEMENTED );
+		}
+
+		try {
+			$query = $request->request->all();
+
+			// Max 100 items.
+			if ( ! isset( $query['limit'] ) || 100 < $query['limit'] ) {
+				$query['limit'] = 100;
+			}
+
+			$list = $model::getAll( $query );
+			foreach ( $list as &$entity ) {
+				$entity = $entity->normalize();
+			}
+		} catch ( \Exception $e ) {
+			return $this->json( [ 'message' => $this->trans( $e->getMessage() ) ], Response::HTTP_INTERNAL_SERVER_ERROR );
+		}
+
+		return $this->json( $list );
+	}
+
+
+	#[Route( '/api/v1/{entity}/{id}', name: 'api_get_entity', requirements: [ 'id' => '\d+' ], methods: [ 'GET' ] )]
+	public function get_entity( Request $request, string $entity, int $id = 0 ): JsonResponse
+	{
+		/** @var EntityModel $model */
+		$model = EntityModel::getEntityModelClass( $entity );
+
+		if ( ! $model ) {
+			return $this->json( [ 'message' => $this->trans( 'Entity not found' ) ], Response::HTTP_NOT_IMPLEMENTED );
+		}
+
+		try {
+			$fetch = $model::get( $id )->normalize();
+		} catch ( \Exception $e ) {
+			return $this->json( [ 'message' => $this->trans( $e->getMessage() ) ], Response::HTTP_INTERNAL_SERVER_ERROR );
+		}
+
+		return $this->json( $fetch );
+	}
+}
--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -7,7 +7,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use SyncEngine\Entity\Automation;
-use SyncEngine\Model\Abstract\EntityModel;
 use SyncEngine\Model\AutomationModel;
 use SyncEngine\Service\Execute;
 use SyncEngine\Service\ExecuteContext;
@@ -18,55 +17,6 @@ class ApiController extends DefaultController
 	public function index(): JsonResponse
 	{
 		return $this->json( [ 'status' => 'online' ] );
-	}
-
-	#[Route( '/api/v1/{entity}', name: 'api_list_entities', methods: [ 'GET' ] )]
-	public function list_entities( Request $request, string $entity ): JsonResponse
-	{
-		/** @var EntityModel $model */
-		$model = EntityModel::getEntityModelClass( $entity );
-
-		if ( ! $model ) {
-			return $this->json( [ 'message' => $this->trans( 'Entity not found' ) ], Response::HTTP_NOT_IMPLEMENTED );
-		}
-
-		try {
-			$query = $request->request->all();
-
-			// Max 100 items.
-			if ( ! isset( $query['limit'] ) || 100 < $query['limit'] ) {
-				$query['limit'] = 100;
-			}
-
-			$list = $model::getAll( $query );
-			foreach ( $list as &$entity ) {
-				$entity = $entity->normalize();
-			}
-		} catch ( \Exception $e ) {
-			return $this->json( [ 'message' => $this->trans( $e->getMessage() ) ], Response::HTTP_INTERNAL_SERVER_ERROR );
-		}
-
-		return $this->json( $list );
-	}
-
-
-	#[Route( '/api/v1/{entity}/{id}', name: 'api_get_entity', requirements: [ 'id' => '\d+' ], methods: [ 'GET' ] )]
-	public function get_entity( Request $request, string $entity, int $id = 0 ): JsonResponse
-	{
-		/** @var EntityModel $model */
-		$model = EntityModel::getEntityModelClass( $entity );
-
-		if ( ! $model ) {
-			return $this->json( [ 'message' => $this->trans( 'Entity not found' ) ], Response::HTTP_NOT_IMPLEMENTED );
-		}
-
-		try {
-			$fetch = $model::get( $id )->normalize();
-		} catch ( \Exception $e ) {
-			return $this->json( [ 'message' => $this->trans( $e->getMessage() ) ], Response::HTTP_INTERNAL_SERVER_ERROR );
-		}
-
-		return $this->json( $fetch );
 	}
 
 	#[Route( '/api/endpoints', name: 'api_list_endpoints', methods: [ 'GET' ] )]
