2f584d8da8dce56d6eb46ed23078730ab50e17da
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-09-18 23:41:44 +0200

    Refactor to use TraceLog handler
    Moves logic from context to the log handler

--- src/Service/Trace/TraceLog.php
+++ src/Service/Trace/TraceLog.php
@@ -2,49 +2,95 @@
 
 namespace SyncEngine\Service\Trace;
 
+use Symfony\Contracts\HttpClient\ResponseInterface;
 use SyncEngine\Service\ResourceData;
 use SyncEngine\Service\Trace\Enum\TraceLogType;
 
-class TraceLog
+class TraceLog extends ResourceData
 {
-	protected string $message = '';
-	protected int|float $timestamp;
-	protected TraceLogType $type;
-	protected ?ResourceData $info = null;
-	protected ?TraceContext $traceContext = null;
-
-	public function __construct( string|array|ResourceData $message, string|TraceLogType $type = 'log', TraceContext $traceContext = null )
+	public static function create( $resource = [], string|TraceLogType $type = 'log', TraceContext $context = null ): static
 	{
-		if ( is_array( $message ) ) {
-			$this->message = $message['message'] ?? '';
-			$this->info = ResourceData::create( $message );
-		} else {
-			$this->message = $message;
+		if ( $resource instanceof TraceLog ) {
+			return $resource;
 		}
 
-		$this->setTimestamp();
-		$this->type = TraceLogType::create( $type );
-		$this->traceContext = $traceContext;
+		$instance = new static();
+
+		if ( $resource instanceof \Throwable ) {
+			$instance->setException( $resource );
+		} elseif ( $resource instanceof ResponseInterface ) {
+			$instance->setResponse( $resource );
+		} else {
+			if ( $resource instanceof ResourceData ) {
+				$resource = $resource->normalize();
+			}
+
+			if ( is_array( $resource ) ) {
+				$instance['message'] = $resource['message'] ?? '';
+				unset( $resource['message'] );
+				$instance->setInfo( $resource );
+			} else {
+				$instance['message'] = $resource;
+			}
+		}
+
+		$instance->setTimestamp();
+		$instance->setType( TraceLogType::create( $type ) );
+		$instance->setContext( $context );
+
+		return $instance;
+	}
+
+	public function getName(): string
+	{
+		return $this->getTypeString() . ' ' . $this->getTimestamp();
+	}
+
+	public function setInfo( $info ): static
+	{
+		if ( $info instanceof ResponseInterface ) {
+			$this->response = $info;
+		} elseif ( $info instanceof \Throwable ) {
+			$this->exception = $info;
+		} else {
+			$info = ResourceData::create( $this->get( 'info' ) );
+			$info->insert( $info );
+			$this->set( $info, 'info' );
+		}
+
+		return $this;
 	}
 
 	public function getMessage(): string
 	{
-		return $this->message;
-	}
-
-	public function setTimestamp( float $microtime = null ): static
-	{
-		$this->timestamp = $microtime ?: microtime( true );
+		return $this->get( 'message' );
 	}
 
 	public function getTimestamp( $microtime = true ): int|float
 	{
-		return $microtime ? $this->timestamp : (int) $this->timestamp;
+		$timestamp = (float) $this->get( 'timestamp', 0 );
+
+		return $microtime ? $timestamp : (int) $timestamp;
+	}
+
+
+	public function setTimestamp( float $microtime = null ): static
+	{
+		$this->set( $microtime ?: microtime( true ), 'timestamp' );
+
+		return $this;
 	}
 
 	public function getContext(): ?TraceContext
 	{
-		return $this->traceContext;
+		return $this->get( 'context' );
+	}
+
+	public function setContext( TraceContext $context ): static
+	{
+		$this->set( $context, 'context' );
+
+		return $this;
 	}
 
 	public function getType(): TraceLogType
@@ -52,8 +98,97 @@ class TraceLog
 		return $this->type;
 	}
 
+	public function setType( TraceLogType $type ): static
+	{
+		$this->type = $type;
+
+		return $this;
+	}
+
+	public function getResponse(): ResponseInterface
+	{
+		return $this->get( 'response' );
+	}
+
+	public function setResponse( ResponseInterface $response ): static
+	{
+		$this->set( $response, 'response' );
+
+		return $this;
+	}
+
+	public function getException(): \Throwable
+	{
+		return $this->get( 'exception' );
+	}
+
+	public function setException( \Throwable $exception ): static
+	{
+		$this->set( $exception, 'exception' );
+
+		return $this;
+	}
+
+	public function getFile(): string
+	{
+		return $this->get( 'file' );
+	}
+
+	public function setFile( string $file ): static
+	{
+		$this->set( $file, 'file' );
+
+		return $this;
+	}
+
 	public function getTypeString(): string
 	{
-		return $this->type->value;
+		return $this->getType()?->value;
+	}
+
+	public static function parseException( \Throwable $throwable, iterable $trace = [] ): array
+	{
+		$trace['message'] = json_decode( $throwable->getMessage(), true ) ?? $throwable->getMessage();
+
+		if ( method_exists( $throwable, 'getResponse' ) ) {
+			/** @var ResponseInterface $response */
+			$response = $throwable->getResponse();
+			if ( $response instanceof ResponseInterface ) {
+				$trace['response'] = static::parseResponse( $throwable->getResponse() );
+			}
+		}
+		if ( method_exists( $throwable, 'getDebugInfo' ) ) {
+			$trace['debug'] = $throwable->getDebugInfo();
+		}
+
+		$trace['line']       = $throwable->getLine();
+		$trace['file']       = $throwable->getFile();
+		$trace['backtrace']  = explode( "\n", $throwable->getTraceAsString() );
+		$trace['_class']     = $throwable::class;
+
+		return $trace;
+	}
+
+	public static function parseResponse( ResponseInterface $response, iterable $trace = [] ): array
+	{
+		if ( method_exists( $response, 'getInfo' ) ) {
+			$trace['info'] = $response->getInfo();
+		}
+		try {
+			if ( method_exists( $response, 'getHeaders' ) ) {
+				$trace['headers'] = $response->getHeaders();
+			}
+		} catch ( \Throwable $e ) {
+			$trace['headers'] = $e->getMessage();
+		}
+		try {
+			if ( method_exists( $response, 'getContent' ) ) {
+				$trace['content'] = $response->getContent( false );
+			}
+		} catch ( \Throwable $e ) {
+			$trace['content'] = $e->getMessage();
+		}
+
+		return $trace;
 	}
 }
