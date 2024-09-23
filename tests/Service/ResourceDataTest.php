b73845c8e449259a989581afe7d5d1f1535cad6a
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-09-24 01:02:50 +0200

    Also fix merge tests for list behavior

--- tests/Service/ResourceDataTest.php
+++ tests/Service/ResourceDataTest.php
@@ -386,38 +386,38 @@ class ResourceDataTest extends TestCase
 	public function testMerge()
 	{
 		$data = new ResourceData( [
-			[
+			'_1' => [
 				'id' => 1,
 				'name' => 'One',
 			],
-			[
+			'_2' => [
 				'id' => 2,
 				'name' => 'Two',
 			],
-			[
+			'_3' => [
 				'id' => 3,
 				'name' => 'Three',
 			],
-			[
+			'_4' => [
 				'id' => 4,
 				'name' => 'Four',
 			],
 		] );
 
 		$new = new ResourceData( [
-			[
+			'_1' => [
 				'id' => 10,
 				'name' => 'Ten',
 			],
-			[
+			'_2' => [
 				'id' => 20,
 				'name' => 'Twenty',
 			],
-			[
+			'_3' => [
 				'id' => 30,
 				'name' => 'Thirty',
 			],
-			[
+			'_4' => [
 				'id' => 40,
 				'name' => 'Forty',
 			],
@@ -425,6 +425,14 @@ class ResourceDataTest extends TestCase
 
 		// Overrides because the keys are the same.
 		$this->assertEquals( $new->get(), $data->merge( $new )->get() );
+
+		$data = $data->list();
+		$new  = $data->list();
+
+		// In case if lists the values will be appended.
+		$this->assertEquals( array_merge( $data->get(), $new->get() ), $data->insert( $new )->get() );
+		// @todo Should ResourceData be immutable?
+		$this->assertEquals( 8, $data->count() );
 
 		$data = new ResourceData( [
 			'foo' => 'bar',
