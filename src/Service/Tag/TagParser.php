72f88d8cb95be5381e8ed2122d40d07f15f0317c
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-09-25 14:34:55 +0200

    Move filters into trait
    Start on separation of filters from parser

--- src/Service/Tag/TagParser.php
+++ src/Service/Tag/TagParser.php
@@ -9,6 +9,8 @@ use SyncEngine\Service\Format\DateTimeFormatter;
 
 class TagParser
 {
+	use TagFilter;
+
 	protected ResourceData $resource;
 	protected TagExtractor $extractor;
 	protected bool $recurseMode;
@@ -273,62 +275,5 @@ class TagParser
 		}
 
 		return $value;
-	}
-
-	protected function filterCount( $value ): string
-	{
-		if ( ! is_countable( $value ) ) {
-			return 0;
-		}
-		return count( $value );
-	}
-
-	protected function filterTrim( $value, ...$config ): string
-	{
-		return trim( $value, reset( $config ) );
-	}
-
-	protected function filterFormat( $value, string $format = ',', ...$config ): mixed
-	{
-		if ( ! is_array( $value ) ) {
-			return $value;
-		}
-
-		$formatter = new DataFormatter();
-
-		return $formatter->encodeFormat( $format, $value, $config );
-	}
-
-	protected function filterString( $value, string $separator = ',', ...$config ): string
-	{
-		if ( ! is_array( $value ) ) {
-			return (string) $value;
-		}
-
-		if ( 2 < strlen( $separator ) ) {
-			$formatter = new DataFormatter();
-
-			if ( array_key_exists( $separator, $formatter->getFormats() ) ) {
-				return $formatter->encodeFormat( $separator, $value, $config );
-			}
-		}
-
-		if ( ! empty( $config[0] ) ) {
-			$enclose = $config[0];
-
-			return $enclose . implode( $enclose . $separator . $enclose, $value ) . $enclose;
-		}
-
-		return implode( $separator, $value );
-	}
-
-	protected function filterDate( $value, string $format = '', string $timezone = '' ): string
-	{
-		$formatter = new DateTimeFormatter( [
-			DateTimeFormatter::FORMAT    => $format ?? null,
-			DateTimeFormatter::TIMEZONE  => $timezone ?? null,
-		] );
-
-		return $formatter->format( $value );
 	}
 }
