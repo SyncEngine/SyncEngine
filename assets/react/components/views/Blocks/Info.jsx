87dc40121672b17b5c702e4b7354f6d83adb5eeb
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-10-01 14:26:48 +0200

    Icon support for info block (tasks)

--- assets/react/components/views/Blocks/Info.jsx
+++ assets/react/components/views/Blocks/Info.jsx
@@ -1,33 +1,38 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import { isObject } from '../../../utils/conditions';
 import { HStack, VStack } from '../../partials/Stack';
 import BadgeControl from './Badge';
 import ValueControl from './Value';
+import Icon from '../../partials/Icon';
 
-export default function Info( props ) {
+export default forwardRef( function Info( props, ref ) {
 	const {
 		item = {},
 		type = item.type,
 		badge,
 	} = props;
 
-	const classes = props.className ?? 'justify-content-center';
-
 	const sub = isObject( props.sub ) ? <ValueControl { ...props } { ...props.sub } /> : item[ props.sub ?? 'description' ];
+	const icon = props.icon ?? item.icon;
 
 	return (
-		<VStack className={ classes }>
-			<HStack gap={2}>
-				{ '' !== props.value &&
-					<span><ValueControl default="--" { ...props } /></span>
-				}
-				{ badge &&
-					<BadgeControl type={ type } { ...( isObject( badge ) ? badge : { label: badge } ) } item={ item } />
-				}
-			</HStack>
-			{ sub &&
-				<small>{ sub }</small>
+		<HStack className={ props.className } ref={ ref }>
+			{ icon &&
+			  <Icon icon={ icon } className="fs-5 me-2 d-flex align-items-center" />
 			}
-		</VStack>
+			<VStack>
+				<HStack gap={2}>
+					{ '' !== props.value &&
+					  <span><ValueControl default="--" { ...props } /></span>
+					}
+					{ badge &&
+					  <BadgeControl type={ type } { ...( isObject( badge ) ? badge : { label: badge } ) } item={ item } />
+					}
+				</HStack>
+				{ sub &&
+				  <small>{ sub }</small>
+				}
+			</VStack>
+		</HStack>
 	)
-}
+} );
