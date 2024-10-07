a4879b3f72898cf4284d93108bc766b1c67dbeca
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-10-08 01:06:54 +0200

    SVG support with caching in global

--- assets/react/components/partials/Icon.jsx
+++ assets/react/components/partials/Icon.jsx
@@ -1,5 +1,24 @@
 import React, { forwardRef } from 'react';
 import useGlobal from '../../hooks/useGlobal';
+import { isString } from '../../utils/conditions';
+
+/**
+ * Parses the HTML into a DOM node which will also remove scripts and other
+ * @param html
+ * @return { SVGElement }
+ */
+function createSvg( html ) {
+	const element = document.createElement('span');
+	element.innerHTML = html;
+
+	const icon = element.firstChild instanceof SVGElement ? element.firstChild : document.createElement( 'svg' );
+
+	icon.setAttribute( 'width', '1em' );
+	icon.setAttribute( 'height', '1em' );
+	icon.setAttribute( 'fill', 'currentColor' );
+
+	return icon;
+}
 
 export default forwardRef( function Icon( props, ref ) {
 	const app = useGlobal();
@@ -21,12 +40,24 @@ export default forwardRef( function Icon( props, ref ) {
 		override.className += ' ' + props.className;
 	}
 
+	const iconRef = isString( icon ) ? icon : null;
+
 	if ( icon ) {
-		if ( app.icons.hasOwnProperty( icon ) ) {
-			icon = app.icons[ icon ];
+		if ( iconRef && app.icons.hasOwnProperty( iconRef ) ) {
+			icon = app.icons[ iconRef ];
 		}
 
-		if ( icon.startsWith( prefix + ' ' ) ) {
+		if ( icon instanceof Element ) {
+			override.icon = null
+			override.dangerouslySetInnerHTML = { __html: icon.outerHTML };
+		} else if ( icon.startsWith( '<svg' ) ) {
+			icon = createSvg( icon );
+			if ( iconRef ) {
+				app.icons[ iconRef ] = icon;
+			}
+			override.icon = null
+			override.dangerouslySetInnerHTML = { __html: icon.outerHTML };
+		} else if ( icon.startsWith( prefix + ' ' ) ) {
 			override.className += ' ' + icon;
 		} else {
 			override.className += ' ' + prefix;
