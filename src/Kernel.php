33914166cc00cbc2d312fe628ae27d42935811df
Inform-All <NKoonen@users.noreply.github.com>
2024-10-11 14:42:53 +0200

    initial registry for module vendors

--- src/Kernel.php
+++ src/Kernel.php
@@ -12,8 +12,47 @@ class Kernel extends BaseKernel
 {
 	use MicroKernelTrait;
 
+	public function buildModulesRegister(): void
+	{
+		$modulesPath = $this->getProjectDir() . '/modules';
+
+		$vendors = array_filter(
+			scandir( $modulesPath ),
+			function ( $folder ) use ( $modulesPath ) {
+				return is_dir( $modulesPath . '/' . $folder ) && ! in_array( $folder, [ '.', '..' ] );
+			}
+		);
+
+		$register = [];
+		foreach ( $vendors as $vendor ) {
+			$vendorPath = $modulesPath . '/' . $vendor;
+			$modules    = array_filter(
+				scandir( $vendorPath ),
+				function ( $folder ) use ( $vendorPath ) {
+					return is_dir( $vendorPath . '/' . $folder ) && ! in_array( $folder, [ '.', '..' ] );
+				}
+			);
+
+			foreach ( $modules as $moduleFolder ) {
+				$filename     = 'register.php';
+				$autoloadPath = $vendorPath . '/' . $moduleFolder . '/' . $filename;
+				$autoloadFile = $vendor . '/' . $moduleFolder . '/' . $filename;
+
+				if ( file_exists( $autoloadPath ) ) {
+					$register[] = "include __DIR__ . '/{$autoloadFile}';";
+				}
+			}
+		}
+
+		$register = "<?php\n" . implode( "\n", $register );
+
+		file_put_contents( $this->getProjectDir() . '/modules/registry.php', $register, LOCK_EX );
+	}
+
 	protected function build( ContainerBuilder $container ): void
 	{
+		$this->buildModulesRegister();
+
 		parent::build( $container );
 
 		$container->registerAttributeForAutoconfiguration(MenuItem::class, static function (
