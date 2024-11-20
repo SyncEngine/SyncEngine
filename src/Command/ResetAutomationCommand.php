0e08fd7c9fa9804cf4e07053139bdf4f1da69f8c
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-11-20 15:47:42 +0100

    Output available automations if argument is not provided

--- src/Command/ResetAutomationCommand.php
+++ src/Command/ResetAutomationCommand.php
@@ -4,6 +4,7 @@ namespace SyncEngine\Command;
 
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -26,12 +27,28 @@ class ResetAutomationCommand extends Command
 
 	protected function configure(): void
 	{
-		$this->addArgument( 'automation', InputArgument::REQUIRED, 'The automation ID or Ref.' );
+		$this->addArgument( 'automation', InputArgument::OPTIONAL, 'The automation ID or Ref.' );
 	}
 
 	protected function execute( InputInterface $input, OutputInterface $output ): int
 	{
 		$automation = $input->getArgument( 'automation' );
+
+		if ( ! $automation ) {
+			$automations = AutomationModel::getAll( [] );
+
+			$output->writeln( '<comment>Available automations:</comment>' );
+			$table = new Table( $output );
+
+			$table->setHeaders(['ID', 'Ref', 'Name']);
+			foreach ( $automations as $automation ) {
+				// @todo Access validation when implemented.
+				$table->addRow([$automation->getId(), $automation->getRef(), $automation->getName()]);
+			}
+			$table->render();
+
+			return Command::SUCCESS;
+		}
 
 		$model = AutomationModel::get( $automation );
 
