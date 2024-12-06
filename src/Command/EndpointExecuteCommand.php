941f03ba1c5ac431dc8676d1d082f1b7eebf385b
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-12-06 23:37:47 +0100

    Event driven progress bar on execute CLI

--- src/Command/EndpointExecuteCommand.php
+++ src/Command/EndpointExecuteCommand.php
@@ -4,6 +4,7 @@ namespace SyncEngine\Command;
 
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\ProgressIndicator;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -24,7 +25,12 @@ use SyncEngine\Service\ExecuteContext;
 class EndpointExecuteCommand extends EndpointCommand
 {
 	private Execute $execute;
-	private static ?OutputInterface $_output = null;
+	private ?OutputInterface $output = null;
+
+	/**
+	 * @var ProgressIndicator[]
+	 */
+	private array $progress;
 
 	public function __construct( Execute $execute, DefaultController $controller )
 	{
@@ -39,7 +45,7 @@ class EndpointExecuteCommand extends EndpointCommand
 
 	protected function execute( InputInterface $input, OutputInterface $output ): int
 	{
-		self::$_output = $output;
+		$this->output = $output;
 
 		$endpoint = $input->getArgument( 'endpoint' );
 
@@ -64,23 +70,68 @@ class EndpointExecuteCommand extends EndpointCommand
 			foreach ( $result['errors'] as $error ) {
 				$output->writeln( '<error>' . $error['message'] . '</error>' );
 			}
-		} else {
-			$output->writeln( '<comment>Endpoint completed</comment>: <info>' . $endpoint . '</info>' );
 		}
 
-		self::$_output = null;
+		$this->output = null;
 
 		return ( $success ) ? Command::SUCCESS : Command::FAILURE;
 	}
 
+	public function setProgress( ExecuteEvent $event ): void
+	{
+		if ( empty( $this->output ) ) {
+			return;
+		}
+
+		$endpoint = $event->getExecuteContext()->getAutomation()->getEndpoint();
+		$progress = $this->getProgress( $event );
+
+		switch ( $event->getEventName() ) {
+			case 'stop':
+				//$progress->setMessage( '<comment>Endpoint stopped</comment>: <info>' . $endpoint . '</info>' );
+			break;
+			case 'success':
+				$progress->finish( '<comment>Endpoint completed</comment>: <info>' . $endpoint . '</info>' );
+			break;
+			case 'error':
+				$progress->finish( 'Failed' );
+			break;
+			default:
+				$progress->setMessage( '<comment>Execute endpoint</comment>: <info>' . $event->getEventName() . '</info> > <info>' . $endpoint . '</info>' );
+				$progress->advance();
+		}
+	}
+
+	public function getProgress( ExecuteEvent $event ): ?ProgressIndicator
+	{
+		if ( empty( $this->output ) ) {
+			return null;
+		}
+
+		$endpoint = $event->getExecuteContext()->getAutomation()->getEndpoint();
+
+		if ( ! isset( $this->progress[ $endpoint ] ) ) {
+			$this->progress[ $endpoint ] = new ProgressIndicator( $this->output, 'very_verbose', 100, ['⠏', '⠛', '⠹', '⢸', '⣰', '⣤', '⣆', '⡇'] );
+			$this->progress[ $endpoint ]->start( '<comment>Executing endpoint</comment>: <info>' . $endpoint . '</info>' );
+		}
+
+		return $this->progress[ $endpoint ];
+	}
+
 	#[AsEventListener( event: 'syncengine.execute.trigger' )]
 	#[AsEventListener( event: 'syncengine.execute.start' )]
-	#[AsEventListener( event: 'syncengine.execute.stop' )]
 	#[AsEventListener( event: 'syncengine.execute.error' )]
 	#[AsEventListener( event: 'syncengine.execute.success' )]
 	public function executeEvent( ExecuteEvent $event ): void
 	{
-		$endpoint = $event->getExecuteContext()->getAutomation()->getEndpoint();
-		self::$_output?->writeln( '<comment>Execute event</comment>: <info>' . $event->getEventName() . '</info> > <info>' . $endpoint . '</info>' );
+		$this->setProgress( $event );
+	}
+
+	#[AsEventListener( event: 'syncengine.execute.flow' )]
+	#[AsEventListener( event: 'syncengine.execute.step' )]
+	#[AsEventListener( event: 'syncengine.execute.task' )]
+	public function executeTraceEvent( ExecuteEvent $event ): void
+	{
+		$this->getProgress( $event )?->advance();
 	}
 }
