f9f4e89aba959cb9ea9f9aeac8694e94d3ddde11
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-12-12 17:06:38 +0100

    Add loop test

---
+++ tests/Task/LoopTest.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace SyncEngine\Tests\Task;
+
+use SyncEngine\Service\ExecuteData;
+use SyncEngine\Tests\TestCase\TaskTestCase;
+
+class LoopTest extends TaskTestCase
+{
+	protected string $_task = 'Loop';
+
+	public function testLoop(): void
+	{
+		$context = $this->getContext();
+
+		$config = [
+			'key'        => '',
+			'action'     => 'tasks',
+			'tasks' => [
+				[
+					'_ref' => 'test',
+					'_class' => 'Trace',
+					'message' => 'Current Index: {{ context.loop.index }}',
+					'info' => '{{ context.loop }}'
+				],
+				[
+					'_ref' => 'testSet',
+					'_class' => 'Set',
+					'params' => [
+						[
+							'key' => 'foo.uno',
+							'value' => '10',
+						],
+						[
+							'key' => 'foo.dos',
+							'value' => '20',
+						]
+					],
+				],
+				[
+					'_ref'   => 'subLoop',
+					'_class' => 'Loop',
+					'key'    => 'foo',
+					'action' => 'tasks',
+					'tasks'  => [
+						[
+							'_ref' => 'testasd',
+							'_class' => 'Trace',
+							'message' => 'Current Index: {{ context.loop.index }}',
+							'info' => '{{ context.loop }}'
+						],
+					]
+				]
+			],
+		];
+
+		$data = [
+			'one' => [ 1, '1' ],
+			'two' => [ 2, '2' ],
+			'three' => [ 3, '3' ],
+		];
+
+		$expected = [
+			'one' => [ 1, '1', 'foo' => [ 'uno' => '10', 'dos' => '20' ] ],
+			'two' => [ 2, '2', 'foo' => [ 'uno' => '10', 'dos' => '20' ] ],
+			'three' => [ 3, '3', 'foo' => [ 'uno' => '10', 'dos' => '20' ] ],
+		];
+
+		$returnData = $this->execute( $config, $context, $data );
+
+		$this->assertEquals( $expected, ExecuteData::create( $returnData )->normalize() );
+
+		$logs = $context->getLogs();
+		foreach ( $logs as &$log ) {
+			$log = (string) $log;
+		}
+
+		$expected = [
+			'Current Index: one', // main.
+			'Current Index: uno',
+			'Current Index: dos',
+			'Current Index: two', // main.
+			'Current Index: uno',
+			'Current Index: dos',
+			'Current Index: three', // main.
+			'Current Index: uno',
+			'Current Index: dos',
+		];
+
+		$this->assertEquals( $expected, $logs );
+	}
+}
