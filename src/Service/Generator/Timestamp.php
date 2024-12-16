634bc2bc84b4126894c645d8e291b4e82d39e6f2
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-12-16 12:46:24 +0100

    baseTimestamp option for Timestamp generator

--- src/Service/Generator/Timestamp.php
+++ src/Service/Generator/Timestamp.php
@@ -6,10 +6,10 @@ use SyncEngine\Service\Interface\GeneratorInterface;
 
 class Timestamp implements GeneratorInterface
 {
-	public function generate( string $from = '' ): string
+	public function generate( string $from = '', ?int $baseTimestamp = null ): string
 	{
 		if ( $from ) {
-			return strtotime( $from );
+			return strtotime( $from, $baseTimestamp );
 		}
 		return time();
 	}
