4b7da1add27c4de382d3ab12e214a2b5bcfd3e9e
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2024-12-19 20:15:39 +0100

    Create basic AbstractCollection and fix float offset params (cast to string)
    Using floats for arrays by default casts them to integers which results in various warnings and bugs

---
+++ src/Service/Collection/AbstractCollection.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace SyncEngine\Service\Collection;
+
+abstract class AbstractCollection implements CollectionInterface
+{
+	protected $collection;
+
+	public static function fixFloatOffset( mixed $offset ): mixed
+	{
+		return is_float( $offset ) ? (string) $offset : $offset;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function offsetExists( mixed $offset ): bool
+	{
+		return isset( $this->collection[ self::fixFloatOffset( $offset ) ] );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function offsetGet( mixed $offset ): mixed
+	{
+		return $this->collection[ self::fixFloatOffset( $offset ) ] ?? null;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function offsetSet( mixed $offset, mixed $value ): void
+	{
+		$this->collection[ self::fixFloatOffset( $offset ) ] = $value;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function offsetUnset( mixed $offset ): void
+	{
+		unset( $this->collection[ self::fixFloatOffset( $offset ) ] );
+	}
+
+	public function current(): mixed
+	{
+		return current( $this->collection );
+	}
+
+	public function next(): void
+	{
+		next( $this->collection );
+	}
+
+	public function key(): mixed
+	{
+		return key( $this->collection );
+	}
+
+	public function valid(): bool
+	{
+		return null !== $this->key();
+	}
+
+	public function rewind(): void
+	{
+		reset( $this->collection );
+	}
+
+	public function count(): int
+	{
+		return count( $this->collection );
+	}
+}
---
+++ src/Service/Collection/CollectionInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace SyncEngine\Service\Collection;
+
+interface CollectionInterface extends \ArrayAccess, \Iterator, \Countable
+{
+
+}
