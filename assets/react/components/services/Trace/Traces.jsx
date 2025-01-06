0a3703c90124edb7ef18e1fb26b181fbd06771c1
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2025-01-06 15:09:05 +0100

    Add hover labels for trace diagnostic badges

--- assets/react/components/services/Trace/Traces.jsx
+++ assets/react/components/services/Trace/Traces.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { HStack, Stack } from '../../partials/Stack';
 import { DurationValue, MemoryValue } from '../../views/Blocks/Value';
 import Badge from '../../partials/Badge';
@@ -50,26 +51,27 @@ function TradeItem( props ) {
 	let disabled = item.config?._disabled;
 
 	return (
-			<AccordionSticky.Item eventKey={ index } key={ index + item._key } ref={ item._ref }>
-				<AccordionSticky.Header className={ disabled && 'opacity-75 grayscale' }>
-					<HStack className="justify-content-between w-100 me-2" gap={2}>
-						{ item.log ?
-							<TraceLogHeader item={ item } find={ find } />
-								:
-							<TraceNodeHeader item={ item } />
-						}
-					</HStack>
-				</AccordionSticky.Header>
-				<AccordionSticky.Collapse eventKey={ index } unmountOnExit>
-					<AccordionSticky.Body>
-						<TraceNode item={ item } />
-					</AccordionSticky.Body>
-				</AccordionSticky.Collapse>
-			</AccordionSticky.Item>
+		<AccordionSticky.Item eventKey={ index } key={ index + item._key } ref={ item._ref }>
+			<AccordionSticky.Header className={ disabled && 'opacity-75 grayscale' }>
+				<HStack className="justify-content-between w-100 me-2" gap={2}>
+					{ item.log ?
+						<TraceLogHeader item={ item } find={ find } />
+							:
+						<TraceNodeHeader item={ item } />
+					}
+				</HStack>
+			</AccordionSticky.Header>
+			<AccordionSticky.Collapse eventKey={ index } unmountOnExit>
+				<AccordionSticky.Body>
+					<TraceNode item={ item } />
+				</AccordionSticky.Body>
+			</AccordionSticky.Collapse>
+		</AccordionSticky.Item>
 	);
 }
 
 function TraceNodeHeader( props ) {
+	const { t } = useTranslation();
 	const {
 		item,
 	} = props;
@@ -95,8 +97,8 @@ function TraceNodeHeader( props ) {
 	return (
 		<HStack className="flex-wrap" gap={2}>
 			<Badge bg={ variant } subtle>{ count }x</Badge>
-			{ disabled && <OverlayToggle overlay={ isString( disabled ) ? disabled : 'Disabled' }><Icon icon={ 'trace-disabled' } /></OverlayToggle> }
-			{ skipped && <OverlayToggle overlay={ isString( skipped ) ? skipped : 'Skipped' }><Icon variant={ variant } icon={ 'trace-skipped' } /></OverlayToggle> }
+			{ disabled && <OverlayToggle overlay={ isString( disabled ) ? disabled : t("Disabled") }><Icon icon={ 'trace-disabled' } /></OverlayToggle> }
+			{ skipped && <OverlayToggle overlay={ isString( skipped ) ? skipped : t("Skipped") }><Icon variant={ variant } icon={ 'trace-skipped' } /></OverlayToggle> }
 			{ type && <Badge bg={ variant } subtle><Icon icon={ iconType( type ) } className="me-1" />{ type }</Badge> }
 			{ title && <small>{ title }</small> }
 			{ ( ref && ref !== title ) && <Badge bg={ variant } subtle><Icon icon="ref" className="me-1" />{ ref }</Badge> }
@@ -104,7 +106,7 @@ function TraceNodeHeader( props ) {
 			  <Badge bg={ variant } subtle>
 				  <Icon icon="trace-duration" className="me-1" />
 				  { ( item.count && 1 < item.count ) &&
-				    <span>~<DurationValue value={ Math.round( ( duration / item.count ) * 1000 ) } ms={ true } /> / </span>
+				    <span title={ t("Avg. time per iteration") }>~<DurationValue value={ Math.round( ( duration / item.count ) * 1000 ) } ms={ true } /> / </span>
 				  }
 				  <DurationValue value={ Math.round( duration * 1000 ) } ms={ true } />
 			  </Badge>
@@ -113,10 +115,10 @@ function TraceNodeHeader( props ) {
 			  <Badge bg={ variant } subtle>
 				  <Icon icon="memory" className="me-1" />
 				  { ( item.count && 1 < item.count ) &&
-				    <span>~<MemoryValue value={ Math.round( item.memory_total / item.count ) } initialView="K" /> | </span>
+				    <span title={ t("Avg. memory per iteration") }>~<MemoryValue value={ Math.round( item.memory_total / item.count ) } initialView="K" /> | </span>
 				  }
-				  <span>^<MemoryValue value={ item.memory_peak  } initialView="K" /></span>
-				  <span> | <MemoryValue value={ item.memory_enter  } initialView="M" /> > <MemoryValue value={ item.memory_leave } initialView="M" /></span>
+				  <span title={ t("Peak memory increase") }>^<MemoryValue value={ item.memory_peak  } initialView="K" /></span>
+				  <span title={ t("Memory usage before > after") }> | <MemoryValue value={ item.memory_enter  } initialView="M" /> > <MemoryValue value={ item.memory_leave } initialView="M" /></span>
 			  </Badge>
 			}
 		</HStack>
