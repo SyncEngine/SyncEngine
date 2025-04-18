d729a4b1037e6c9718c2c59ca91d2aa60d11b66c
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2025-04-18 14:18:46 +0200

    Show progress bar in trace nodes

---
+++ assets/react/components/partials/Progress/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { ProgressBar as ProgressBarBootstrap } from 'react-bootstrap';
+
+export default function ProgressBar( props ) {
+	return <ProgressBarBootstrap { ...props } />
+}
