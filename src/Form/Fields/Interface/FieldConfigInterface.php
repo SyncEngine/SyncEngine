f767dd9f4751472e373c22ab1de117bd0c4fbee9
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2025-04-22 23:01:32 +0200

    Add setters

--- src/Form/Fields/Interface/FieldConfigInterface.php
+++ src/Form/Fields/Interface/FieldConfigInterface.php
@@ -5,13 +5,42 @@ namespace SyncEngine\Form\Fields\Interface;
 interface FieldConfigInterface
 {
 	public function getName(): string;
-	public function getLabel(): string;
+
+	public function setName( string $name ): self;
+
+	public function getLabel(): string|array;
+
+	public function setLabel( string|array $label ): self;
+
 	public function getType(): string;
+
+	public function setType( string $type ): self;
+
 	public function getDescription(): string;
+
+	public function setDescription( string $description ): self;
+
 	public function getHelp(): string|array;
+
+	public function setHelp( string|array $help ): self;
+
 	public function getDefaultValue();
+
+	public function setDefaultValue( $value ): self;
+
 	public function getConditions(): iterable; // @todo Conditions component.
+
+	public function setConditions( iterable $conditions ): self;
+
 	public function isRequired(): bool;
+
+	public function setRequired( bool $required ): self;
+
 	public function isDisabled(): bool;
+
+	public function setDisabled( bool $disabled ): self;
+
 	public function isReadonly(): bool;
+
+	public function setReadonly( bool $readonly ): self;
 }
