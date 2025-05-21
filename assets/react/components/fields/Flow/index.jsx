c35a9fb19b8cf32969cca6bf2f49a415586acfa8
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2025-05-21 16:29:11 +0200

    Start on flow field (concept stage)

---
+++ assets/react/components/fields/Flow/index.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { HStack, VStack } from '../../partials/Stack';
+import Entities from '../Entities';
+import Icon from '../../partials/Icon';
+
+export default function Flow( props ) {
+
+	const [ sidebar, setSidebar ] = useState( null );
+
+	const initSidebar = ( e, context ) => {
+		const {
+			_ref,
+			type,
+			entity,
+			callbacks,
+			entities,
+			toolbar,
+		} = context;
+
+		setSidebar(
+			<VStack className="p-3 w-100">
+				<HStack className="justify-content-between sticky-top bg-body">
+					<span className="h4">{ entity.name }</span>
+					<Icon className="icon-btn" icon="close" onClick={ () => setSidebar( null ) } />
+				</HStack>
+				{ toolbar.actions.config.config }
+			</VStack>
+		);
+	}
+
+	if ( sidebar ) {
+		return (
+			<div className="vh-100">
+				<div className="position-fixed top-0 start-0 bottom-0 end-0 overflow-auto bg-body" style={ { zIndex: 2000 } }>
+					<HStack className="align-items-start">
+						<div className="p-3 w-100 sticky-top">
+							<Entities { ...props } onClick={ initSidebar } />
+						</div>
+						{ sidebar }
+					</HStack>
+				</div>
+			</div>
+		);
+	}
+
+	return <Entities { ...props } onClick={ initSidebar } />;
+}
