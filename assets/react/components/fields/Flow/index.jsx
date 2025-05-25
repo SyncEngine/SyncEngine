d6ae0d946837b8830a540c0980a66c82ee105047
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2025-05-25 15:54:35 +0200

    Handle active step on selection

--- assets/react/components/fields/Flow/index.jsx
+++ assets/react/components/fields/Flow/index.jsx
@@ -9,6 +9,7 @@ export default function Flow( props ) {
 	const { t } = useTranslation();
 
 	const [ sidebar, setSidebar ] = useState( null );
+	const [ activeRef, setActiveRef ] = useState( null );
 	const context = useContext( ParentContext );
 
 	const container = context?.container?.closest('.modal-content');
@@ -28,6 +29,11 @@ export default function Flow( props ) {
 		}
 	}, [ sidebar, container ] );
 
+	const handleClose = () => {
+		setActiveRef( null );
+		setSidebar( null );
+	}
+
 	const initSidebar = ( e, context ) => {
 		const {
 			_ref,
@@ -38,11 +44,12 @@ export default function Flow( props ) {
 			toolbar,
 		} = context;
 
+		setActiveRef( _ref );
 		setSidebar(
 			<VStack className="w-100 overflow-auto">
 				<HStack className="justify-content-between sticky-top bg-body p-3 border border-1 border-input mb-2">
 					<span className="h4 m-0">{ entity.name }</span>
-					<Icon className="icon-btn" icon="close" onClick={ () => setSidebar( null ) } />
+					<Icon className="icon-btn" icon="close" onClick={ handleClose } />
 				</HStack>
 				{ toolbar.actions.config.fields }
 			</VStack>
@@ -61,12 +68,14 @@ export default function Flow( props ) {
 		return { edit: edit, ...actions };
 	}
 
+	const entitiesComponent = <Entities { ...props } onClick={ initSidebar } itemActions={ itemActions } activeKey={ activeRef } />;
+
 	if ( sidebar ) {
 		return (
 			<div className="position-fixed top-0 start-0 bottom-0 end-0 overflow-hidden bg-body-tertiary" style={ { zIndex: 2000 } }>
 				<HStack className="align-items-center h-100 p-3" gap={3}>
 					<VStack className="align-self-center w-100 mh-100 overflow-auto">
-						<Entities { ...props } onClick={ initSidebar } itemActions={ itemActions } />
+						{ entitiesComponent }
 					</VStack>
 					{ sidebar }
 				</HStack>
@@ -74,5 +83,5 @@ export default function Flow( props ) {
 		);
 	}
 
-	return <Entities { ...props } onClick={ initSidebar } itemActions={ itemActions } />;
+	return entitiesComponent;
 }
