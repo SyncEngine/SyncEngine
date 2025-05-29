067de1277531ad91068a0fd6e6a1b04eb8bf68a5
Jory <info@keraweb.nl>
2025-05-29 15:12:15 +0200

    First working version of a TaggableInput component using the Slate component
    Signed-off-by: Jory <info@keraweb.nl>

---
+++ assets/react/components/form/Input/TaggableInput.jsx
@@ -0,0 +1,178 @@
+import React, { useMemo, useState } from 'react';
+import { createEditor, Editor, Range, Text, Transforms } from 'slate';
+import { Editable, ReactEditor, Slate, withReact } from 'slate-react';
+import { withHistory } from 'slate-history';
+import Icon from '../../partials/Icon';
+import { isTag, splitByTags, trimTag } from '../../../utils/tags';
+
+// Add zero-width spaces around editable children to avoid Chrome cursor bugs
+const InlineChromiumBugfix = () => (
+	<span contentEditable={false} style={ { fontSize: 0 } }>
+		{String.fromCodePoint(160) /* Non-breaking space */}
+	</span>
+)
+
+// Renders a tag badge with an editable text child
+const TagElement = ({ attributes, children, element, editor }) => {
+	const openTagModal = () => {
+		// @todo.
+		const current = element.children[0]?.text || ''
+		const newValue = window.prompt('Edit tag:', current)
+		if (newValue != null) {
+			const path = ReactEditor.findPath(editor, element)
+			Transforms.setNodes(editor, { children: [{ text: newValue }] }, { at: [...path, 0] })
+		}
+	}
+
+	const removeTag = e => {
+		e.stopPropagation();
+		const path = ReactEditor.findPath(editor, element)
+		Transforms.removeNodes(editor, { at: path })
+	}
+
+	return (
+		<span {...attributes} onClick={e => e.preventDefault()} className="badge d-inline bg-info pointer">
+			<InlineChromiumBugfix />{children}<InlineChromiumBugfix/>
+			<Icon btn icon="clear" onClick={ removeTag } className="ms-2 me-n1 mt-n1 btn p-0 border-0" />
+        </span>
+	)
+}
+
+const Element = ({ attributes, children, element, editor }) => {
+	return element.type === 'tag'
+			? <TagElement attributes={attributes} children={children} element={element} editor={editor} />
+			: <span {...attributes}>{children}</span>
+}
+
+const Leaf = ({ attributes, children, leaf }) => <span style={ leaf.text === '' ? { paddingLeft: '0.1px' } : undefined } {...attributes}>{children}</span>
+
+// Plugin for tags: inline, auto-insert, normalize
+const withTags = editor => {
+	const { isInline, normalizeNode, insertText: origInsertText } = editor
+	editor.isInline = el => el.type === 'tag' || isInline(el)
+
+	// Trigger tag creating when typing "{{"
+	editor.insertText = text => {
+		const { selection } = editor;
+		origInsertText(text)
+		if (
+			text === '{' &&
+			selection && Range.isCollapsed(selection) &&
+			! Editor.above( editor, { match: n => n.type === 'tag' } )
+		) {
+			setTimeout(() => {
+				if (!editor.selection) return;
+				const { anchor } = editor.selection
+				const one = Editor.before(editor, anchor, { unit: 'character', distance: 1 })
+				const two = one && Editor.before(editor, one, { unit: 'character', distance: 1 })
+				if (two && Editor.string(editor, { anchor: two, focus: anchor }) === '{{') {
+					Editor.withoutNormalizing(editor, () => {
+						Transforms.delete(editor, { at: { anchor: two, focus: anchor } })
+						const tagNode = { type: 'tag', children: [{ text: ' ' }] }
+						Transforms.insertNodes(editor, tagNode, { select: true })
+						Transforms.collapse(editor, { edge: 'start' })
+					});
+					ReactEditor.focus(editor)
+				}
+			}, 0)
+		}
+	}
+
+	// Parse tags from strings.
+	editor.normalizeNode = entry => {
+		const [node, path] = entry;
+		if (Text.isText(node) && path.length === 1) {
+			const text = node.text;
+			const parts = splitByTags(text);
+			if (parts.length > 1) {
+				const nodes = partsToNodes(parts);
+				Editor.withoutNormalizing(editor, () => {
+					Transforms.removeNodes(editor, { at: path });
+					Transforms.insertNodes(editor, nodes, { at: path });
+				});
+				return; // skip further normalize on this entry
+			}
+		}
+		normalizeNode(entry);
+	};
+
+	return editor
+}
+
+function partsToNodes( parts ) {
+	const nodes = [];
+	if ( ! parts.length ) {
+		return nodes;
+	}
+	for ( const i in parts ) {
+		const content = parts[i]
+
+		if ( isTag( content ) ) {
+			// Tag node (strip outer `{{` and `}}`)
+			const raw = trimTag( content );
+			nodes.push({ type: 'tag', children: [{ text: raw || '' }] });
+		} else {
+			// Text node
+			nodes.push({ text: content || '' });
+		}
+	}
+	return nodes;
+}
+
+function parseValue( value ) {
+	const parts = splitByTags( value );
+
+	if ( ! parts.length ) {
+		return [ { type: 'paragraph', children: [{ text: value }] } ]
+	}
+
+	return partsToNodes( parts );
+}
+
+// Serialize Slate tree to raw string with tags
+const serializeNode = node => {
+	if (node.type === 'tag') {
+		const textChild = node.children[0] || { text: '' }
+		return `{{ ${textChild.text} }}`
+	}
+	if (Text.isText(node)) {
+		return node.text
+	}
+	if (node.children) {
+		return node.children.map(serializeNode).join('')
+	}
+	return ''
+}
+
+const serialize = nodes => nodes.map(serializeNode).join('')
+
+// Main component
+export default function TaggableInput( props ) {
+	const {
+		name,
+		onChange
+	} = props;
+
+	const editor = useMemo(() => withTags(withHistory(withReact(createEditor()))), []);
+	const [ value, setValue ] = useState( parseValue( props.value ) );
+
+	const handleChange = newValue => {
+		const string = serialize( newValue );
+		setValue( parseValue( string ) );
+		onChange && onChange( string );
+	}
+
+	return (
+		<div className="form-control">
+			<Slate editor={editor} initialValue={value} onChange={handleChange}>
+				<Editable
+					renderElement={ props => <Element { ...props } editor={ editor } /> }
+					renderLeaf={ Leaf }
+					style={ { outline: 'none' } }
+					placeholder={ props.placeholder }
+					autoFocus
+				/>
+			</Slate>
+		</div>
+	)
+}
