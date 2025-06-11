d2762aff3628e1aa0f126468f3ca64559ceacdec
Jory <info@keraweb.nl>
2025-06-11 11:56:30 +0200

    Use FieldCollection type
    Signed-off-by: Jory <info@keraweb.nl>

--- src/Model/Interface/Configurable.php
+++ src/Model/Interface/Configurable.php
@@ -2,9 +2,11 @@
 
 namespace SyncEngine\Model\Interface;
 
+use SyncEngine\Form\Fields\Collection\FieldCollection;
+
 interface Configurable
 {
-	public function getFields();
+	public function getFields(): FieldCollection|array;
 
 	public function getConfig( $key = null, $default = null );
 }
