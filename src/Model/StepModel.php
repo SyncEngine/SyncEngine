d7883e4f05afe89a202378e63e562e0b224dcef5
Jory <info@keraweb.nl>
2025-07-04 10:12:04 +0200

    StepModel to store flow step configs
    Signed-off-by: Jory <info@keraweb.nl>

---
+++ src/Model/StepModel.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace SyncEngine\Model;
+
+use SyncEngine\Model\Trait\Config;
+use SyncEngine\Model\Trait\Ref;
+use SyncEngine\Structure\Data\ConfigData;
+
+/**
+ * @internal
+ */
+final class StepModel
+{
+	use Ref;
+	use Config;
+
+	private RoutineModel $routine;
+
+	public function getRoutine(): RoutineModel
+	{
+		return $this->routine;
+	}
+
+	public function setRoutine( RoutineModel $routine ): StepModel
+	{
+		$this->routine = $routine;
+
+		return $this;
+	}
+
+	public function getRef(): string
+	{
+		// @todo Generate ref?
+		return $this->ref ?? '';
+	}
+
+	public function setRef( string $ref ): static
+	{
+		$this->ref = $ref;
+
+		return $this;
+	}
+
+	public function getInputConfig(): ConfigData
+	{
+		return ConfigData::create( $this->getConfig( 'input' ) );
+	}
+
+	public function getVariablesConfig(): ConfigData
+	{
+		return ConfigData::create( $this->getConfig( 'variables' ) );
+	}
+}
