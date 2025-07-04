37cf60372f19e8b202fabc7e25a9f352973d19d0
Jory <info@keraweb.nl>
2025-07-04 10:11:41 +0200

    SequenceData as a general sequence handler
    Signed-off-by: Jory <info@keraweb.nl>

---
+++ src/Structure/Data/SequenceData.php
@@ -0,0 +1,186 @@
+<?php
+
+namespace SyncEngine\Structure\Data;
+
+use Symfony\Component\PropertyAccess\PropertyAccess;
+use Traversable;
+
+/**
+ * @template T
+ */
+class SequenceData implements \ArrayAccess, \Countable, \IteratorAggregate, \SeekableIterator
+{
+	private int $current = 0;
+	private array $sequence = [];
+
+	/**
+	 * @var string[]
+	 */
+	private array $refs = [];
+	private $initCallback;
+
+	public function __construct( array $sequence = [], ?callable $initCallback = null )
+	{
+		foreach ( $sequence as $index => $step ) {
+			$this->add( $index, $step );
+		}
+
+		$this->initCallback = $initCallback;
+	}
+
+	public function add( int $index, array|object $step ): static
+	{
+		$accessor = PropertyAccess::createPropertyAccessor();
+
+		$ref = $accessor->getValue( $step, '_ref' ) ?: $accessor->getValue( $step, 'ref' ) ?: null;
+
+		$this->refs[ $index ]     = (string) ( $ref ?? $index );
+		$this->sequence[ $index ] = $step;
+
+		return $this;
+	}
+
+	public function remove( int|string $ref ): static
+	{
+		$index = $this->getIndex( $ref );
+
+		unset( $this->sequence[ $index ] );
+		unset( $this->refs[ $index ] );
+
+		return $this;
+	}
+
+	public function getIndex( int|string $ref ): ?int
+	{
+		if ( is_int( $ref ) ) {
+			return $ref;
+		}
+
+		return array_search( $ref, $this->refs, true );
+	}
+
+	/**
+	 * @param  int|string  $ref
+	 *
+	 * @return mixed
+	 */
+	public function get( int|string $ref ): mixed
+	{
+		return $this->sequence[ $this->getIndex( $ref ) ] ?? null;
+	}
+
+	public function getRefs(): array
+	{
+		return $this->refs;
+	}
+
+	public function getRef( int $offset ): ?string
+	{
+		return $this->refs[ $offset ] ?? null;
+	}
+
+	/**
+	 * @param  int|string  $ref
+	 *
+	 * @return T
+	 */
+	public function init( int|string $ref ): mixed
+	{
+		$index  = $this->getIndex( $ref );
+		$config = $this->get( $index );
+		$ref    = $this->getRef( $index );
+
+		$initialized = $this->initCallback ? call_user_func( $this->initCallback, $config, $ref ) : $config;
+
+		$this->sequence[ $index ] = $initialized;
+
+		return $initialized;
+	}
+
+	public function getIterator(): Traversable
+	{
+		foreach ( $this->sequence as $ref => $config ) {
+			yield $ref => $this->init( $config );
+		}
+	}
+
+	public function offsetExists( mixed $offset ): bool
+	{
+		if ( is_int( $offset ) ) {
+			return isset( $this->sequence[ $offset ] );
+		}
+
+		return in_array( $offset, $this->refs, true );
+	}
+
+	public function offsetGet( mixed $offset ): ?array
+	{
+		return $this->get( $offset );
+	}
+
+	public function offsetSet( mixed $offset, mixed $value ): void
+	{
+		$this->add( $offset, $value );
+	}
+
+	public function offsetUnset( mixed $offset ): void
+	{
+		$this->remove( $offset );
+	}
+
+	public function count(): int
+	{
+		return count( $this->sequence );
+	}
+
+	public function normalize(): array
+	{
+		return $this->sequence;
+	}
+
+	public function current(): mixed
+	{
+		return $this->sequence[ $this->current ];
+	}
+
+	public function previous(): void
+	{
+		--$this->current;
+	}
+
+	public function next(): void
+	{
+		++$this->current;
+	}
+
+	public function ref(): string
+	{
+		return $this->refs[ $this->current ];
+	}
+
+	public function key(): mixed
+	{
+		return $this->current;
+	}
+
+	public function valid(): bool
+	{
+		return $this->offsetExists( $this->current );
+	}
+
+	public function rewind(): void
+	{
+		$this->current = 0;
+	}
+
+	public function seek( int|string $offset ): void
+	{
+		$index = $this->getIndex( $offset );
+
+		if ( null === $index ) {
+			throw new \OutOfBoundsException("invalid seek position ($offset)");
+		}
+
+		$this->current = $index;
+	}
+}
