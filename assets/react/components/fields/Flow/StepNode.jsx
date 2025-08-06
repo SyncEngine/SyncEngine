52ee61eaecbe795132e97774d9c4b48bf989548d
Jory <info@keraweb.nl>
2025-08-06 15:46:12 +0200

    Tag context for Flow UI nodes
    Signed-off-by: Jory <info@keraweb.nl>

--- assets/react/components/fields/Flow/StepNode.jsx
+++ assets/react/components/fields/Flow/StepNode.jsx
@@ -1,18 +1,25 @@
-import React, { useCallback } from 'react';
-import Entity from '../Entity';
-import { Position } from '@xyflow/react';
+import React, { useCallback, useContext } from 'react';
+import { Position, useReactFlow } from '@xyflow/react';
 import LimitedHandle from './LimitedHandle';
+import Entity from '../Entity';
+import useEntities from '../../../hooks/useEntities';
+import { deepClone } from '../../../utils/data';
+import { TagsContext } from '../../../context/TagsContext';
 
 export default function StepNode( props ) {
-
 	const {
 		data = {},
 	} = props;
 
 	const {
-		entity: entity = '',
-		onChange
+		entity = '',
+		onChange,
 	} = data;
+
+	const { getNodes, getEdges } = useReactFlow();
+	// @todo find a way to fetch entity info more cleanly. Maybe centralize this somewhere? Or get callbacks from Entity field?
+	const [ entities, entityCallbacks, loading ] = useEntities( entity, [], {} );
+	const tagsContext = useContext( TagsContext );
 
 	const handleChange = useCallback( newValue => {
 		const entityId = newValue.id;
@@ -25,16 +32,66 @@ export default function StepNode( props ) {
 		actions.push( 'create' );
 	}
 
-	console.log( data );
+	const stepsContext = {};
+	if ( ! loading ) {
+
+		const nodes = getNodes();
+		const edges = getEdges();
+		const nodeMap = Object.fromEntries( nodes.map( n => [ n.id, n ] ) );
+
+		const previousNodes = collectPreviousNodes( data._ref, nodeMap, edges );
+
+		for ( let i = 0; i < previousNodes.length; i ++ ) {
+			let row = previousNodes[ i ];
+			stepsContext[ row.data._ref ] = entityCallbacks.get( row.data[ entity ] )?._step?.tags || {};
+		}
+	}
+
+	const tags = {
+		step: deepClone( stepsContext ),
+		...deepClone( tagsContext ),
+	};
 
 	return (
 		<>
-			<LimitedHandle limit={1} type="target" position={ Position.Top } />
+			<LimitedHandle limit={ 1 } type="target" position={ Position.Top }/>
 			<div className="p-2 bg-body border border-1 border-input" onClick={ e => e.stopPropagation() }>
 				<h5 className="mb-0">Step: { data._ref }</h5>
-				<Entity className="nodrag" entity={ entity } value={ { id: data[ entity ], ...( data.config ?? {} ) } } onChange={ handleChange } config={ 'entity:_step.fields' } actions={ actions } />
+				<TagsContext.Provider value={ tags }>
+					<Entity
+						className="nodrag"
+						entity={ entity }
+						value={ { id: data[ entity ], ...( data.config ?? {} ), } }
+						onChange={ handleChange }
+						config={ 'entity:_step.fields' }
+						actions={ actions }
+					/>
+				</TagsContext.Provider>
 			</div>
-			<LimitedHandle limit={1} type="source" position={ Position.Bottom } />
+			<LimitedHandle limit={ 1 } type="source" position={ Position.Bottom }/>
 		</>
 	)
+}
+
+
+// Recursively collect previous nodes in order
+function collectPreviousNodes( nodeId, nodeMap, edges, visited = new Set() ) {
+	if ( visited.has( nodeId ) ) {
+		return [];
+	}
+	visited.add( nodeId );
+
+	const prevEdges = edges.filter( e => e.target === nodeId );
+	let result = [];
+	for ( const edge of prevEdges ) {
+		const prevNode = nodeMap[ edge.source ];
+		if ( prevNode ) {
+			result = [
+				...collectPreviousNodes( prevNode.id, nodeMap, edges, visited ),
+				prevNode,
+				...result,
+			];
+		}
+	}
+	return result;
 }
