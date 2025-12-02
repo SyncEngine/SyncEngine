17ee4d6c3f7edcb9a991ef41d6cb86088dfb068b
JoryHogeveen <info@keraweb.nl>
2025-12-02 11:07:54 +0100

    Missing import

--- src/Controller/ExecuteController.php
+++ src/Controller/ExecuteController.php
@@ -2,6 +2,7 @@
 
 namespace SyncEngine\Controller;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
