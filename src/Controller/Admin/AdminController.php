3682590c3a383b2d1c7d1257f4dab42779691cfa
Inform-All <NKoonen@users.noreply.github.com>
2025-12-02 15:59:42 +0100

    Use repository instead of direct queries

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -2,24 +2,19 @@
 
 namespace SyncEngine\Controller\Admin;
 
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use SyncEngine\Attribute\MenuItem;
 use SyncEngine\Controller\DefaultController;
 use SyncEngine\Model\AutomationModel;
 use SyncEngine\Model\TraceModel;
+use SyncEngine\Repository\MessengerMessageRepository;
 
 class AdminController extends DefaultController
 {
 	#[Route( '/', name: 'admin_index' )]
 	#[MenuItem( menu: 'main', route: 'syncengine_admin_index', label: 'Dashboard', parent: 'dashboards', icon: 'dashboard', position: 0 )]
-	public function dashboard(
-		#[Autowire( '%env(string:DB_TABLE_PREFIX)%' )]
-		$prefix,
-		EntityManagerInterface $em,
-	): Response
+	public function dashboard( MessengerMessageRepository $messageRepository): Response
 	{
 		$query = [
 			'limit'        => 10,
@@ -28,10 +23,9 @@ class AdminController extends DefaultController
 			'order'        => [ 'created' => 'DESC' ],
 		];
 
-		$conn = $em->getConnection();
-		$sql  = 'SELECT * FROM ' . $prefix . 'messenger_messages ORDER BY created_at DESC LIMIT 10';
+		$messengerItems = $messageRepository->findAll();
+		$totalMessenger = count($messengerItems);
 
-		$messengerItems = $conn->executeQuery($sql)->fetchAllAssociative();
 		foreach ( $messengerItems as &$item ) {
 			$decoded = json_decode( $item['body'], true );
 			$item['body'] = $decoded ?: substr( $item['body'], 0, 200 ) . '...';
@@ -39,8 +33,6 @@ class AdminController extends DefaultController
 				$item['automation'] = AutomationModel::get( $item['body']['automationId'] )?->normalize( false, false );
 			}
 		}
-
-		$totalMessenger = $conn->executeQuery( 'SELECT COUNT(*) FROM ' . $prefix . 'messenger_messages' )->fetchOne();
 
 		return $this->render(
 			'admin/dashboard.html.twig',
