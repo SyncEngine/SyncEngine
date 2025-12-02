72c2987caf5d4b5111c97bf55c17e804c3cd812f
JoryHogeveen <info@keraweb.nl>
2025-12-02 11:53:43 +0100

    Fix route param names, mapping and types

--- src/Controller/ExecuteController.php
+++ src/Controller/ExecuteController.php
@@ -13,7 +13,7 @@ use SyncEngine\Service\System;
 class ExecuteController extends DefaultController
 {
 	// @todo Allow in dev only?
-	#[Route( '/execute/endpoint/{endpoint:endpoint}/{action:action}', name: 'endpoint_execute', defaults: [ 'action' => 'execute' ] )]
+	#[Route( '/execute/endpoint/{endpoint}/{action}', name: 'endpoint_execute', defaults: [ 'action' => 'execute' ] )]
 	public function endpoint_profiler( string $endpoint, string $action, ApiEndpointController $endpointController, Execute $execute, ?Request $request = null ): Response
 	{
 		$response = $endpointController->endpoint( $endpoint, $action, $execute, $request );
