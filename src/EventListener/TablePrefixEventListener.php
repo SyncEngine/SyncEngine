aa2a4361db2c25eb0d7e1d1e68acc6ca74163e3f
JoryHogeveen <info@keraweb.nl>
2026-01-08 17:20:58 +0100

    Codestyle

--- src/EventListener/TablePrefixEventListener.php
+++ src/EventListener/TablePrefixEventListener.php
@@ -6,51 +6,49 @@ use Doctrine\Bundle\DoctrineBundle\Attribute\AsDoctrineListener;
 use Doctrine\ORM\Event\LoadClassMetadataEventArgs;
 use Doctrine\ORM\Mapping\ClassMetadataInfo;
 
-#[AsDoctrineListener(event: 'loadClassMetadata')]
+#[AsDoctrineListener( event: 'loadClassMetadata' )]
 class TablePrefixEventListener
 {
 	public function __construct(
 		private readonly string $prefix
 	) {}
 
-	public function loadClassMetadata(LoadClassMetadataEventArgs $eventArgs): void
+	public function loadClassMetadata( LoadClassMetadataEventArgs $eventArgs ): void
 	{
 		$classMetadata = $eventArgs->getClassMetadata();
 
 		if (
-			!$classMetadata->isInheritanceTypeSingleTable() ||
-			$classMetadata->getName() === $classMetadata->rootEntityName
+			! $classMetadata->isInheritanceTypeSingleTable()
+			|| $classMetadata->getName() === $classMetadata->rootEntityName
 		) {
-			$this->prefixTable($classMetadata);
+			$this->prefixTable( $classMetadata );
 		}
-		
-		foreach ($classMetadata->getAssociationMappings() as $fieldName => $mapping) {
+
+		foreach ( $classMetadata->getAssociationMappings() as $fieldName => $mapping ) {
 			if (
-				$mapping['type'] === ClassMetadataInfo::MANY_TO_MANY &&
-				$mapping['isOwningSide']
+				$mapping['type'] === ClassMetadataInfo::MANY_TO_MANY
+				&& $mapping['isOwningSide']
 			) {
-				$this->prefixJoinTable($classMetadata, $fieldName, $mapping);
+				$this->prefixJoinTable( $classMetadata, $fieldName, $mapping );
 			}
 		}
 	}
 
-	private function prefixTable($classMetadata): void
+	private function prefixTable( $classMetadata ): void
 	{
 		$tableName = $classMetadata->getTableName();
 
-		if (!str_starts_with($tableName, $this->prefix)) {
-			$classMetadata->setPrimaryTable([
-				                                'name' => $this->prefix . $tableName,
-			                                ]);
+		if ( ! str_starts_with( $tableName, $this->prefix ) ) {
+			$classMetadata->setPrimaryTable( [ 'name' => $this->prefix . $tableName ] );
 		}
 	}
 
-	private function prefixJoinTable($classMetadata, string $fieldName, array $mapping): void
+	private function prefixJoinTable( $classMetadata, string $fieldName, array $mapping ): void
 	{
 		$tableName = $mapping['joinTable']['name'];
 
-		if (!str_starts_with($tableName, $this->prefix)) {
-			$classMetadata->associationMappings[$fieldName]['joinTable']['name'] =
+		if ( ! str_starts_with( $tableName, $this->prefix ) ) {
+			$classMetadata->associationMappings[ $fieldName ]['joinTable']['name'] =
 				$this->prefix . $tableName;
 		}
 	}
