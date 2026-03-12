8d23be8291ef1ac4606ea5fdf3991e6c595ea6c3
JoryHogeveen <info@keraweb.nl>
2026-03-12 14:40:26 +0100

    Add baseTimestamp parser

--- src/Service/Generator/Timestamp.php
+++ src/Service/Generator/Timestamp.php
@@ -6,8 +6,18 @@ use SyncEngine\Service\Interface\GeneratorInterface;
 
 class Timestamp implements GeneratorInterface
 {
-	public function generate( string $from = '', ?int $baseTimestamp = null ): string
+	public function generate( string $from = '', null|int|string $baseTimestamp = null ): string
 	{
+		if ( is_string( $baseTimestamp ) ) {
+			if ( empty( $baseTimestamp ) ) {
+				$baseTimestamp = null;
+			} else if ( is_numeric( $baseTimestamp ) ) {
+				$baseTimestamp = (int) $baseTimestamp;
+			} else {
+				$baseTimestamp = strtotime( $baseTimestamp );
+			}
+		}
+
 		if ( $from ) {
 			return strtotime( $from, $baseTimestamp );
 		}
