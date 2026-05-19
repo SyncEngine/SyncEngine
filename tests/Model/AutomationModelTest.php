53fc6a314d7f5181d6c2b7d1316a0eb15159ed05
JoryHogeveen <info@keraweb.nl>
2026-05-19 17:13:11 +0200

    Add more tests for the iteration mechanism

--- tests/Model/AutomationModelTest.php
+++ tests/Model/AutomationModelTest.php
@@ -5,6 +5,8 @@ namespace SyncEngine\Tests\Model;
 
 use SyncEngine\Model\AutomationModel;
 use SyncEngine\Model\TraceModel;
+use SyncEngine\Service\Execute;
+use SyncEngine\Service\ExecuteContext;
 use SyncEngine\Tests\TestCase\BaseTestCase;
 
 class AutomationModelTest extends BaseTestCase
@@ -98,5 +100,70 @@ class AutomationModelTest extends BaseTestCase
 		];
 
 		$this->assertEquals( $compare, (array) $model->getIteration( $trace ) );
+	}
+
+	public function testIteratorEnabledWithoutTraceReturnsDefaults(): void
+	{
+		/** @var AutomationModel $model */
+		$model = AutomationModel::create();
+		$model->enableIterator();
+		$model->setLimit( 25 );
+
+		$compare = [
+			'current' => 0,
+			'index'   => -1,
+			'limit'   => 25,
+			'offset'  => 0,
+			'size'    => null,
+			'isFirst' => false,
+			'isLast'  => null,
+		];
+
+		$this->assertEquals( $compare, (array) $model->getIteration() );
+	}
+
+	public function testTagsResourceUsesTraceIterationWhenContextHasTrace(): void
+	{
+		/** @var AutomationModel $model */
+		$model = AutomationModel::create();
+		$model->enableIterator();
+		$model->setLimit( 50 );
+
+		$trace = TraceModel::create();
+		$trace->setCurrentIteration( 3 );
+
+		/** @var Execute $execute */
+		$execute = static::getContainer()->get( Execute::class );
+		$context = new ExecuteContext( $execute, $model );
+		$context->registerTrace( $trace );
+
+		$iteration = $model->getTagsResource( [], $context )['iteration'];
+
+		$this->assertSame( 3, $iteration['current'] );
+		$this->assertSame( 2, $iteration['index'] );
+		$this->assertSame( 100, $iteration['offset'] );
+		$this->assertSame( 50, $iteration['limit'] );
+	}
+
+	public function testContextTagsExposeIteratorAndIterationAlias(): void
+	{
+		/** @var AutomationModel $model */
+		$model = AutomationModel::create();
+		$model->enableIterator();
+		$model->setLimit( 20 );
+
+		$trace = TraceModel::create();
+		$trace->setCurrentIteration( 2 );
+
+		/** @var Execute $execute */
+		$execute = static::getContainer()->get( Execute::class );
+		$context = new ExecuteContext( $execute, $model );
+		$context->registerTrace( $trace );
+
+		$tags = $context->getTagsResource();
+
+		$this->assertArrayHasKey( 'iteration', $tags );
+		$this->assertSame( 2, $tags['iteration']['current'] );
+		$this->assertSame( 20, $tags['iteration']['limit'] );
 	}
 }
