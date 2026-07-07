80168258e3bacfb097764842b780867c981a7770
JoryHogeveen <info@keraweb.nl>
2026-07-07 16:10:13 +0200

    Auto create grouped fields and nested groups

--- src/Form/Fields/FieldTypeFactory.php
+++ src/Form/Fields/FieldTypeFactory.php
@@ -15,7 +15,17 @@ class FieldTypeFactory
 		if ( ! isset( $config['type'] ) ) {
 			if ( isset( $config['tabs'] ) ) {
 				$config['type'] = 'tabs';
+			} elseif ( isset( $config['steps'] ) || isset( $config['wizard'] ) ) {
+				$config['type'] = 'wizard';
 			}
+		}
+
+		if ( isset( $config['nested'] ) ) {
+			return new NestedGroupFieldType( $config );
+		}
+
+		if ( isset( $config['fields'] ) ) {
+			return new GroupFieldType( $config );
 		}
 
 		return match ( $config['type'] ?? '' ) {
