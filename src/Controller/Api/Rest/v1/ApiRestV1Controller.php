b426158991ac9d447e6c92f34ca1749c2db6e691
JoryHogeveen <info@keraweb.nl>
2026-07-13 14:27:15 +0200

    Validate entity types

--- src/Controller/Api/Rest/v1/ApiRestV1Controller.php
+++ src/Controller/Api/Rest/v1/ApiRestV1Controller.php
@@ -7,17 +7,29 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use SyncEngine\Controller\Api\ApiController;
-use SyncEngine\Model\Abstract\EntityModel;
+use SyncEngine\Model\Abstract\EngineModel;
+use SyncEngine\Model\AutomationModel;
+use SyncEngine\Model\ConnectionModel;
+use SyncEngine\Model\FlowModel;
+use SyncEngine\Model\RoutineModel;
+use SyncEngine\Model\StorageModel;
 
-#[Route('/rest/v1', name: 'rest_v1_')]
+#[Route( '/rest/v1', name: 'rest_v1_' )]
 class ApiRestV1Controller extends ApiController
 {
+	private const ALLOWED_MODELS = [
+		'automation' => AutomationModel::class,
+		'connection' => ConnectionModel::class,
+		'flow'       => FlowModel::class,
+		'routine'    => RoutineModel::class,
+		'storage'    => StorageModel::class,
+	];
 
 	#[Route( '/{entity:entity}', name: 'list_entities', methods: [ 'GET' ] )]
 	public function list_entities( Request $request, string $entity ): JsonResponse
 	{
-		/** @var EntityModel $model */
-		$model = EntityModel::getEntityModelClass( $entity );
+		/** @var class-string<EngineModel> $model */
+		$model = $this->resolveModel( $entity );
 
 		if ( ! $model ) {
 			return $this->json( [ 'message' => $this->trans( 'Entity not found' ) ], Response::HTTP_NOT_IMPLEMENTED );
@@ -36,38 +48,45 @@ class ApiRestV1Controller extends ApiController
 				$entity = $entity->normalize();
 			}
 		} catch ( \Exception $e ) {
-			return $this->json( [ 'message' => $this->trans( $e->getMessage() ) ], Response::HTTP_INTERNAL_SERVER_ERROR );
+			return $this->json(
+				[ 'message' => $this->trans( $e->getMessage() ) ],
+				Response::HTTP_INTERNAL_SERVER_ERROR
+			);
 		}
 
 		return $this->json( $list );
 	}
 
-
 	#[Route( '/{entity:entity}/{id:id}', name: 'get_entity', requirements: [ 'id' => '\d+' ], methods: [ 'GET' ] )]
 	public function get_entity( Request $request, string $entity, int $id = 0 ): JsonResponse
 	{
-		/** @var EntityModel $model */
-		$model = EntityModel::getEntityModelClass( $entity );
+		/** @var class-string<EngineModel> $model */
+		$model = $this->resolveModel( $entity );
 
 		if ( ! $model ) {
-			return $this->json( [ 'message' => $this->trans( 'Entity not found' ) ], Response::HTTP_NOT_IMPLEMENTED );
+			return $this->json(
+				[ 'message' => $this->trans( 'Entity type not found' ) ],
+				Response::HTTP_NOT_IMPLEMENTED
+			);
 		}
 
 		try {
 			$fetch = $model::get( $id )->normalize();
 		} catch ( \Exception $e ) {
-			return $this->json( [ 'message' => $this->trans( $e->getMessage() ) ], Response::HTTP_INTERNAL_SERVER_ERROR );
+			return $this->json(
+				[ 'message' => $this->trans( $e->getMessage() ) ],
+				Response::HTTP_INTERNAL_SERVER_ERROR
+			);
 		}
 
 		return $this->json( $fetch );
 	}
 
-
 	#[Route( '/{entity:entity}', name: 'create_entity', methods: [ 'POST' ] )]
 	public function create_entity( Request $request, string $entity ): JsonResponse
 	{
-		/** @var EntityModel $model */
-		$model = EntityModel::getEntityModelClass( $entity );
+		/** @var class-string<EngineModel> $model */
+		$model = $this->resolveModel( $entity );
 
 		if ( ! $model ) {
 			return $this->json( [ 'message' => $this->trans( 'Entity not found' ) ], Response::HTTP_NOT_IMPLEMENTED );
@@ -93,26 +112,37 @@ class ApiRestV1Controller extends ApiController
 			}
 
 			if ( $unknown ) {
-				return $this->json( [ 'message' => 'Unknown fields: ' . implode( ', ', $unknown ) ], Response::HTTP_BAD_REQUEST );
+				return $this->json(
+					[ 'message' => 'Unknown fields: ' . implode( ', ', $unknown ) ],
+					Response::HTTP_BAD_REQUEST
+				);
 			}
 
 			$modelInstance->save( true );
 
 			return $this->json( $modelInstance->normalize(), Response::HTTP_CREATED );
 		} catch ( \Exception $e ) {
-			return $this->json( [ 'message' => $this->trans( $e->getMessage() ) ], Response::HTTP_INTERNAL_SERVER_ERROR );
+			return $this->json(
+				[ 'message' => $this->trans( $e->getMessage() ) ],
+				Response::HTTP_INTERNAL_SERVER_ERROR
+			);
 		}
 	}
 
-
-	#[Route( '/{entity:entity}/{id:id}', name: 'update_entity', requirements: [ 'id' => '\d+' ], methods: [ 'PUT', 'PATCH' ] )]
+	#[Route( '/{entity:entity}/{id:id}', name: 'update_entity', requirements: [ 'id' => '\d+' ], methods: [
+		'PUT',
+		'PATCH',
+	] )]
 	public function update_entity( Request $request, string $entity, int $id ): JsonResponse
 	{
-		/** @var EntityModel $model */
-		$model = EntityModel::getEntityModelClass( $entity );
+		/** @var class-string<EngineModel> $model */
+		$model = $this->resolveModel( $entity );
 
 		if ( ! $model ) {
-			return $this->json( [ 'message' => $this->trans( 'Entity type not found' ) ], Response::HTTP_NOT_IMPLEMENTED );
+			return $this->json(
+				[ 'message' => $this->trans( 'Entity type not found' ) ],
+				Response::HTTP_NOT_IMPLEMENTED
+			);
 		}
 
 		$modelInstance = $model::get( $id );
@@ -139,23 +169,28 @@ class ApiRestV1Controller extends ApiController
 			}
 
 			if ( $unknown ) {
-				return $this->json( [ 'message' => 'Unknown fields: ' . implode( ', ', $unknown ) ], Response::HTTP_BAD_REQUEST );
+				return $this->json(
+					[ 'message' => 'Unknown fields: ' . implode( ', ', $unknown ) ],
+					Response::HTTP_BAD_REQUEST
+				);
 			}
 
 			$modelInstance->update( true );
 
 			return $this->json( $modelInstance->normalize() );
 		} catch ( \Exception $e ) {
-			return $this->json( [ 'message' => $this->trans( $e->getMessage() ) ], Response::HTTP_INTERNAL_SERVER_ERROR );
+			return $this->json(
+				[ 'message' => $this->trans( $e->getMessage() ) ],
+				Response::HTTP_INTERNAL_SERVER_ERROR
+			);
 		}
 	}
-
 
 	#[Route( '/{entity:entity}/{id:id}', name: 'delete_entity', requirements: [ 'id' => '\d+' ], methods: [ 'DELETE' ] )]
 	public function delete_entity( Request $request, string $entity, int $id ): JsonResponse
 	{
-		/** @var EntityModel $model */
-		$model = EntityModel::getEntityModelClass( $entity );
+		/** @var class-string<EngineModel> $model */
+		$model = $this->resolveModel( $entity );
 
 		if ( ! $model ) {
 			return $this->json( [ 'message' => $this->trans( 'Entity not found' ) ], Response::HTTP_NOT_IMPLEMENTED );
@@ -169,9 +204,28 @@ class ApiRestV1Controller extends ApiController
 
 		try {
 			$modelInstance->delete( true );
+
 			return $this->json( [ 'message' => $this->trans( 'Entity deleted' ) ], Response::HTTP_OK );
 		} catch ( \Exception $e ) {
-			return $this->json( [ 'message' => $this->trans( $e->getMessage() ) ], Response::HTTP_INTERNAL_SERVER_ERROR );
+			return $this->json(
+				[ 'message' => $this->trans( $e->getMessage() ) ],
+				Response::HTTP_INTERNAL_SERVER_ERROR
+			);
 		}
+	}
+
+	private function resolveModel( string $entity ): ?string
+	{
+		if ( ! isset( self::ALLOWED_MODELS[ $entity ] ) ) {
+			return null;
+		}
+
+		$modelClass = self::ALLOWED_MODELS[ $entity ];
+
+		if ( ! is_subclass_of( $modelClass, EngineModel::class ) ) {
+			return null;
+		}
+
+		return $modelClass;
 	}
 }
