7ce4803fc50012b3b22077ab16bad02d84ba5af0
JoryHogeveen <info@keraweb.nl>
2026-07-22 20:44:24 +0200

    Forgot to commit ApiControllerInterface

---
+++ src/Controller/Interface/ApiControllerInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace SyncEngine\Controller\Interface;
+
+/**
+ * Modules can use this interface to prefix all routes with /api
+ * Prefixing with /api will automatically apply Token authorization to these routes.
+ */
+interface ApiControllerInterface
+{
+
+}
