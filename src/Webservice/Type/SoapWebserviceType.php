f41c1b396c09c8c59ee843253ae544f4b385c0bc
Jory Hogeveen <JoryHogeveen@users.noreply.github.com>
2026-07-23 17:13:28 +0200

    Prepare cache constants and add note about possible SoapClient trait

--- src/Webservice/Type/SoapWebserviceType.php
+++ src/Webservice/Type/SoapWebserviceType.php
@@ -8,6 +8,8 @@ class SoapWebserviceType extends GenericWebserviceType
 	const NAME        = 'SOAP';
 	const DESCRIPTION = 'SOAP webservice';
 
+	// @todo Move below constants to separate SoapClient trait?
+
 	// SOAP version constants for config use.
 	const SOAP_1_1 = '1.1';
 	const SOAP_1_2 = '1.2';
@@ -17,4 +19,11 @@ class SoapWebserviceType extends GenericWebserviceType
 	const COMPRESSION_GZIP   = 'gzip';
 	const COMPRESSION_DEFLATE = 'deflate';
 	const COMPRESSION_BOTH   = 'both';
+
+	// WSDL cache constants for config use.
+	// These mirror PHP's native WSDL_CACHE_* constants.
+	const CACHE_NONE    = 'none';
+	const CACHE_MEMORY  = 'memory';
+	const CACHE_FILE    = 'file';
+	const CACHE_BOTH    = 'both';
 }
