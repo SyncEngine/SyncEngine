c7fb59c45ca7d4288de72520f7ec6534a98c74da
Jory Hogeveen <JoryHogeveen@users.noreply.github.com>
2026-07-23 17:11:51 +0200

    Add config constants for version and compression (forgot commit)

--- src/Webservice/Type/SoapWebserviceType.php
+++ src/Webservice/Type/SoapWebserviceType.php
@@ -7,4 +7,14 @@ class SoapWebserviceType extends GenericWebserviceType
 	const TYPE        = 'soap';
 	const NAME        = 'SOAP';
 	const DESCRIPTION = 'SOAP webservice';
+
+	// SOAP version constants for config use.
+	const SOAP_1_1 = '1.1';
+	const SOAP_1_2 = '1.2';
+
+	// Compression constants for config use.
+	const COMPRESSION_NONE   = 'none';
+	const COMPRESSION_GZIP   = 'gzip';
+	const COMPRESSION_DEFLATE = 'deflate';
+	const COMPRESSION_BOTH   = 'both';
 }
