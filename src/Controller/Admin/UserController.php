192c37ec63b987885796eb0c7ddf3d7bb23a3242
Jory Hogeveen <JoryHogeveen@users.noreply.github.com>
2026-07-29 12:22:17 +0200

    Create basic user management for admins

---
+++ src/Controller/Admin/UserController.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace SyncEngine\Controller\Admin;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use SyncEngine\Attribute\MenuItem;
+use SyncEngine\Entity\User;
+use SyncEngine\Form\UserFormType;
+use SyncEngine\Repository\UserRepository;
+
+#[IsGranted( 'ROLE_ADMIN' )]
+class UserController extends AdminController
+{
+	#[Route( '/users', name: 'users_list' )]
+	#[MenuItem( menu: 'main', route: 'syncengine_users_list', label: 'Users', icon: 'system-users' )]
+	public function renderList( Request $request, UserRepository $userRepository ): Response
+	{
+		$users = $userRepository->findAll();
+
+		return $this->render(
+			'admin/user/index.html.twig',
+			[
+				'users'       => $users,
+				'breadcrumbs' => [ [ 'title' => $this->trans( 'Users' ), 'current' => true ] ],
+			]
+		);
+	}
+
+	#[Route( '/user/create', name: 'user_create' )]
+	public function renderCreate(
+		Request $request,
+		UserPasswordHasherInterface $passwordHasher,
+		EntityManagerInterface $entityManager,
+	): Response {
+		$user = new User();
+		$form = $this->createForm( UserFormType::class, $user );
+		$form->add( 'save', SubmitType::class, [ 'label' => $this->trans( 'Create' ) ] );
+		$form->handleRequest( $request );
+
+		if ( $form->isSubmitted() && $form->isValid() ) {
+			$newPassword = $form->get( 'plainPassword' )->getData();
+			if ( $newPassword ) {
+				$user->setPassword( $passwordHasher->hashPassword( $user, $newPassword ) );
+			}
+
+			$entityManager->persist( $user );
+			$entityManager->flush();
+
+			$this->addFlash( 'success', $this->trans( 'Successfully created user!' ) );
+
+			return $this->redirectToRoute( 'syncengine_users_list' );
+		}
+
+		return $this->render(
+			'admin/user/create.html.twig',
+			[
+				'form'        => $form,
+				'backlink'    => true,
+				'breadcrumbs' => [
+					[ 'link' => $this->generateUrl( 'syncengine_users_list' ), 'title' => $this->trans( 'Users' ) ],
+					[ 'title' => $this->trans( 'Create' ), 'current' => true ],
+				],
+			]
+		);
+	}
+
+	#[Route( '/user/edit/{id}', name: 'user_edit' )]
+	public function renderEdit(
+		#[MapEntity( id: 'id' )]
+		User $user, Request $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager
+	): Response {
+		$form = $this->createForm( UserFormType::class, $user );
+		$form->add( 'save', SubmitType::class, [ 'label' => $this->trans( 'Update' ) ] );
+		$form->handleRequest( $request );
+
+		if ( $form->isSubmitted() && $form->isValid() ) {
+			$newPassword = $form->get( 'plainPassword' )->getData();
+			if ( $newPassword ) {
+				$user->setPassword( $passwordHasher->hashPassword( $user, $newPassword ) );
+			}
+
+			$entityManager->persist( $user );
+			$entityManager->flush();
+
+			$this->addFlash( 'success', $this->trans( 'Successfully edited user!' ) );
+
+			return $this->redirectToRoute( 'syncengine_user_edit', [ 'id' => $user->getId() ] );
+		}
+
+		return $this->render(
+			'admin/user/edit.html.twig',
+			[
+				'form'        => $form,
+				'user'        => $user,
+				'backlink'    => true,
+				'breadcrumbs' => [
+					[ 'link' => $this->generateUrl( 'syncengine_users_list' ), 'title' => $this->trans( 'Users' ) ],
+					[ 'title' => $this->trans( 'Edit' ), 'current' => true ],
+				],
+			]
+		);
+	}
+
+	#[Route( '/user/delete/{id}', name: 'user_delete', methods: [ 'POST' ] )]
+	public function deleteAction(
+		#[MapEntity( id: 'id' )]
+		User $user, Request $request, EntityManagerInterface $entityManager
+	): RedirectResponse {
+		$currentUser = $this->getUser();
+		if ( $currentUser instanceof User && $currentUser->getId() === $user->getId() ) {
+			$this->addFlash( 'warning', $this->trans( 'You cannot delete your own account.' ) );
+
+			return $this->redirectToRoute( 'syncengine_users_list' );
+		}
+
+		if ( $this->isCsrfTokenValid( 'delete_' . $user->getId(), $request->request->get( '_token' ) ) ) {
+			$entityManager->remove( $user );
+			$entityManager->flush();
+
+			$this->addFlash( 'success', $this->trans( 'Successfully deleted user!' ) );
+		}
+
+		return $this->redirectToRoute( 'syncengine_users_list' );
+	}
+}
---
+++ src/Form/UserFormType.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace SyncEngine\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use SyncEngine\Entity\User;
+
+/**
+ * User editing form type, meant for Admins editing other users.
+ */
+class UserFormType extends AbstractType
+{
+	public function buildForm( FormBuilderInterface $builder, array $options ): void
+	{
+		$builder
+			->add('email', TextType::class, [
+				'row_attr' => [
+					'class' => 'form-floating mb-3',
+				],
+			])
+			->add('name', TextType::class, [
+				'row_attr' => [
+					'class' => 'form-floating mb-3',
+				],
+			])
+			->add('plainPassword', PasswordType::class, [
+				'row_attr' => [
+					'class' => 'form-floating mb-3',
+				],
+				// instead of being set onto the object directly,
+				// this is read and encoded in the controller
+				'mapped' => false,
+				'attr' => ['autocomplete' => 'new-password'],
+				'constraints' => [
+					new NotBlank([
+						'message' => 'Please enter a password',
+					]),
+					new Length([
+						'min' => 6,
+						'minMessage' => 'Your password should be at least { limit } characters',
+						// max length allowed by Symfony for security reasons
+						'max' => 4096,
+					]),
+				],
+			])
+			->add( 'roles', ChoiceType::class, [
+				'choices'  => [
+					'Admin' => 'ROLE_ADMIN',
+				],
+				'multiple' => true,
+				'expanded' => true,
+				'label'  => 'Roles',
+			] );
+	}
+
+	public function configureOptions( OptionsResolver $resolver ): void
+	{
+		$resolver->setDefaults( [
+			'data_class' => User::class,
+		] );
+	}
+}
