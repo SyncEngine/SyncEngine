2304593a141d6249a6c854399638b43bb96a8f7a
Jory Hogeveen <JoryHogeveen@users.noreply.github.com>
2026-07-30 15:48:34 +0200

    Move IP validation to a dedicated request event listener

---
+++ src/Security/TrustedIpListener.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace SyncEngine\Security;
+
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\HttpFoundation\IpUtils;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+
+#[AsEventListener]
+class TrustedIpListener
+{
+	public function __construct(
+		#[Autowire( '%syncengine.security.trusted_ips%' )]
+		private string $trustedIps,
+	) {}
+
+	public function __invoke( RequestEvent $event ): void
+	{
+		if ( empty( $this->trustedIps ) ) {
+			// No IP restrictions.
+			return;
+		}
+
+		$request = $event->getRequest();
+
+		$ips = array_map( 'trim', explode( ',', $this->trustedIps ) );
+
+		if ( ! IpUtils::checkIp( $request->getClientIp(), $ips ) ) {
+			throw new AccessDeniedHttpException();
+		}
+	}
+}
