44eab2fec2349d7fedb1abb80d40b242560c629f
Jory Hogeveen <JoryHogeveen@users.noreply.github.com>
2026-08-05 00:14:31 +0200

    Password is not required for account form

--- src/Form/AccountFormType.php
+++ src/Form/AccountFormType.php
@@ -34,6 +34,7 @@ class AccountFormType extends AbstractType
 				// instead of being set onto the object directly,
 				// this is read and encoded in the controller
 				'mapped' => false,
+				'required' => false,
 				'attr' => ['autocomplete' => 'new-password'],
 				'constraints' => [
 					new Length([
