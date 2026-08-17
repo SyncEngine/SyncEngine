b28064d62bc55a77ba3a3abd25b7baed6291e3d4
Jory Hogeveen <JoryHogeveen@users.noreply.github.com>
2026-08-17 19:29:44 +0200

    Add delete_permanent flag in the API delete params

--- src/Controller/Api/Rest/v1/ApiRestV1Controller.php
+++ src/Controller/Api/Rest/v1/ApiRestV1Controller.php
@@ -246,7 +246,12 @@ class ApiRestV1Controller extends AbstractApiController
 		}
 
 		try {
-			$modelInstance->delete( true );
+			$permanent = $request->request->get( 'delete_permanent' );
+			if ( $permanent ) {
+				$modelInstance->delete( true );
+			} else {
+				$modelInstance->trash( true );
+			}
 
 			return $this->json( [ 'message' => $this->trans( 'Entity deleted' ) ], Response::HTTP_OK );
 		} catch ( \Exception $e ) {
