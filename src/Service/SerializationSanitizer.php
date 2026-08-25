3cf40c39cae860262b774715ecd86ba864172dfa
Jory Hogeveen <JoryHogeveen@users.noreply.github.com>
2026-08-25 19:34:17 +0200

    Introduce HTML sanitizer (enabled by default)

--- src/Service/SerializationSanitizer.php
+++ src/Service/SerializationSanitizer.php
@@ -3,17 +3,33 @@
 namespace SyncEngine\Service;
 
 use Masterminds\HTML5\Parser\UTF8Utils;
+use Symfony\Component\HtmlSanitizer\HtmlSanitizer;
+use Symfony\Component\HtmlSanitizer\HtmlSanitizerConfig;
 
 class SerializationSanitizer
 {
 	const SANITIZE_UTF8 = 'encode_utf8';
 	const SANITIZE_RESOURCE = 'sanitize_resource';
+	const SANITIZE_HTML = 'sanitize_html';
+
+	private ?HtmlSanitizer $htmlSanitizer = null;
+
+	/**
+	 * @return $this
+	 */
+	public function setHtmlSanitizer( HtmlSanitizer $htmlSanitizer ): self
+	{
+		$this->htmlSanitizer = $htmlSanitizer;
+
+		return $this;
+	}
 
 	public function sanitize( mixed $data, array $options = [] ): mixed
 	{
 		$options = array_merge( [
 			self::SANITIZE_UTF8     => true,
 			self::SANITIZE_RESOURCE => true,
+			self::SANITIZE_HTML     => true,
 		], $options );
 
 		if ( is_iterable( $data ) ) {
@@ -28,7 +44,11 @@ class SerializationSanitizer
 		}
 
 		if ( is_string( $data ) && ! empty( $options[ self::SANITIZE_UTF8 ] ) ) {
-			$data = $this->encode_utf8( $data );
+			$data = $this->encodeUtf8( $data );
+		}
+
+		if ( is_string( $data ) && ! empty( $options[ self::SANITIZE_HTML ] ) ) {
+			$data = $this->sanitizeHtml( $data );
 		}
 
 		return $data;
@@ -46,11 +66,20 @@ class SerializationSanitizer
 		return $data;
 	}
 
-	public function encode_utf8( string $data ): string
+	public function encodeUtf8( string $data ): string
 	{
 		if ( mb_detect_encoding( $data, 'UTF-8', true ) !== 'UTF-8') {
 			return UTF8Utils::convertToUTF8( $data );
 		}
 		return $data;
+	}
+
+	public function sanitizeHtml( string $html ): string
+	{
+		if ( ! isset( $this->htmlSanitizer ) ) {
+			$this->htmlSanitizer = new HtmlSanitizer( ( new HtmlSanitizerConfig() )->allowSafeElements() );
+		}
+
+		return $this->htmlSanitizer->sanitize( $html );
 	}
 }
