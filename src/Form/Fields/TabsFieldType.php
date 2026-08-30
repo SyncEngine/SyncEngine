b0a0d222d058efcc94885e48f9d0917c3e3b056c
Jory Hogeveen <JoryHogeveen@users.noreply.github.com>
2026-08-31 01:21:02 +0200

    Add Tabs and Wizard types

---
+++ src/Form/Fields/TabsFieldType.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace SyncEngine\Form\Fields;
+
+use SyncEngine\Form\Fields\Collection\FieldCollection;
+use SyncEngine\Form\Fields\Interface\FieldConfigInterface;
+
+class TabsFieldType extends AbstractFieldType
+{
+	public function setTabs( FieldCollection|array $tabs ): static
+	{
+		if ( ! $tabs instanceof FieldCollection ) {
+			$tabs = new FieldCollection( $tabs );
+		}
+
+		$this->_set( 'tabs', $tabs );
+
+		return $this;
+	}
+
+	public function getTabs(): FieldCollection
+	{
+		if ( ! isset( $this['tabs'] ) ) {
+			$this->_set( 'tabs', new FieldCollection() );
+		}
+
+		return $this->_get( 'tabs' );
+	}
+
+	public function getTab( string $name ): ?FieldConfigInterface
+	{
+		if ( isset( $this['tabs'] ) ) {
+			return $this['tabs'][ $name ];
+		}
+
+		return null;
+	}
+
+	public function addTab( string $name, FieldConfigInterface|array $config ): static
+	{
+		if ( empty( $this['tabs'] ) ) {
+			$this['tabs'] = new FieldCollection();
+		}
+
+		$this['tabs'][ $name ] = $config;
+
+		return $this;
+	}
+
+	public function removeTab( string $name ): static
+	{
+		if ( isset( $this['tabs'] ) ) {
+			unset( $this['tabs'][ $name ] );
+		}
+
+		return $this;
+	}
+}
---
+++ src/Form/Fields/WizardFieldType.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace SyncEngine\Form\Fields;
+
+use SyncEngine\Form\Fields\Collection\FieldCollection;
+use SyncEngine\Form\Fields\Interface\FieldConfigInterface;
+
+class WizardFieldType extends AbstractFieldType
+{
+	public function setSteps( FieldCollection|array $steps ): static
+	{
+		if ( ! $steps instanceof FieldCollection ) {
+			$steps = new FieldCollection( $steps );
+		}
+
+		$this->_set( 'steps', $steps );
+
+		return $this;
+	}
+
+	public function getSteps(): FieldCollection
+	{
+		if ( ! isset( $this['steps'] ) ) {
+			$this->_set( 'steps', new FieldCollection() );
+		}
+
+		return $this->_get( 'steps' );
+	}
+
+	public function getStep( string $name ): ?FieldConfigInterface
+	{
+		if ( isset( $this['steps'] ) ) {
+			return $this['steps'][ $name ];
+		}
+
+		return null;
+	}
+
+	public function addStep( string $name, FieldConfigInterface|array $config ): static
+	{
+		if ( empty( $this['steps'] ) ) {
+			$this['steps'] = new FieldCollection();
+		}
+
+		$this['steps'][ $name ] = $config;
+
+		return $this;
+	}
+
+	public function removeStep( string $name ): static
+	{
+		if ( isset( $this['steps'] ) ) {
+			unset( $this['steps'][ $name ] );
+		}
+
+		return $this;
+	}
+}
