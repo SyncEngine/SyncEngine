7d55056af53c98e53f7a58f317d22ee8d8cc0dcf
Jory Hogeveen <JoryHogeveen@users.noreply.github.com>
2026-09-09 00:27:56 +0200

    Add CLI command to update trace data column

---
+++ src/Command/Tmp/FixTraceDataColumnCommand.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace SyncEngine\Command\Tmp;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use SyncEngine\Controller\DefaultController;
+
+/**
+ * @return void
+ */
+#[AsCommand(
+	name: 'syncengine:execute:dev:update:rename-trace-data-column',
+	description: 'Fix trace data column since 2026-09-08',
+)]
+class FixTraceDataColumnCommand extends Command
+{
+	// @phpstan-ignore-next-line property.onlyWritten (Required to trigger DI container initialization)
+	public function __construct( private DefaultController $controller )
+	{
+		parent::__construct();
+	}
+
+	protected function execute( InputInterface $input, OutputInterface $output ): int
+	{
+		try {
+			$em = $this->controller::getEntityManager();
+			$em->getConnection()->executeStatement('ALTER TABLE trace CHANGE trace data LONGTEXT NOT NULL');
+		} catch (\Exception $e) {
+			$output->writeln('<error>Error: ' . $e->getMessage() . '</error>');
+			return Command::FAILURE;
+		}
+
+		return Command::SUCCESS;
+	}
+}
