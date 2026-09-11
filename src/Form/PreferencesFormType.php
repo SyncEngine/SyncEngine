09c1ac97d32b11623717ff91302117f46320bfbe
Jory Hogeveen <JoryHogeveen@users.noreply.github.com>
2026-09-11 17:03:36 +0200

    Only show locales that actually exist

--- src/Form/PreferencesFormType.php
+++ src/Form/PreferencesFormType.php
@@ -2,6 +2,8 @@
 
 namespace SyncEngine\Form;
 
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\Finder\Finder;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -13,15 +15,31 @@ class PreferencesFormType extends AbstractType
 {
 	public function __construct(
 		private readonly Preferences $preferences,
+		#[Autowire( '%syncengine.dir.translations%' )]
+		private string $transDir = __DIR__ . '/../../translations',
 	) {}
 
 	public function buildForm( FormBuilderInterface $builder, array $options ): void
 	{
 		$localeChoices = [];
 
+		$finder = new Finder();
+		$coreTranslations = $finder->in($this->transDir)->files()->name( 'messages*');
+		$availableLocales = [];
+
+		foreach ( $coreTranslations as $file ) {
+			if ( preg_match( '/^messages(?:\+intl-icu)?\.([^.]+)\./', $file->getFilename(), $matches ) ) {
+				$availableLocales[ $matches[1] ] = true;
+			}
+		}
+
 		foreach ( Locales::getNames() as $locale => $name ) {
-			$label = $name . ' (' . $locale . ')';
-			$localeChoices[ $label ] = $locale;
+			// Fix this if statement to check if the locale is in the core translations.
+			// File format is messages.{locale}.{ext} or messages+intl-icu.{locale}.{ext}.
+			if ( isset( $availableLocales[ $locale ] ) ) {
+				$label = $name . ' (' . $locale . ')';
+				$localeChoices[ $label ] = $locale;
+			}
 		}
 
 		$builder
