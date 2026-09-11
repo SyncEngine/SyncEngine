d418c06368b0218530d9860d9dccb21a47974e98
Jory Hogeveen <JoryHogeveen@users.noreply.github.com>
2026-09-12 00:41:07 +0200

    Tags are not allowed in translation strings

--- src/Task/Set.php
+++ src/Task/Set.php
@@ -56,7 +56,7 @@ class Set extends TaskModel
 					$this->trans(
 						'When existing data is scalar, create new data collection from values set in this task.'
 					),
-					$this->trans( 'You can still reference the original value through the {{ data|String }} tag.' ),
+					$this->trans( 'You can still reference the original value through the {placeholder} tag.', [ 'placeholder' => '{{ data|String }}' ] ),
 				],
 				'type'  => 'switch',
 			],
