ff46958cb0d12891aee28d429f69db47e98fedc7
Jory Hogeveen <JoryHogeveen@users.noreply.github.com>
2026-09-14 18:02:43 +0200

    Commit missing UxTranslatorBundle

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
     Scheb\TwoFactorBundle\SchebTwoFactorBundle::class => ['all' => true],
+    Symfony\UX\Translator\UxTranslatorBundle::class => ['all' => true],
 ];
