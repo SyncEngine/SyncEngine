6727b9dbe37c9856052a7c439353dedd7c66c4fc
Jory Hogeveen <JoryHogeveen@users.noreply.github.com>
2026-09-14 14:10:46 +0200

    Implement Symfony UX Translator as a global function as well as a react hook (wrapper) with the "frontend" default domain.

---
+++ assets/react/hooks/useTranslator.js
@@ -0,0 +1,14 @@
+import { trans } from '../var/translations/index.js';
+
+/**
+ * useTranslator hook.
+ * Wraps Symfony UX Translator's trans() function to provide a similar API to react-i18next's useTranslation().
+ *
+ * This allows easy migration from i18next while leveraging Symfony's
+ * translation infrastructure.
+ */
+export function useTranslator( domain = 'frontend' ) {
+	return {
+		t: ( key, params = {} ) => trans( key, params, domain ),
+	};
+}
